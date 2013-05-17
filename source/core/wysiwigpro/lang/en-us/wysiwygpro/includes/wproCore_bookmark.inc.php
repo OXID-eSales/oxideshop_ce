ec46a0fc95b91f4958d1252b36b5e5f74230b152
Tomas Liubinas <github@liubinas.com>
2013-05-17 14:26:01 +0200

    Merging pull request

--- source/core/wysiwigpro/lang/en-us/wysiwygpro/includes/wproCore_bookmark.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('IN_WPRO')) exit;
-$lang = array();
-$lang['wproCore_bookmark'] = array();
-$lang['wproCore_bookmark']['bookmarkName'] = 'Bookmark name:';
-$lang['wproCore_bookmark']['existingBookmarks'] = 'Existing bookmarks:';
-?>

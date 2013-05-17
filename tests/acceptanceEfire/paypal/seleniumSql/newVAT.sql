59c00d484ce434a873a6fdaa42018d65669d6161
vilma liorensaityte <vilma.liorensaityte@gmail.com>
2013-05-17 14:19:31 +0300

    fixes

--- tests/acceptanceEfire/paypal/seleniumSql/newVAT.sql
+++
@@ -1,5 +0,0 @@
-#change Articles demodata VAT's						 
-UPDATE `oxarticles` SET `OXVAT` = '2' WHERE `OXID` = '1000';
-UPDATE `oxarticles` SET `OXVAT` = '13' WHERE `OXID` = '1001';
-UPDATE `oxarticles` SET `OXVAT` = '11' WHERE `OXID` = '1002';
-UPDATE `oxarticles` SET `OXVAT` = '15' WHERE `OXID` = '1003';

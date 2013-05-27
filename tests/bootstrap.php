3ddfe8f0ae46b46d134b3723866b8ed413c8a0c5
Artūras Ševčenko <arturas.sevcenko@gmail.com>
2013-05-27 11:03:01 +0300

    removed unnecessary lines from tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -48,73 +48,7 @@ if (getenv('oxADMIN_PASSWD')) {
 
 
 if (getenv('CODECOVERAGE')) {
-    // PHPUnit_Util_Filter configuration
-    /*PHPUnit_Util_Filter::$addUncoveredFilesFromWhitelist = true;
-
-    PHPUnit_Util_Filter::addDirectoryToFilter(oxPATH);
-
-
-    //PHPUnit_Util_Filter::addDirectoryToWhitelist(oxPATH.'/admin');
-    PHPUnit_Util_Filter::addDirectoryToWhitelist(oxPATH.'/core');
-    PHPUnit_Util_Filter::addDirectoryToWhitelist(oxPATH.'/application');
-
-    //PHPUnit_Util_Filter::addDirectoryToWhitelist(oxPATH.'/views/');
-
-   //PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/admin/inc/');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/phpdtaus/');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/jpgraph/');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/application/controllers/admin/reports/');
-
-    //PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/openid/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/adodblite/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/tcpdf/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/phpmailer/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/smarty/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/utils/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/facebook/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/application/views/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/out/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/tmp/');
-    PHPUnit_Util_Filter::removeDirectoryFromWhitelist(oxPATH.'/core/objects/');
-
-    /*PHPUnit_Util_Filter::removeFileFromWhitelist(oxPATH.'/admin/index.php');
-    PHPUnit_Util_Filter::removeFileFromWhitelist(oxPATH.'/core/oxerpbase.php');
-    PHPUnit_Util_Filter::removeFileFromWhitelist(oxPATH.'/core/oxerpcsv.php');
-    */
-    //PHPUnit_Util_Filter::removeFileFromWhitelist(oxPATH.'/core/oxopeniddb.php');
-    //PHPUnit_Util_Filter::removeFileFromWhitelist(oxPATH.'/core/oxopenidhttpfetcher.php');
-    //PHPUnit_Util_Filter::removeFileFromWhitelist(oxPATH.'/core/oxopenidgenericconsumer.php');
-
-
-/*
-    // add separate files
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/block.oxhasrights.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/emos.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxcontent.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxgetseourl.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxid_include_dynamic.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxinputhelp.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxmultilang.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxscript.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/function.oxvariantselect.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/insert.oxid_cmplogin.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/insert.oxid_cssmanager.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/insert.oxid_newbasketitem.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/insert.oxid_nocache.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/insert.oxid_tracker.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxaddparams.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxaddslashes.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxenclose.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxformdate.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxlower.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxmultilangassign.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxmultilangsal.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxnumberformat.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxtruncate.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxupper.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/modifier.oxwordwrap.php');
-    PHPUnit_Util_Filter::addFileToWhitelist(oxPATH.'/core/smarty/plugins/oxemosadapter.php');
-    */
+    // All configs moved to phpunit.xml
 }
 
 

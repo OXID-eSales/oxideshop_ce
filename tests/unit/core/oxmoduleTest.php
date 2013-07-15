d8667c54661f470f0f9c7508a6f85a9939ef00a5
Mantas Vaitkunas <manvait@gmail.com>
2013-07-15 18:16:04 +0300

    0005276: oxmodule->isActive returns true even there is no module registered in any way (cherry picked from commit 00ec940)

--- tests/unit/core/oxmoduleTest.php
+++ tests/unit/core/oxmoduleTest.php
@@ -104,10 +104,25 @@ class Unit_Core_oxmoduleTest extends OxidTestCase
      *
      * @return null
      */
-    public function testLoadWhenModuleDoesNotExists()
+    public function testLoad_WhenModuleDoesNotExists()
     {
         $oModule = new oxModule;
         $this->assertFalse( $oModule->load( 'non_existing_module' ) );
+    }
+
+    /**
+     * oxmodule::load() test case, when there is standalone file
+     *
+     * @return null
+     */
+    public function testLoad_WhenModuleExistsAndIsStandaloneFile()
+    {
+        $oModule = new oxModule;
+        $sModuleName = 'oe_testable_module.php';
+        $sModulePath = $oModule->getConfig()->getModulesDir() . '/' . $sModuleName;
+        touch( $sModulePath );
+        $this->assertTrue( $oModule->load( $sModuleName ) );
+        unlink( $sModulePath );
     }
 
     /**

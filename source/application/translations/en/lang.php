652755bcc64bc7160bc704464fbd3a12bdfc8424
Tadas Rimkus <dev.tadas.rimkus@gmail.com>
2013-07-30 14:42:20 +0300

    Reverting tax to VAT in english translation

--- source/application/translations/en/lang.php
+++ source/application/translations/en/lang.php
@@ -517,10 +517,10 @@ $aLang = array(
 'SHIPPING_CARRIER'                                            => "Shipping Carrier",
 'SHIPPING_COST'                                               => "Shipping cost",
 'SHIPPING_NET'                                                => "Shipping (net)",
-'PLUS_VAT'                                                    => "plus tax",
+'PLUS_VAT'                                                    => "plus VAT",
 'SHIPPING_VAT2'                                               => "% Amount",
-'VAT_PLUS_PERCENT_AMOUNT'                                     => "plus tax %s%% Amount",
-'SURCHARGE_PLUS_PERCENT_AMOUNT'                               => 'Surcharge tax %s%% Amount',
+'VAT_PLUS_PERCENT_AMOUNT'                                     => "plus VAT %s%% Amount",
+'SURCHARGE_PLUS_PERCENT_AMOUNT'                               => 'Surcharge VAT %s%% Amount',
 'SHOPLUPE'                                                    => "Shoplupe",
 'SIMILAR_PRODUCTS'                                            => "Similar Products",
 'SOCIAL_BOOKMARKS'                                            => "Social Bookmarks",
@@ -815,7 +815,7 @@ $aLang = array(
 'PARTNERS'                                                    => 'Partners',
 
 //not used
-'EMAIL_ORDER_CUST_HTML_PAYMENTCHARGEVAT1'                     => "Payment Charge tax",
+'EMAIL_ORDER_CUST_HTML_PAYMENTCHARGEVAT1'                      => "Payment Charge VAT",
 'FORM_REGISTER_COMPLETEMARKEDFIELDS'                          => "(Please fill in all mandatory fields labeled in bold.)",
 'MESSAGE_NO_DELIVERY_METHOD'                                  => '<p>Currently we have no delivery method set up for this country.</p> <p>We are aiming to find a possible delivery method and we will inform you as soon as possible via e-mail about the result, including further information about delivery costs.</p> <p>If you agree to this procedure, please select "move on to next step"</p>',
     'ERROR_MESSAGE_PAGE_HEADLINE'                                 => "The shop is temporary offline<br>Please come back later<br><br>Thank you for understanding.",

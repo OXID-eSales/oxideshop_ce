89cddb7fcc7eb01269da5ade2914a6128ed5cfbc
shiftas <Shiftass@gmail.com>
2013-08-07 18:12:43 +0300

    article variants selector javascript fix

--- source/out/azure/src/js/widgets/oxarticlevariant.js
+++ source/out/azure/src/js/widgets/oxarticlevariant.js
@@ -94,7 +94,7 @@
                     $('input[name=fnc]', form).val("");
                     form.submit();
                 }
-        e.stopPropagation();
+        return false;
     }
 
             /**
@@ -106,7 +106,7 @@
                 var form = $("form.js-oxWidgetReload");
                 $('input[name=fnc]', form).val("");
                 form.submit();
-        e.stopPropagation();
+        return false;
     }
 
     /**

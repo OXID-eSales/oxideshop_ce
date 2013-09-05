057b33c37bb8c55e531c439f2ac7180e7d0ff85c
Tomas Lygutas <Tomas.Lygutas@oxid-esales.com>
2013-09-05 14:49:48 +0300

    0005376: oxUtilsObject::_setToCache() - file_put_contents() missing file lock
    Added LOCK_EX parameter to used file_put_contents usages in
    source/core/oxcachebackenddefault.php
    source/core/oxutils.php
    source/core/oxutilsobject.php
    (cherry picked from commit 4d0449a)

--- source/core/oxutils.php
+++ source/core/oxutils.php
@@ -1395,7 +1395,7 @@ class oxUtils extends oxSuperCfg
     public function setLangCache( $sCacheName, $aLangCache )
     {
         $sCache = "<?php\n\$aLangCache = ".var_export( $aLangCache, true ).";\n?>";
-        $blRes = file_put_contents($this->getCacheFilePath($sCacheName), $sCache);
+        $blRes = file_put_contents($this->getCacheFilePath($sCacheName), $sCache, LOCK_EX);
         return $blRes;
     }
 

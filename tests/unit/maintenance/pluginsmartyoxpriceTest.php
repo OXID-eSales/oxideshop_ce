3c103f91c23d860e4f2790ac213ada0409b55c1b
l3k <linas.kukulskis@gmail.com>
2013-09-05 18:07:22 +0300

    0002773: (price currency) --> (currency price) very hard to switch, although its standard in many other shops (cherry picked from commit ae17cd1)

--- tests/unit/maintenance/pluginsmartyoxpriceTest.php
+++ tests/unit/maintenance/pluginsmartyoxpriceTest.php
@@ -25,7 +25,7 @@ require_once realpath( "." ).'/unit/OxidTestCase.php';
 require_once realpath( "." ).'/unit/test_config.inc.php';
 require_once oxConfig::getInstance()->getConfigParam( 'sShopDir' ).'core/smarty/plugins/function.oxprice.php';
 
-class Unit_Maintenance_pluginSmartyOxContentTest extends OxidTestCase
+class Unit_Maintenance_pluginSmartyOxPriceTest extends OxidTestCase
 {
     /**
      * Data provider

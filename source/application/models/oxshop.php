9a42bbb388f590db991eeb70f23c40cfdc27d06f
Tomas Lygutas <Tomas.Lygutas@oxid-esales.com>
2013-09-10 16:10:32 +0300

    0005385 - added removing of unused oxv_* views during updateViews() - second solution (cherry picked from commit 9716755) (cherry picked from commit 65f1d97)

---
+++ source/application/models/oxshop.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ *    This file is part of OXID eShop Community Edition.
+ *
+ *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OXID eShop Community Edition is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   core
+ * @copyright (C) OXID eSales AG 2003-2013
+ * @version OXID eShop CE
+ */
+
+/**
+ * Shop manager.
+ * Performs configuration and object loading or deletion.
+ *
+ * @package model
+ */
+class oxShop extends oxI18n
+{
+    /**
+     * Name of current class.
+     *
+     * @var string
+     */
+    protected $_sClassName = 'oxshop';
+
+    /**
+     * Multi shop tables, set in config.
+     *
+     * @var array
+     */
+    protected $_aMultiShopTables = array();
+
+
+    /**
+     * Class constructor, initiates parent constructor (parent::oxBase()).
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->init( 'oxshops' );
+
+        if ( $iMax = $this->getConfig()->getConfigParam('iMaxShopId') ) {
+            $this->setMaxShopId( $iMax );
+        }
+
+    }
+
+    /**
+     * Sets multi shop tables
+     *
+     * @param string $aMultiShopTables multi shop tables
+     *
+     * @return null
+     */
+    public function setMultiShopTables( $aMultiShopTables )
+    {
+        $this->_aMultiShopTables = $aMultiShopTables;
+    }
+
+
+    /**
+     * (Re)generates shop views
+     *
+     * @param bool  $blMultishopInheritCategories config option blMultishopInherit
+     * @param array $aMallInherit                 array of config options blMallInherit
+     *
+     * @return bool is all views generated successfully
+     */
+    public function generateViews( $blMultishopInheritCategories = false, $aMallInherit = null )
+    {
+        $oDb        = oxDb::getDb();
+        $aLanguages = oxRegistry::getLang()->getLanguageIds();
+
+        $aTables = $aMultilangTables = oxRegistry::getLang()->getMultiLangTables();
+
+        $aQ = array();
+
+        // Generate multitable views
+        foreach ( $aTables as $sTable ) {
+            $aQ[] = 'CREATE OR REPLACE SQL SECURITY INVOKER VIEW oxv_'.$sTable.' AS SELECT * FROM '.$sTable.' '.$this->_getViewJoinAll($sTable);
+
+            if (in_array($sTable, $aMultilangTables)) {
+                foreach ($aLanguages as $iLang => $sLang) {
+                    $aQ[] = 'CREATE OR REPLACE SQL SECURITY INVOKER VIEW oxv_'.$sTable.'_'.$sLang.' AS SELECT '.$this->_getViewSelect($sTable, $iLang).' FROM '.$sTable.' '.$this->_getViewJoinLang($sTable, $iLang);
+                }
+            }
+        }
+
+        $bSuccess = true;
+        foreach ($aQ as $sQ) {
+            if ( !$oDb->execute( $sQ ) ) {
+                $bSuccess = false;
+            }
+        }
+
+        $oViewsValidator = oxNew( 'oxShopViewValidator' );
+
+        $oViewsValidator->setShopId( $this->getConfig()->getShopId() );
+        $oViewsValidator->setLanguages( $aLanguages );
+        $oViewsValidator->setMultiLangTables( $aMultilangTables );
+        $oViewsValidator->setMultiShopTables( $aMultishopTables );
+
+        $aViews = $oViewsValidator->getInvalidViews();
+
+        foreach ($aViews as $sView) {
+            $oDb->execute( 'DROP VIEW IF EXISTS '. $sView );
+        }
+
+        return $bSuccess;
+    }
+
+    /**
+     * Returns table field name mapping sql section for single language views
+     *
+     * @param string $sTable table name
+     * @param array  $iLang  language id
+     *
+     * @return string $sSQL
+     */
+    protected function _getViewSelect($sTable,$iLang)
+    {
+        $oMetaData = oxNew('oxDbMetaDataHandler');
+        $aFields = $oMetaData->getSinglelangFields($sTable, $iLang);
+        foreach ($aFields as $sCoreField => $sField) {
+            if ($sCoreField !== $sField) {
+                $aFields[$sCoreField] = $sField.' AS '.$sCoreField;
+            }
+        }
+
+        return implode(',', $aFields);
+    }
+
+    /**
+     * Returns all language table view JOIN section
+     *
+     * @param string $sTable table name
+     *
+     * @return string $sSQL
+     */
+    protected function _getViewJoinAll($sTable)
+    {
+        $sJoin = ' ';
+        $oMetaData = oxNew('oxDbMetaDataHandler');
+        $aTables = $oMetaData->getAllMultiTables($sTable);
+        if (count($aTables)) {
+            foreach ($aTables as $sTableKey => $sTableName) {
+                $sJoin .= "LEFT JOIN {$sTableName} USING (OXID) ";
+            }
+        }
+        return $sJoin;
+    }
+
+    /**
+     * Returns language table view JOIN section
+     *
+     * @param string $sTable table name
+     * @param array  $iLang  language id
+     *
+     * @return string $sSQL
+     */
+    protected function _getViewJoinLang($sTable,$iLang)
+    {
+        $sJoin = ' ';
+        $sLangTable = getLangTableName($sTable, $iLang);
+        if ($sLangTable && $sLangTable !== $sTable) {
+            $sJoin .= "LEFT JOIN {$sLangTable} USING (OXID) ";
+        }
+        return $sJoin;
+    }
+
+
+    /**
+     * Returns default category of the shop.
+     *
+     * @return string
+     */
+    public function getDefaultCategory()
+    {
+        return $this->oxshops__oxdefcat->value;
+    }
+
+}
---
+++ tests/unit/core/oxshopviewvalidatorTest.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ *    This file is part of OXID eShop Community Edition.
+ *
+ *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OXID eShop Community Edition is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   tests
+ * @copyright (C) OXID eSales AG 2003-2013
+ * @version OXID eShop CE
+ */
+
+require_once realpath( "." ).'/unit/OxidTestCase.php';
+require_once realpath( "." ).'/unit/test_config.inc.php';
+
+class Unit_Core_oxShopViewValidatorTest extends OxidTestCase
+{
+    /**
+     * Testing MultiLangTables getter and setter
+     */
+    public function testSetGetMultiLangTables()
+    {
+        $oValidator = oxNew( "oxShopViewValidator" );
+        $oValidator->setMultiLangTables( array( "table1", "table2" ) );
+
+        $aList = $oValidator->getMultiLangTables();
+
+        $this->assertEquals( 2,  count( $aList ) );
+        $this->assertEquals( "table1",  $aList[0] );
+        $this->assertEquals( "table2",  $aList[1] );
+    }
+
+    /**
+     * Testing MultiLangTables getter and setter
+     */
+    public function testSetGetMultiShopTables()
+    {
+        $oValidator = oxNew( "oxShopViewValidator" );
+        $oValidator->setMultiShopTables( array( "table3", "table4" ) );
+
+        $aList = $oValidator->getMultiShopTables();
+
+        $this->assertEquals( 2,  count( $aList ) );
+        $this->assertEquals( "table3",  $aList[0] );
+        $this->assertEquals( "table4",  $aList[1] );
+    }
+
+    /**
+     * Testing MultiLangTables getter and setter
+     */
+    public function testSetGetLanguages()
+    {
+        $oValidator = oxNew( "oxShopViewValidator" );
+        $oValidator->setLanguages( array( "de", "xx" ) );
+
+        $aList = $oValidator->getLanguages();
+
+        $this->assertEquals( 2,  count( $aList ) );
+        $this->assertEquals( "de",  $aList[0] );
+        $this->assertEquals( "xx",  $aList[1] );
+    }
+
+    /**
+     * Testing MultiLangTables getter and setter
+     */
+    public function testSetGetShopId()
+    {
+        $oValidator = oxNew( "oxShopViewValidator" );
+        $oValidator->setShopId( 100 );
+
+        $this->assertEquals( 100,  $oValidator->getShopId() );
+    }
+
+    /**
+     * Tests getting list of invalid views
+     */
+    public function testGetInvalidViews()
+    {
+        $oDb = oxDb::getDb();
+        $oDb->execute( "DELETE FROM `oxshops` WHERE `oxid` > 1" );
+
+        $oShop = oxNew( "oxshop" );
+        $oShop->setId( 19 );
+        $oShop->oxshop__oxactive = new oxField( 1 );
+        $oShop->oxshop__oxname = new oxField( "Shop 19" );
+        $oShop->save();
+
+        $oValidator = $this->getMock( 'oxShopViewValidator', array( '_getAllViews', ) );
+        $oValidator->expects( $this->once() )->method( '_getAllViews' )->will( $this->returnValue( array(
+            'oxv_oxartextends',
+            'oxv_oxartextends_en',
+            'oxv_oxartextends_de',
+            'oxv_oxartextends_lt',
+            'oxv_oxarticles',
+            'oxv_oxarticles_en',
+            'oxv_oxarticles_de',
+            'oxv_oxarticles_ru',
+            'oxv_oxarticles_1',
+            'oxv_oxarticles_1_en',
+            'oxv_oxarticles_1_de',
+            'oxv_oxarticles_1_lt',
+            'oxv_oxarticles_10',
+            'oxv_oxarticles_10_en',
+            'oxv_oxarticles_10_de',
+            'oxv_oxarticles_10_lt',
+            'oxv_oxarticles_19',
+            'oxv_oxarticles_19_en',
+            'oxv_oxarticles_19_de',
+            'oxv_oxarticles_19_lt',
+        ) ) );
+
+        $aLanguageIds = array( 0 => 'de', 1 => 'en' );
+
+        $oValidator->setShopId( 1 );
+        $oValidator->setLanguages( $aLanguageIds );
+        $oValidator->setMultiLangTables( array( 'oxartextends', 'oxarticles' ) );
+        $oValidator->setMultiShopTables( array( 'oxarticles' ) );
+
+        $aResult = $oValidator->getInvalidViews();
+
+        $this->assertEquals( 3, count($aResult) );
+        $this->assertContains( 'oxv_oxartextends_lt', $aResult );
+        $this->assertContains( 'oxv_oxarticles_1_lt', $aResult );
+        $this->assertContains( 'oxv_oxarticles_ru', $aResult );
+    }
+}

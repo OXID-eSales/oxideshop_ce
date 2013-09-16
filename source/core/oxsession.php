7074dec0318fd84318ab49c0995c0c1bd151b88c
Tomas Liubinas <git@liubinas.com>
2013-09-16 16:01:24 +0300

    ESDEV-424 force_sid problem oxSession::isActualSidInCookie() implementation

--- source/core/oxsession.php
+++ source/core/oxsession.php
@@ -829,8 +829,8 @@ class oxSession extends oxSuperCfg
      */
     public function isActualSidInCookie()
     {
-        $blReturn = ($_COOKIE[$this->getName()] == $this->getId());
-        $blReturn;
+        $blReturn = (isset($_COOKIE[$this->getName()]) &&  ($_COOKIE[$this->getName()] == $this->getId()));
+        return $blReturn;
     }
 
     /**

4220a4c8444070462eea8fd41ebbb852b3e615d5
Vaidas <vaidasif@gmail.com>
2013-10-03 17:11:46 +0300

    Sending even empty form values through ajax  Issue was that you can t unselect selected pr

--- source/out/azure/src/js/widgets/oxarticlevariant.js
+++ source/out/azure/src/js/widgets/oxarticlevariant.js
@@ -1,18 +1,18 @@
 /**
- *    This file is part of OXID eShop Community Edition.
+ * This file is part of OXID eShop Community Edition.
  *
- *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    OXID eShop Community Edition is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
  * @package   out
@@ -132,7 +132,7 @@
                         var aSelectionInputs = $("input[name^=varselid]", $("form.js-oxProductForm"));
                         if (aSelectionInputs.length) {
                             var sHash = '';
-                    aSelectionInputs.not("*[value='']").each(function(i) {
+                    aSelectionInputs.each(function(i) {
                         sHash = sHash+i+':'+$(this).val()+"|";
                                 aOptions[$(this).attr( "name" )] = $(this).val();
                             });

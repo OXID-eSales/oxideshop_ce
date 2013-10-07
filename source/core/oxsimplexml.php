1049b4d71e364fea92c48cc9a0cb0aa0581af780
Tomas Liubinas <git@liubinas.com>
2013-10-07 18:16:36 +0300

    Simple XML encoder/decoder implemented

---
+++ source/core/oxsimplexml.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   core
+ * @copyright (C) OXID eSales AG 2003-2013
+ * @version OXID eShop CE
+ */
+
+/**
+ * Simple XML handler
+ */
+class oxSimpleXml {
+
+
+    /**
+     * Recursively converts OxStdClass object instance to SimpleXMLElement structure
+     *
+     * @param SimpleXMLElement    $oXml   Xml handler
+     * @param string|array|object $oInput Input object
+     *
+     * @return SimpleXMLElement
+     */
+    protected function _addSimpleXmlElement($oXml, $oInput)
+    {
+        $aObjectVars = get_object_vars( $oInput );
+
+        foreach ($aObjectVars as $sKey => $oVar) {
+            if (is_object( $oVar ) ) {
+
+                $oChildNode = $oXml->addChild($sKey);
+                $this->_addSimpleXmlElement($oChildNode, $oVar);
+
+            } elseif (is_array( $oVar) ) {
+                foreach ($oVar as $oSubValue) {
+                    $oXml->addChild($sKey, $oSubValue);
+                }
+            } else {
+                //assume $oVar is string
+                $oXml->addChild($sKey, $oVar);
+            }
+        }
+
+        return $oXml;
+    }
+
+    /**
+     * Parses object structure to XML string
+     *
+     * Example object:
+     * oxStdClass Object
+     *   (
+     *       [title] => TestTitle
+     *       [keys] => oxStdClass Object
+     *           (
+     *               [key] => Array
+     *                   (
+     *                       [0] => testKey1
+     *                       [1] => testKey2
+     *                   )
+     *           )
+     *   )
+     *
+     * would produce the following XML:
+     * <?xml version="1.0" encoding="utf-8"?>
+     * <testXml><title>TestTitle</title><keys><key>testKey1</key><key>testKey2</key></keys></testXml>
+     *
+     * @param  object $oInput    Input object
+     * @param  string $sDocument Document name.
+     *
+     * @return string
+     */
+    public function objectToXml($oInput, $sDocument)
+    {
+        $oXml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"utf-8\"?><$sDocument/>");
+        $this->_addSimpleXmlElement($oXml, $oInput);
+
+        return $oXml->asXml();
+    }
+
+    /**
+     * Parses XML string into object structure
+     *
+     * @param string $sXml XML Input
+     */
+    public function xmlToObject($sXml)
+    {
+        return simplexml_load_string($sXml);
+    }
+}
---
+++ tests/unit/core/oxsimplexmlTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   tests
+ * @copyright (C) OXID eSales AG 2003-2013
+ * @version OXID eShop CE
+ */
+
+require_once realpath( "." ).'/unit/OxidTestCase.php';
+
+/**
+ * Testing oxXml class.
+ */
+class Unit_Core_oxSimpleXmlTest extends OxidTestCase
+{
+    public function testObjectToXml()
+    {
+        $oXml = new oxSimpleXml();
+
+        $oTestObject = new oxStdClass();
+        $oTestObject->title = "TestTitle";
+        $oTestObject->keys = new oxStdClass();
+        $oTestObject->keys->key = array("testKey1", "testKey2");
+
+        $sTestResult = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<testXml><title>TestTitle</title><keys><key>testKey1</key><key>testKey2</key></keys></testXml>\n";
+
+        $this->assertEquals($sTestResult, $oXml->objectToXml( $oTestObject, "testXml" ));
+    }
+
+    public function testXmlToObject()
+    {
+        $oXml = new oxSimpleXml();
+
+        $sTestXml = '<?xml version="1.0"?><testXml><title>TestTitle</title><keys><key>testKey1</key><key>testKey2</key></keys></testXml>';
+
+        $oRes = $oXml->xmlToObject( $sTestXml );
+
+        $this->assertEquals((string) $oRes->title,         "TestTitle");
+        $this->assertEquals((string) $oRes->keys->key[0],  "testKey1");
+        $this->assertEquals((string) $oRes->keys->key[1],  "testKey2");
+    }
+}

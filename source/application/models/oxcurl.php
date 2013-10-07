44f054d447709d05bc8f27f30dfc15ff2078dc53
vilmal <vilma.liorensaityte@gmail.com>
2013-10-07 18:14:22 +0300

    added curl wrapper (oxCurl)

--- source/application/models/oxcurl.php
+++ source/application/models/oxcurl.php
@@ -28,113 +28,319 @@
  * @package model
  */
 
-class oxCurl {
+class oxCurl
+{
+    /**
+     * Curl instance.
+     *
+     * @var resource
+     */
+    protected $_rCurl = null;
 
     /**
-     * Export output folder
+     * URL to call
      *
+     * @var string | null
+     */
+    protected $_sUrl = null;
+
+    /**
+     * Query like "param1=value1&param2=values2.."
+     *
+     * @return string
+     */
+    protected $_sQuery = null;
+
+    /**
+     * Set CURL method
+     *
+     * @return string
+     */
+    protected $_sMethod = 'POST';
+
+    /**
+     * Parameter to be added to call url
+     *
+     * @var array | null
+     */
+    protected $_aParameters = null;
+
+    /**
+     * Connection Charset.
      * @var string
      */
-    protected $_iCurlTimeout = 30;
+    protected $_sConnectionCharset = "UTF-8";
 
     /**
-     * Web service script
-     *
-     * @var string
-     */
-    protected $_sWebServiceUrl = '';
-
-    /**
-     * Web service script
-     *
+     * Curl call header.
      * @var array
      */
-    protected $_aWebServiceParams = array();
+    protected $_aHeader = null;
 
     /**
-     * CRUL time out setter
-     *
-     * @param $iSeconds integer
+     * Host for header.
+     * @var string
      */
-    public function setTimeout( $iSeconds )
+    protected $_sHost = null;
+
+    /**
+     * Sets resource
+     *
+     * @param resource $rCurl curl.
+     */
+    protected function _setResource( $rCurl )
     {
-        if ( $iSeconds > 0 ) {
-            $this->_iCurlTimeout = $iSeconds;
+        $this->_rCurl = $rCurl;
         }
-    }
 
     /**
-     * CRUL time out getter
+     * Returns curl resource
      *
-     * @return integer
+     * @return resource
      */
-    public function getTimeout()
+    protected function _getResource()
     {
-        return $this->_iCurlTimeout;
-    }
-
-    /**
-     * web service URL setter
-     *
-     * @param $sServiceUrl string
-     */
-    public function setWebServiceUrl( $sServiceUrl )
-    {
-        if ( !empty( $sServiceUrl ) ) {
-            $this->_sWebServiceUrl = $sServiceUrl;
+        if ( is_null( $this->_rCurl ) ) {
+            $this->_setResource( curl_init() );
         }
+
+        return $this->_rCurl;
     }
 
     /**
-     * web service URL setter
+     * Sets url to call
+     *
+     * @param string $sUrl URL to call.
+     *
+     * @throws oxException if url is not valid
+     *
+     * @return null
+     */
+    public function setUrl( $sUrl )
+    {
+        if ( false === filter_var( $sUrl, FILTER_VALIDATE_URL ) ) {
+            /**
+             * @var oxException $oException
+             */
+            $oException = oxNew( "oxException", 'EXCEPTION_NOT_VALID_URL' );
+            throw $oException;
+        }
+        $this->_sUrl = $sUrl;
+    }
+
+    /**
+     * Get url
      *
      * @return string
      */
-    public function getWebServiceUrl()
+    public function getUrl()
     {
-        return $this->_sWebServiceUrl;
-    }
-
-
-    /**
-     * web service URL setter
-     *
-     * @param $aServiceParams array of parameters in format [key = > value]
-     */
-    public function setWebServiceParams( $aServiceParams )
-    {
-        if ( !empty( $aServiceParams ) ) {
-            $this->_aWebServiceParams = $aServiceParams;
+        return $this->_sUrl;
         }
+
+    /**
+     * Set query like "param1=value1&param2=values2.."
+     */
+    public function setQuery( $sQuery )
+    {
+        $this->_sQuery = $sQuery;
     }
 
     /**
-     * web service url getter
+     * Builds query like "param1=value1&param2=values2.."
      *
-     * @param null $aServiceParams - optional  web service parameters can be set here instead of setter
      * @return string
      */
-    public function getWebServiceRequestUrl( $aServiceParams = null )
+    public function getQuery()
     {
-        return $this->_sWebServiceUrl . '?' . http_build_query( ( $aServiceParams != null ) ? $aServiceParams : $this->_aWebServiceParams );
-    }
+        if ( is_null( $this->_sQuery ) ) {
 
+            $aParams = $this->getParameters();
+            $aParams = array_filter( $aParams );
+            $aParams = array_map(array($this, '_htmlDecode'), $aParams);
+
+            $this->setQuery( http_build_query( $aParams, "", "&" ) );
+        }
+
+        return $this->_sQuery;
+    }
 
     /**
-     * @param null $aServiceParams - optional  web service parameters can be set here instead of setter
-     * @return string XML
+     * Sets parameters to be added to call url.
+     *
+     * @param array $aParameters parameters
      */
-    public function callWebService( $aServiceParams = null )
+    public function setParameters( $aParameters )
     {
-        $curl = curl_init();
-        curl_setopt( $curl,CURLOPT_URL, $this->getWebServiceRequestUrl( $aServiceParams ) );
-        curl_setopt( $curl,CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $curl,CURLOPT_CONNECTTIMEOUT, $this->_iCurlTimeout );
-        $sResponse = curl_exec( $curl );
-        curl_close( $curl );
-
-        return $sResponse;
+        $this->_aParameters = $aParameters;
     }
 
+    /**
+     * Return parameters to be added to call url.
+     *
+     * return array
+     */
+    public function getParameters()
+    {
+        return $this->_aParameters;
+        }
+
+    /**
+     * Set connection charset
+     *
+     * @param string $sCharset charset
+     */
+    public function setConnectionCharset( $sCharset )
+    {
+        $this->_sConnectionCharset = $sCharset;
+    }
+
+    /**
+     * Return connection charset
+     *
+     * @return string
+     */
+    public function getConnectionCharset()
+    {
+        return $this->_sConnectionCharset;
+    }
+
+    /**
+     * Sets host.
+     *
+     * @param string $sHost
+     *
+     * @return null
+     */
+    public function setHost( $sHost )
+    {
+        $this->_sHost = $sHost;
+    }
+
+    /**
+     * Returns host.
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->_sHost;
+    }
+
+    /**
+     * Set header.
+     *
+     * @param array $aHeader
+     *
+     * @return null
+     */
+    public function setHeader( $aHeader = null )
+    {
+        if ( is_null( $aHeader ) ) {
+            $sHost = $this->getHost();
+
+            $aHeader = array();
+            $aHeader[] = 'POST /cgi-bin/webscr HTTP/1.1';
+            $aHeader[] = 'Content-Type: application/x-www-form-urlencoded';
+            if ( isset( $sHost ) ) {
+                $aHeader[] = 'Host: '. $sHost;
+            }
+            $aHeader[] = 'Connection: close';
+        }
+        $this->_aHeader = $aHeader;
+    }
+
+    /**
+     * Forms header from host.
+     *
+     * @return array
+     */
+    public function getHeader()
+    {
+        if ( is_null( $this->_aHeader ) ) {
+            $this->setHeader();
+        }
+        return $this->_aHeader;
+    }
+
+    /**
+     * Set method to send (POST/GET)
+     *
+     * @param string $sMethod method to send (POST/GET)
+     *
+     * @return CurlWrapper
+     */
+    public function setMethod($sMethod)
+    {
+        $this->_sMethod = strtoupper($sMethod);
+    }
+
+    /**
+     * Return method to send
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->_sMethod;
+    }
+
+    /**
+     * Sets an option for a cURL transfer
+     *
+     * @param string $sName  curl option name to set value to.
+     * @param string $sValue curl option value to set.
+     */
+    public function setOption( $sName, $sValue )
+    {
+        if (strpos( $sName, 'CURLOPT_' ) !== 0 || !defined($sConstant = strtoupper($sName))) {
+            /**
+             * @var oxException $oException
+             */
+            $oException = oxNew( "oxException", 'EXCEPTION_NOT_VALID_CONSTANT' );
+            throw $oException;
+        }
+
+        $this->_setOpt( $sName, $sValue );
+    }
+
+    /**
+     * Wrapper function to be mocked for testing.
+     *
+     * @param string $sName  curl option name to set value to.
+     * @param string $sValue curl option value to set.
+     */
+    protected function _setOpt( $sName, $sValue )
+    {
+        curl_setopt( $this->_getResource(), $sName, $sValue );
+    }
+
+    /**
+     * Set Curl Options.
+     */
+    protected function _setOptions()
+    {
+       /* foreach( $this->getEnvironmentParameters() as $sName => $mValue  ) {
+            $this->setOption( constant( $sName ), $mValue );
+        }*/
+
+        $this->_setOpt( CURLOPT_HTTPHEADER, $this->getHeader() );
+        $this->_setOpt( CURLOPT_URL, $this->getUrl() );
+        $this->_setOpt( CURLOPT_POSTFIELDS, $this->getQuery() );
+    }
+
+    /**
+     * Decode (if needed) html entity
+     *
+     * @param string $sString query
+     *
+     * @return string
+     */
+    protected function _htmlDecode( $sString )
+    {
+        $sString = html_entity_decode( stripslashes( $sString ), ENT_QUOTES, $this->getConnectionCharset() );
+
+        return $sString;
+    }
 
 }
--- tests/unit/core/oxcurlTest.php
+++ tests/unit/core/oxcurlTest.php
@@ -27,7 +27,257 @@ require_once realpath( "." ).'/unit/test_config.inc.php';
 class Unit_Core_oxCurlTest extends OxidTestCase
 {
 
-    public function testGetWebServiceRequestURL()
+    /**
+     * Test oxCurl::setUrl()
+     * Test oxCurl::getUrl()
+     */
+    public function testGetUrl_urlSet_setReturned()
+    {
+        $sEndpointUrl = 'http://www.oxid-esales.com/index.php?anid=article';
+
+        $oCurl = new oxCurl();
+        $oCurl->setUrl( $sEndpointUrl );
+        $sUrlToCall = $oCurl->getUrl();
+
+        $this->assertEquals( $sEndpointUrl, $sUrlToCall, 'Url should be same as provided from config.' );
+    }
+
+    /**
+     * Test oxCurl::getUrl()
+     */
+    public function testGetUrl_notSet_null()
+    {
+        $oCurl = new oxCurl();
+        $this->assertNull( $oCurl->getUrl() );
+    }
+
+    /**
+     * Test oxCurl::setUrl()
+     * Test oxCurl::getUrl()
+     */
+    public function testGetUrl_badUrlSet_Exception()
+    {
+        $this->setExpectedException( 'oxException' );
+
+        $sEndpointUrl = 'url';
+        $oCurl = new oxCurl();
+        $oCurl->setUrl( $sEndpointUrl );
+
+        $this->assertEquals( $sEndpointUrl, $oCurl->getUrl() );
+    }
+
+
+    /**
+     * Test oxCurl::setQuery()
+     */
+    public function testSetQuery_set_get()
+    {
+        $oCurl = new oxCurl();
+        $oCurl->setQuery( 'param1=value1&param2=values2' );
+
+        $this->assertEquals( 'param1=value1&param2=values2', $oCurl->getQuery() );
+    }
+
+    /**
+     * Test oxCurl::getQuery()
+     */
+    public function testGetQuery_setParameter_getQueryFromParameters()
+    {
+        $oCurl = new oxCurl();
+        $oCurl->setParameters( array( 'param1'=>'value1', 'param2' => 'values2' ) );
+
+        $this->assertEquals( 'param1=value1&param2=values2', $oCurl->getQuery() );
+    }
+
+    /**
+     * Test oxCurl::getQuery()
+     */
+    public function testGetQuery_setParameterNotUtf_getQueryFromParameters()
+    {
+        $oCurl = new oxCurl();
+        $oCurl->setParameters( array( 'param1' => 'Jäger', 'param2' => 'values2' ) );
+
+        $aPramsUtf = array( 'param1' => 'Jäger', 'param2' => 'values2' );
+
+        $this->assertEquals( http_build_query($aPramsUtf), $oCurl->getQuery() );
+    }
+
+    /**
+     * Test oxCurl::getParameters()
+     */
+    public function testGetParameters_default_null()
+    {
+        $oCurl = new oxCurl();
+        $this->assertNull( $oCurl->getParameters() );
+    }
+
+    /**
+     * Test oxCurl::getParameters()
+     */
+    public function testGetParameters_set_returnSet()
+    {
+        $aParameters = array('parameter' => 'value');
+
+        $oCurl = new oxCurl();
+        $oCurl->setParameters( $aParameters );
+        $this->assertEquals( $aParameters, $oCurl->getParameters() );
+    }
+
+    /**
+     * Test oxCurl::setConnectionCharset()
+     */
+    public function testSetConnectionCharset_set_get()
+    {
+        $oCurl = new oxCurl();
+        $oCurl->setConnectionCharset( 'ISO-8859-1' );
+
+        $this->assertEquals( 'ISO-8859-1', $oCurl->getConnectionCharset() );
+    }
+
+    /**
+     * Test oxCurl::getConnectionCharset()
+     */
+    public function testGetConnectionCharset_notSet_UTF()
+    {
+        $oCurl = new oxCurl();
+        $this->assertEquals( 'UTF-8', $oCurl->getConnectionCharset() );
+    }
+    /**
+     * Checks if function returns null when nothing is set.
+     */
+    public function testGetHost_notSet_null()
+    {
+        $oCurl = new oxCurl();
+        $this->assertNull( $oCurl->getHost(), 'Default value must be null.' );
+    }
+
+    /**
+     * Check if getter returns what is set in setter.
+     */
+    public function testGetHost_setHost_host()
+    {
+        $sHost = 'someHost';
+
+        $oCurl = new oxCurl();
+        $oCurl->setHost( $sHost );
+
+        $this->assertEquals( $sHost, $oCurl->getHost(), 'Check if getter returns what is set in setter.' );
+    }
+
+    /**
+     * Checks if returned header is correct when header was not set and host was set.
+     */
+    public function testGetHeader_headerNotSetAndHostSet_headerWithHost()
+    {
+        $sHost = 'someHost';
+        $aExpectedHeader = array(
+            'POST /cgi-bin/webscr HTTP/1.1',
+            'Content-Type: application/x-www-form-urlencoded',
+            'Host: '. $sHost,
+            'Connection: close'
+        );
+        $oCurl = new oxCurl();
+        $oCurl->setHost( $sHost );
+
+        $this->assertEquals( $aExpectedHeader, $oCurl->getHeader(), 'Header must be formed from set host.' );
+    }
+
+    /**
+     * Checks if returned header is correct when header was not set and host was not set.
+     */
+    public function testGetHeader_headerNotSetAndHostNotSet_headerWithoutHost()
+    {
+        $aExpectedHeader = array(
+            'POST /cgi-bin/webscr HTTP/1.1',
+            'Content-Type: application/x-www-form-urlencoded',
+            'Connection: close'
+        );
+        $oCurl = new oxCurl();
+
+        $this->assertEquals( $aExpectedHeader, $oCurl->getHeader(), 'Header must be without host as host not set.' );
+    }
+
+    /**
+     * Checks if returned header is correct when header was set and host was set.
+     */
+    public function testGetHeader_headerSetAndHostSet_headerFromSet()
+    {
+        $sHost = 'someHost';
+        $aHeader = array( 'Test header' );
+        $oCurl = new oxCurl();
+        $oCurl->setHost( $sHost );
+        $oCurl->setHeader( $aHeader );
+
+        $this->assertEquals( $aHeader, $oCurl->getHeader(), 'Header must be same as set header.' );
+    }
+
+    /**
+     * Checks if returned header is correct when header was set and host was not set.
+     */
+    public function testGetHeader_headerSetAndHostNotSet_headerWithoutHost()
+    {
+        $aHeader = array( 'Test header' );
+        $oCurl = new oxCurl();
+        $oCurl->setHeader( $aHeader );
+
+        $this->assertEquals( $aHeader, $oCurl->getHeader(), 'Header must be same as set header.' );
+    }
+
+    /**
+     * Test oxCurl::setOption()
+     */
+    public function testSetOption()
+    {
+        $oCurl = $this->getMock( 'oxCurl', array( '_setOpt' ) );
+        $oCurl->expects( $this->any() )->method( '_setOpt' )->with( $this->equalTo( 'CURLOPT_VERBOSE' ), $this->equalTo( 0 ) );
+        $oCurl->setOption( 'CURLOPT_VERBOSE', 0 );
+    }
+
+    /**
+     * Test oxCurl::setOption()
+     */
+    public function testSetOption_NotCurlOption()
+    {
+        $this->setExpectedException( 'oxException' );
+
+        $oCurl = $this->getMock( 'oxCurl', array( '_setOpt' ) );
+        $oCurl->expects( $this->never() )->method( '_setOpt' );
+        $oCurl->setOption( "rParam1", "rValue1" );
+    }
+
+    /**
+     * Test oxCurl::setOption()
+     */
+    public function testSetOption_WrongOptionName()
+    {
+        $this->setExpectedException( 'oxException' );
+
+        $oCurl = $this->getMock( 'oxCurl', array( '_setOpt' ) );
+        $oCurl->expects( $this->never() )->method( '_setOpt' );
+        $oCurl->setOption( 'CURLOPT_WRONG', "rValue1" );
+    }
+
+    /**
+     * Test oxCurl::setMethod()
+     * Test oxCurl::getMethod()
+     */
+    public function testSetGetMethod()
+    {
+        $oCurl = new oxCurl();
+        $oCurl->setMethod( 'POST' );
+
+        $this->assertEquals( 'POST', $oCurl->getMethod() );
+    }
+
+
+
+
+
+
+
+
+
+  /*  public function testGetWebServiceRequestURL()
     {
         $oCurl = oxNew( "oxCurl" );
 
@@ -41,5 +291,5 @@ class Unit_Core_oxCurlTest extends OxidTestCase
 
         $this->assertEquals( "www.google.com?param1=val1&param2=val2", $oCurl->getWebServiceRequestURL() );
         $this->assertEquals( "www.google.com?param3=val3&param4=val4", $oCurl->getWebServiceRequestURL( array( "param3" => "val3", "param4" => "val4" ) ) );
-    }
+    }*/
 }

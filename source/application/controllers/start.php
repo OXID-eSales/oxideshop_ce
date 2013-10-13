305020e021edf6604b53d1c40c41b7a8e8c5b451
Artūras Ševčenko <arturas.sevcenko@gmail.com>
2013-10-13 19:39:48 +0300

    revert e1ab58d (fatal)

--- source/application/controllers/start.php
+++ source/application/controllers/start.php
@@ -125,7 +125,6 @@ class Start extends oxUBase
      */
     public function render()
     {
-
         if ( oxConfig::getParameter( 'showexceptionpage' ) == '1' ) {
             return 'message/exception.tpl';
         }

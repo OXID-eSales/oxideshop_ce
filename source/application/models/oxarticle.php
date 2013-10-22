e07fe69968640d1c6139800182f0edee81eb9663
Frank Liepert <trainmaster@gmx.net>
2013-10-22 20:56:58 +0200

    Fix undefined constant

--- source/application/models/oxarticle.php
+++ source/application/models/oxarticle.php
@@ -3143,7 +3143,7 @@ class oxArticle extends oxI18n implements oxIArticle, oxIUrl
     protected function _getGroupPrice()
     {
         $sPriceSufix = $this->_getUserPriceSufix();
-        $sVarName = oxarticles__oxprice.$sPriceSufix;
+        $sVarName = $this->oxarticles__oxprice . $sPriceSufix;
         $dPrice = $this->$sVarName->value;
 
         // #1437/1436C - added config option, and check for zero A,B,C price values

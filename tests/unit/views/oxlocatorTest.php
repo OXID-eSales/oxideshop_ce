f10876eac4ce1279d8422ceb0a5f275d90ee7fbb
l3k <linas.kukulskis@gmail.com>
2013-11-15 12:02:26 +0200

    0005031: Function 'getVendorTree' is missing (cherry picked from commit c949b3d) (cherry picked from commit 4fe4391)

--- tests/unit/views/oxlocatorTest.php
+++ tests/unit/views/oxlocatorTest.php
@@ -19,7 +19,6 @@
  * @package   tests
  * @copyright (C) OXID eSales AG 2003-2013
  * @version OXID eShop CE
- * @version   SVN: $Id$
  */
 
 
@@ -257,7 +256,7 @@ class Unit_Views_oxlocatorTest extends OxidTestCase
     public function testSetVendorLocatorData()
     {
         // seo off
-        modConfig::getInstance()->setConfigParam( 'blSeoMode', false );
+        $this->getConfig()->setConfigParam( 'blSeoMode', false );
         oxUtils::getInstance()->seoIsActive( true );
 
         $myConfig = oxConfig::getInstance();
@@ -282,7 +281,6 @@ class Unit_Views_oxlocatorTest extends OxidTestCase
         $oLocatorTarget->expects( $this->any() )->method( 'setCatTreePath' );
         $oLocatorTarget->expects( $this->any() )->method( 'getCatTreePath' );
         $oLocatorTarget->expects( $this->once() )->method( 'getActVendor' )->will( $this->returnValue( $oVendor ) );
-        $oLocatorTarget->expects( $this->once() )->method( 'getVendorTree' )->will( $this->returnValue( new oxvendorlist ) );
         $oLocatorTarget->expects( $this->once() )->method( 'showSorting' )->will( $this->returnValue( true ) );
         $oLocatorTarget->expects( $this->any() )->method( 'getLinkType' )->will( $this->returnValue( OXARTICLE_LINKTYPE_VENDOR ) );
 
@@ -326,7 +324,6 @@ class Unit_Views_oxlocatorTest extends OxidTestCase
         $oLocatorTarget->expects( $this->any() )->method( 'setCatTreePath' );
         $oLocatorTarget->expects( $this->any() )->method( 'getCatTreePath' );
         $oLocatorTarget->expects( $this->once() )->method( 'getActVendor' )->will( $this->returnValue( $oVendor ) );
-        $oLocatorTarget->expects( $this->once() )->method( 'getVendorTree' )->will( $this->returnValue( new oxvendorlist ) );
         $oLocatorTarget->expects( $this->once() )->method( 'showSorting' )->will( $this->returnValue( true ) );
         $oLocatorTarget->expects( $this->any() )->method( 'getLinkType' )->will( $this->returnValue( OXARTICLE_LINKTYPE_VENDOR ) );
 

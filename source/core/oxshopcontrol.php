14e66531491dff74fa3dd20a990d82627ac2634e
Mantas Vaitkunas <manvait@gmail.com>
2013-12-10 11:41:02 +0200

    ESDEV-899 Solve "Black Friday" problem Added some params and fixed comments

--- source/core/oxshopcontrol.php
+++ source/core/oxshopcontrol.php
@@ -604,7 +604,7 @@ class oxShopControl extends oxSuperCfg
      *
      * @param $oEx
      */
-    protected function _handleSystemException()
+    protected function _handleSystemException( $oEx )
     {
         //possible reason: class does not exist etc. --> just redirect to start page
         if ( $this->_isDebugMode() ) {
@@ -613,12 +613,15 @@ class oxShopControl extends oxSuperCfg
         }
         $oEx->debugOut();
 
+        $myConfig = $this->getConfig();
         if ( !$myConfig->getConfigParam( 'iDebug' ) ) {
             oxRegistry::getUtils()->redirect( $myConfig->getShopHomeUrl() .'cl=start', true, 302 );
         }
     }
 
     /**
+     * Redirect to start page, in debug mode shows error message.
+     *
      * @param $oEx
      */
     protected function _handleCookieException( $oEx )
@@ -630,7 +633,7 @@ class oxShopControl extends oxSuperCfg
     }
 
     /**
-     * R&R handling -> redirect to error msg, also, can call _process again, specifying error handler view class
+     * R&R handling -> redirect to error msg, also, can call _process again, specifying error handler view class.
      *
      * @param $oEx
      */

feb4da9e5c3a1c7aae59e8c2cb374c19a8ac4934
tomasly <Tomas.Lygutas@oxid-esales.com>
2013-12-11 17:41:26 +0200

    ESDEV-889 Port Sepa bug fix to development branch Moving data attribute to the top of the file

--- source/core/oxsepavalidator.php
+++ source/core/oxsepavalidator.php
@@ -30,6 +30,76 @@
 class oxSepaValidator
 {
     /**
+     * @var array IBAN Code Length array
+     */
+    protected $_aIBANCodeLengths = array(
+        'AL' => 28,
+        'AD' => 24,
+        'AT' => 20,
+        'AZ' => 28,
+        'BH' => 22,
+        'BE' => 16,
+        'BA' => 20,
+        'BR' => 29,
+        'BG' => 22,
+        'CR' => 21,
+        'HR' => 21,
+        'CY' => 28,
+        'CZ' => 24,
+        'DK' => 18, // Same DENMARK
+        'FO' => 18, // Same DENMARK
+        'GL' => 18, // Same DENMARK
+        'DO' => 28,
+        'EE' => 20,
+        'FI' => 18,
+        'FR' => 27,
+        'GE' => 22,
+        'DE' => 22,
+        'GI' => 23,
+        'GR' => 27,
+        'GT' => 28,
+        'HU' => 28,
+        'IS' => 26,
+        'IE' => 22,
+        'IL' => 23,
+        'IT' => 27,
+        'KZ' => 20,
+        'KW' => 30,
+        'LV' => 21,
+        'LB' => 28,
+        'LI' => 21,
+        'LT' => 20,
+        'LU' => 20,
+        'MK' => 19,
+        'MT' => 31,
+        'MR' => 27,
+        'MU' => 30,
+        'MD' => 24,
+        'MC' => 27,
+        'ME' => 22,
+        'NL' => 18,
+        'NO' => 15,
+        'PK' => 24,
+        'PS' => 29,
+        'PL' => 28,
+        'PT' => 25,
+        'RO' => 24,
+        'SM' => 27,
+        'SA' => 24,
+        'RS' => 22,
+        'SK' => 24,
+        'SI' => 19,
+        'ES' => 24,
+        'SE' => 24,
+        'CH' => 21,
+        'TN' => 24,
+        'TR' => 26,
+        'AE' => 23,
+        'GB' => 22,
+        'VG' => 24
+    );
+
+    /**
      * Business identifier code validation
      *
      * @param string $sBIC code to check
@@ -118,73 +188,5 @@ class oxSepaValidator
         return $this->_aIBANCodeLengths;
     }
 
-    /**
-     * @var array IBAN Code Length array
-     */
-    protected $_aIBANCodeLengths = array(
-        'AL' => 28,
-        'AD' => 24,
-        'AT' => 20,
-        'AZ' => 28,
-        'BH' => 22,
-        'BE' => 16,
-        'BA' => 20,
-        'BR' => 29,
-        'BG' => 22,
-        'CR' => 21,
-        'HR' => 21,
-        'CY' => 28,
-        'CZ' => 24,
-        'DK' => 18, // Same DENMARK
-        'FO' => 18, // Same DENMARK
-        'GL' => 18, // Same DENMARK
-        'DO' => 28,
-        'EE' => 20,
-        'FI' => 18,
-        'FR' => 27,
-        'GE' => 22,
-        'DE' => 22,
-        'GI' => 23,
-        'GR' => 27,
-        'GT' => 28,
-        'HU' => 28,
-        'IS' => 26,
-        'IE' => 22,
-        'IL' => 23,
-        'IT' => 27,
-        'KZ' => 20,
-        'KW' => 30,
-        'LV' => 21,
-        'LB' => 28,
-        'LI' => 21,
-        'LT' => 20,
-        'LU' => 20,
-        'MK' => 19,
-        'MT' => 31,
-        'MR' => 27,
-        'MU' => 30,
-        'MD' => 24,
-        'MC' => 27,
-        'ME' => 22,
-        'NL' => 18,
-        'NO' => 15,
-        'PK' => 24,
-        'PS' => 29,
-        'PL' => 28,
-        'PT' => 25,
-        'RO' => 24,
-        'SM' => 27,
-        'SA' => 24,
-        'RS' => 22,
-        'SK' => 24,
-        'SI' => 19,
-        'ES' => 24,
-        'SE' => 24,
-        'CH' => 21,
-        'TN' => 24,
-        'TR' => 26,
-        'AE' => 23,
-        'GB' => 22,
-        'VG' => 24
-    );
+
 }

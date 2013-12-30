e3ad94506df9aa79c73e93687151d79342f17287
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2013-12-30 10:53:10 +0200

    0005568: oxshopcontrol change (cherry picked from commit 81068b6)

--- tests/unit/views/oxshopcontrolTest.php
+++ tests/unit/views/oxshopcontrolTest.php
@@ -606,4 +606,24 @@ class Unit_Views_oxShopControlTest extends OxidTestCase
     }
 
 
+
+    /**
+     * 0005568: Execution of any private/protected Methods in any Controller by external requests to the shop possible
+     */
+    public function testCannotAccessProtectedMethod()
+    {
+        $sCL = 'Account';
+        $sFNC = '_getLoginTemplate';
+        $oProtectedMethodException = new oxAccessRightException( 'Non public method cannot be accessed' );
+
+        $oView = $this->getMock( $sCL, array( 'executeFunction', 'getFncName' ) );
+        $oView->expects( $this->never() )->method( 'executeFunction' );
+        $oView->expects( $this->once() )->method( 'getFncName' )->will( $this->returnValue( $sFNC ) );
+
+        $oControl = $this->getMock( 'oxShopControl', array( '_initializeViewObject', '_handleAccessRightsException' ) );
+        $oControl->expects( $this->once() )->method( '_initializeViewObject' )->with( $sCL, $sFNC, null, null )->will( $this->returnValue( $oView ) );
+        $oControl->expects( $this->once() )->method( '_handleAccessRightsException' )->with( $oProtectedMethodException )->will( $this->returnValue( true ) );
+
+        $oControl->start( $sCL, $sFNC );
+    }
 }

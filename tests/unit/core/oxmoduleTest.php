0cc74bfcca06dd232f12be7c8d45f115b8dd9a64
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-01-07 14:11:58 +0200

    code beautifying (cherry picked from commit dbb72b4)

--- tests/unit/core/oxmoduleTest.php
+++ tests/unit/core/oxmoduleTest.php
@@ -17,7 +17,7 @@
  *
  * @link      http://www.oxid-esales.com
  * @package   tests
- * @copyright (C) OXID eSales AG 2003-2013
+ * @copyright (C) OXID eSales AG 2003-2014
  * @version OXID eShop CE
  * @version   SVN: $Id: oxcaptchaTest.php 26841 2010-03-25 13:58:15Z arvydas $
  */
@@ -476,19 +476,19 @@ class Unit_Core_oxModuleTest extends OxidTestCase
     public function testActivate()
     {
         $aModulesBefore = array();
-        $aModulesAfter  = array('oxtest' => 'testdir/mytest');
+        $aModulesAfter  = array( 'oxtest' => 'testdir/mytest' );
 
-        $oModule = $this->getProxyClass('oxmodule');
-        $aExtend  = array('extend' => array('oxtest' => 'testdir/mytest'), 'id' => 'test', 'dir' => 'testdir');
+        $oModule = $this->getProxyClass( 'oxmodule' );
+        $aExtend  = array( 'extend' => array( 'oxtest' => 'testdir/mytest' ), 'id' => 'test', 'dir' => 'testdir' );
         $oModule->setNonPublicVar( "_aModule", $aExtend );
 
         //odConfig::getInstance()->setConfigParam( "aModules", $aModulesBefore );
         oxRegistry::getConfig()->setConfigParam( "aModules", $aModulesBefore );
 
-        $this->assertEquals($aModulesBefore, modConfig::getInstance()->getConfigParam("aModules") );
+        $this->assertEquals( $aModulesBefore, modConfig::getInstance()->getConfigParam( "aModules" ) );
 
-        $this->assertTrue($oModule->activate());
-        $this->assertEquals($aModulesAfter, modConfig::getInstance()->getConfigParam("aModules") );
+        $this->assertTrue( $oModule->activate() );
+        $this->assertEquals( $aModulesAfter, modConfig::getInstance()->getConfigParam( "aModules" ) );
     }
 
     /**

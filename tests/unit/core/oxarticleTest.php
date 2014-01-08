e26e0fffff4f65e5431d0ac0e3d8a381a5da14c5
Tomas Liubinas <git@liubinas.com>
2014-01-08 15:36:06 +0200

    Restoring unit test class name.

--- tests/unit/core/oxarticleTest.php
+++ tests/unit/core/oxarticleTest.php
@@ -17,7 +17,7 @@
  *
  * @link      http://www.oxid-esales.com
  * @package   tests
- * @copyright (C) OXID eSales AG 2003-2013
+ * @copyright (C) OXID eSales AG 2003-2014
  * @version OXID eShop CE
  */
 
@@ -145,7 +145,7 @@ class modUtilsObject_oxarticle extends oxUtilsObject
 /**
  * Testing oxArticle class.
  */
-class oxArticleTest extends OxidTestCase
+class Unit_Core_oxArticleTest extends OxidTestCase
 {
     /**
      * Test case for #0003393: getSqlActiveSnippet(true) does not force core table usage

8b5164eae5a3d419f394b98111e204d30f3231c6
l3k <linas.kukulskis@gmail.com>
2014-01-08 15:40:46 +0200

    0004129: metadata.php changes are not committed right to aModules (cherry picked from commit 0348cc9)

--- source/core/oxmodule.php
+++ source/core/oxmodule.php
@@ -417,9 +417,9 @@ class oxModule extends oxSuperCfg
 
         if ( $this->hasMetadata() || $this->hasExtendClass() ) {
 
-            $this->_removeFromDisabledList();
-
             $this->_addExtensions();
+
+            $this->_removeFromDisabledList();
 
                 $this->_addTemplateBlocks( $this->getInfo("blocks") );
 

2fc24e6896f7314916a987d61d2d7d11be647201
shiftas <Shiftass@gmail.com>
2014-01-09 18:07:05 +0200

    Fixing Unit tests Moving everything that's not needed for suite formation to bootstrap, so that it could be used without AllTestsUnit Refactoring how test suite is formatted - scanning for directories instead of relying on hardcoded array. (cherry picked from commit da3a6aa)

--- tests/AllTestsUnit.php
+++ tests/AllTestsUnit.php
@@ -11,46 +11,7 @@
  * @version   SVN: $Id: $
  */
 
-
-    switch (getenv('OXID_VERSION')) {
-        case 'EE':
-            define ('oxCCTempDir', '/tmp/oxCCTempDir_ee/');
-            define('OXID_VERSION_EE', true );
-            define('OXID_VERSION_PE', false);
-            define('OXID_VERSION_PE_PE', false );
-            define('OXID_VERSION_PE_CE', false );
-            break;
-        case 'PE':
-            define ('oxCCTempDir', '/tmp/oxCCTempDir_pe/');
-            define('OXID_VERSION_EE',    false);
-            define('OXID_VERSION_PE',    true );
-            define('OXID_VERSION_PE_PE', true );
-            define('OXID_VERSION_PE_CE', false );
-        break;
-        case 'CE':
-            define ('oxCCTempDir', '/tmp/oxCCTempDir_ce/');
-            define('OXID_VERSION_EE',    false);
-            define('OXID_VERSION_PE',    true );
-            define('OXID_VERSION_PE_PE', false );
-            define('OXID_VERSION_PE_CE', true );
-        break;
-
-        default:
-            die('bad version--- : '."'".getenv('OXID_VERSION')."'");
-        break;
-    }
-
-    define ('oxPATH', getenv('oxPATH'));
-    define ('OXID_VERSION', getenv('OXID_VERSION'));
-    define ('OXID_TEST_UTF8', getenv('OXID_TEST_UTF8'));
-
-    if (!is_dir(oxCCTempDir)) {
-        mkdir(oxCCTempDir, 0777, 1);
-    }
-
 require_once 'PHPUnit/Framework/TestSuite.php';
-error_reporting( (E_ALL ^ E_NOTICE) | E_STRICT );
-ini_set('display_errors', true);
 
 echo "=========\nrunning php version ".phpversion()."\n\n============\n";
 
@@ -59,79 +20,163 @@ echo "=========\nrunning php version ".phpversion()."\n\n============\n";
  */
 class AllTestsUnit extends PHPUnit_Framework_TestCase
 {
+
     /**
-     * Test suite
+     * Default test suites
+     *
+     * @var array
+     */
+    protected static $_aTestSuites = array( 'unit', 'integration' );
+
+    /**
+     * Returns whether to run UTF8 tests
+     *
+     * @return bool
+     */
+    public static function isUtf8()
+    {
+        return getenv('OXID_TEST_UTF8')? true : false;
+                }
+
+    /**
+     * Returns test files filter
+     *
+     * @return string
+     */
+    public static function getTestFileFilter()
+    {
+        $sTestFileNameEnd = '*Test.php';
+        if ( self::isUtf8() ) {
+            $sTestFileNameEnd = '*utf8Test.php';
+            }
+
+        return $sTestFileNameEnd;
+        }
+
+    /**
+     * Returns test files regEx filter
+     *
+     * @return string
+     */
+    public static function getTestFileRegExFilter()
+    {
+        return getenv("PREG_FILTER");
+        }
+
+    /**
+     * Forms test suite
      *
      * @return object
      */
-    static function suite()
+    public static function suite()
     {
-        chdir(dirname(__FILE__));
+        $aTestDirectories = self::_getTestDirectories();
+
         $oSuite = new PHPUnit_Framework_TestSuite( 'PHPUnit' );
-        $sFilter = getenv("PREG_FILTER");
+        foreach ( $aTestDirectories as $sDirectory ) {
+            $sFilesSelector = "$sDirectory/".self::getTestFileFilter();
+            $aTestFiles = glob( $sFilesSelector );
 
-        $aTestSuites = array(
-            'unit' => array( '', 'core', 'maintenance', 'views', 'admin', 'setup', 'components/widgets'  ),
-            'integration' => array( '', 'price', 'timestamp', 'cache' )
-        );
-
-        if (getenv('TEST_DIRS')) {
-            $aTestSuitesDefault = $aTestSuites;
-            $aTestSuites = array();
-            foreach ( explode(',', getenv('TEST_DIRS')) as $sTestSuiteParts ) {
-                list( $sSuiteKey, $sSuiteTests ) = explode(':', $sTestSuiteParts);
-                if ( strpos($sSuiteKey, '%')) {
-                    $aTestSuites[ 'unit' ] = explode('%', $sSuiteKey);
-                } else {
-                    $aTestSuites[ $sSuiteKey ] = empty( $sSuiteTests )? $aTestSuitesDefault[ $sSuiteKey ] : explode('%', $sSuiteTests);
-                }
-            }
-        }
-
-        $sTestFileNameEnd = 'Test.php';
-        if ( getenv('OXID_TEST_UTF8') ) {
-            $sTestFileNameEnd = 'utf8Test.php';
-        }
-
-        foreach ( $aTestSuites as $sTestSuiteDir => $aTestDirs ) {
-        foreach ($aTestDirs as $sTestDir ) {
-
-            if ($sTestDir == '_root_') {
-                $sTestDir = '';
+            if ( empty( $aTestFiles ) ) {
+                continue;
             }
 
-                $sDir = rtrim($sTestSuiteDir.'/'.$sTestDir, '/');
+            echo "Adding unit tests from $sFilesSelector\n";
 
-                echo "Searching for $sDir\n";
-                //adding UNIT Tests
-                if (!is_dir($sDir)) {
-                    continue;
+            $oSuite = self::_addFilesToSuite( $oSuite, $aTestFiles );
                 }
-                echo "Adding unit tests from $sDir/*{$sTestFileNameEnd}\n";
-                foreach ( glob( "$sDir/*".$sTestFileNameEnd ) as $sFilename) {
 
-                    if ( !getenv('OXID_TEST_UTF8') && strpos( $sFilename, 'utf8Test.php' ) !== false ) {
-                        continue;
+        return $oSuite;
                     }
 
-                    if (!$sFilter || preg_match("&$sFilter&i", $sFilename)) {
-                        error_reporting( (E_ALL ^ E_NOTICE) | E_STRICT );
-                        ini_set('display_errors', true);
+    /**
+     * Adds files to test suite
+     *
+     * @param $oSuite
+     * @param $aTestFiles
+     * @throws Exception
+     */
+    protected static function _addFilesToSuite( $oSuite, $aTestFiles )
+    {
+        foreach ( $aTestFiles as $sFilename ) {
+
+            $sFilter = self::getTestFileRegExFilter();
+            if ( !$sFilter || preg_match("&$sFilter&i", $sFilename) ) {
+
                         include_once $sFilename;
                         $sClassName = str_replace( array( "/", ".php" ), array( "_", "" ), $sFilename );
 
                         if ( class_exists( $sClassName ) ) {
                             $oSuite->addTestSuite( $sClassName );
                         } else {
-                            echo "\n\nWarning: class not found: $sClassName in $sFilename\n\n\n ";
+                    if ( !isset( $blThrowException ) || $blThrowException ) {
+                        echo "\n\nFile with wrong class name found!: $sClassName in $sFilename";
+                        exit();
                         }
-                    } else {
-                        echo "skiping $sFilename\n";
                     }
                 }
             }
+        return $oSuite;
         }
 
-        return $oSuite;
+    /**
+     * Returns array of directories, which should be tested
+     *
+     * @return array
+     */
+    protected static function _getTestDirectories()
+    {
+        $aTestSuites = self::$_aTestSuites;
+
+        if ( getenv('TEST_DIRS') ) {
+            $aTestSuites = array();
+            foreach ( explode(',', getenv('TEST_DIRS')) as $sTestSuiteParts ) {
+                $aTestSuites = array_merge( $aTestSuites, self::_getSuiteDirectories( $sTestSuiteParts ) );
+            }
     }
+
+        return self::_getDirectoryTree( $aTestSuites );
+    }
+
+    /**
+     * Returns test suite directories
+     *
+     * @param $sTestSuiteParts
+     * @return array
+     */
+    protected static function _getSuiteDirectories( $sTestSuiteParts )
+    {
+        $aDirectories = array();
+
+        list( $sSuiteKey, $sSuiteTests ) = explode(':', $sTestSuiteParts);
+        if ( empty( $sSuiteTests ) ) {
+            foreach ( explode('%', $sSuiteTests) as $sSubDirectory ) {
+                $aDirectories[] = "$sSuiteKey/$sSubDirectory";
+            }
+        } else {
+            $aDirectories[] = $sSuiteKey;
+        }
+        return $aDirectories;
+    }
+
+    /**
+     * Scans given tests directories and returns formed directory tree
+     *
+     * @param array $aDirectories
+     * @return array
+     */
+    protected static function _getDirectoryTree( $aDirectories )
+    {
+        $aTree = array();
+
+        foreach ( $aDirectories as $sDirectory ) {
+            $aTree = array_merge( $aTree, array_diff( glob( $sDirectory . "/*", GLOB_ONLYDIR ), array('.', '..') ) );
+        }
+
+        if ( !empty( $aTree ) ) {
+            $aTree = array_merge( $aTree, self::_getDirectoryTree( $aTree ) );
+        }
+        return $aTree;
+    }
+
 }

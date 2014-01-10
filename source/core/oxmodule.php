fa133da9c9910479ea882be2ff2e52ccea07ee35
Mantas Vaitkunas <manvait@gmail.com>
2014-01-10 17:15:23 +0200

    0004424: There should be possibility to extend same class more than one time in one module.

--- source/core/oxmodule.php
+++ source/core/oxmodule.php
@@ -318,33 +318,35 @@ class oxModule extends oxSuperCfg
     {
         $blActive = false;
         $sId = $this->getId();
-        if (isset($sId)) {
+        if ( isset( $sId ) ) {
             if ( $this->hasExtendClass() ) {
                 $aAddModules = $this->getExtensions();
                 $aInstalledModules = $this->getModulesWithExtendedClass();
-                $iClCount = count($aAddModules);
+                $iClCount = count( $aAddModules );
                 $iActive  = 0;
 
-                foreach ( $aAddModules as $sClass => $sModule ) {
-                    if ( is_array( $sModule ) ) {
-                        $aExtendedClasses = isset( $aInstalledModules[$sClass] ) ? $aInstalledModules[$sClass] : array();
-                        if ( count( array_diff( $aExtendedClasses, $sModule ) ) == 0 ) {
+                foreach ( $aAddModules as $sClass => $mModule ) {
+                    if ( is_array( $mModule ) ) {
+                        foreach ( $mModule as $sModule ) {
+                            if ( ( isset( $aInstalledModules[$sClass] ) && in_array( $sModule, $aInstalledModules[$sClass] ) ) ) {
                             $iActive++;
+                                break;
                         }
-                    } elseif ( ( isset( $aInstalledModules[$sClass] ) && in_array( $sModule, $aInstalledModules[$sClass] ) ) ) {
+                        }
+                    } elseif ( ( isset( $aInstalledModules[$sClass] ) && in_array( $mModule, $aInstalledModules[$sClass] ) ) ) {
                         $iActive++;
                     }
                 }
                 $blActive = $iClCount > 0 && $iActive == $iClCount;
 
                 $aDisabledModules = $this->getDisabledModules();
-                if ( $blActive && ( is_array($aDisabledModules) && in_array($sId, $aDisabledModules) ) ) {
+                if ( $blActive && ( is_array( $aDisabledModules ) && in_array( $sId, $aDisabledModules ) ) ) {
                     $blActive = false;
                 }
             } else {
                 //handling modules that does not extend any class
                 $aDisabledModules = $this->getDisabledModules();
-                if ( is_array($aDisabledModules) && !in_array($sId, $aDisabledModules) ) {
+                if ( is_array( $aDisabledModules ) && !in_array( $sId, $aDisabledModules ) ) {
                     $blActive = true;
                 }
             }

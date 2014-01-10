179acb748e60fca266536b955879428e241a8cd6
shiftas <Shiftass@gmail.com>
2014-01-10 11:39:59 +0200

    If TEST_DIRS subdirectory is specified as _root_, add root directory to the test suite (cherry picked from commit 11a64b8)

--- tests/AllTestsUnit.php
+++ tests/AllTestsUnit.php
@@ -131,10 +131,8 @@ class AllTestsUnit extends PHPUnit_Framework_TestCase
         list( $sSuiteKey, $sSuiteTests ) = explode(':', $sTestSuiteParts);
         if ( !empty( $sSuiteTests ) ) {
             foreach ( explode('%', $sSuiteTests) as $sSubDirectory ) {
-                if ( $sSubDirectory == "_root_") {
-                    $sSubDirectory = '';
-                }
-                $aDirectories[] = "$sSuiteKey/$sSubDirectory";
+                $sSubDirectory = ( $sSubDirectory == "_root_")? "" : '/'.$sSubDirectory;
+                $aDirectories[] = "${sSuiteKey}${sSubDirectory}";
             }
         } else {
             $aDirectories[] = $sSuiteKey;

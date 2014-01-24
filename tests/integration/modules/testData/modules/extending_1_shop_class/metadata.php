16c2c9ef1eef8d85d157e9717953e69091f7f11b
tomasly <tomas.lygutas@oxid-esales.com>
2014-01-24 09:35:55 +0200

    ESDEV-1070 Write module fictions

---
+++ tests/integration/modules/testData/modules/extending_1_shop_class/metadata.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *    This file is part of OXID eShop Community Edition.
+ *
+ *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OXID eShop Community Edition is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   main
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version OXID eShop CE
+ * @version   SVN: $Id: metadata.php 25466 2010-02-01 14:12:07Z alfonsas $
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'           => 'extending_1_shop_class',
+    'title'        => 'Test extending 1 shop class',
+    'description'  => 'Module testing extending 1 shop class',
+    'thumbnail'    => 'picture.png',
+    'version'      => '1.0',
+    'author'       => 'OXID eSales AG',
+    'extend'       => array(
+        'oxorder' => 'extending_1_shop_class/myorder'
+    )
+);
---
+++ tests/integration/modules/testData/modules/extending_3_shop_classes/metadata.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *    This file is part of OXID eShop Community Edition.
+ *
+ *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    OXID eShop Community Edition is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @package   main
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version OXID eShop CE
+ * @version   SVN: $Id: metadata.php 25466 2010-02-01 14:12:07Z alfonsas $
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'           => 'extending_3_shop_classes',
+    'title'        => 'Test extending 3 shop classes',
+    'description'  => 'Module testing extending 3 shop classes',
+    'thumbnail'    => 'picture.png',
+    'version'      => '1.0',
+    'author'       => 'OXID eSales AG',
+    'extend'       => array(
+        'oxarticle' => 'extending_1_shop_class/myarticle',
+        'oxorder' => 'extending_1_shop_class/myorder',
+        'oxuser' => 'extending_1_shop_class/myuser'
+    )
+);

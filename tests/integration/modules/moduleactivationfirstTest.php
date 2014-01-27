40a375ae9eefd3f1a87052192d6635251e1bc43d
l3k <linas.kukulskis@gmail.com>
2014-01-27 18:05:09 +0200

    added first test case

--- tests/integration/modules/moduleactivationfirstTest.php
+++ tests/integration/modules/moduleactivationfirstTest.php
@@ -23,6 +23,7 @@
  */
 
 require_once realpath(dirname(__FILE__).'/../../') . '/unit/OxidTestCase.php';
+require_once realpath( dirname(__FILE__) ) . '/environmentvalidator.php';
 require_once realpath( dirname(__FILE__) ) . '/environment.php';
 
 class Integration_Modules_ModuleActivationFirstTest extends OxidTestCase
@@ -38,34 +39,94 @@ class Integration_Modules_ModuleActivationFirstTest extends OxidTestCase
     }
 
 
-    public function providerModuleDeactivation()
+    public function providerModuleActivation()
     {
         return array(
             array(
-                array( 'extending_1_class', 'with_2_templates', 'with_everything' ),
-                'with_everything',
+
+                // modules to be activated during test preparation
                 array(
-                    'blocks' => '',
-                    'extend' => '',
-                    'files' => '',
+                    'extending_1_class', 'with_2_templates', 'with_2_files',
+                    'extending_3_blocks', 'with_events',
+                ),
+
+                // module that will be activated
+                'with_everything',
+
+                // environment asserts
+                array(
+                    'blocks' => array(
+                        array('template' => 'page/checkout/basket.tpl',  'block'=>'basket_btn_next_top',    'file'=>'/views/blocks/page/checkout/myexpresscheckout.tpl'),
+                        array('template' => 'page/checkout/basket.tpl',  'block'=>'basket_btn_next_bottom', 'file'=>'/views/blocks/page/checkout/myexpresscheckout.tpl'),
+                        array('template' => 'page/checkout/payment.tpl', 'block'=>'select_payment',         'file'=>'/views/blocks/page/checkout/mypaymentselector.tpl'),
+                        array('template' => 'page/checkout/basket.tpl',  'block'=>'basket_btn_next_top',    'file'=>'/views/blocks/page/checkout/myexpresscheckout.tpl'),
+                        array('template' => 'page/checkout/payment.tpl', 'block'=>'select_payment',         'file'=>'/views/blocks/page/checkout/mypaymentselector.tpl'),
+                    ),
+                    'extend' => array(
+                        'oxorder' => 'extending_1_class/myorder&with_everything/myorder1&with_everything/myorder2&with_everything/myorder3',
+                        'oxarticle' => 'with_everything/myarticle',
+                        'oxuser' => 'with_everything/myuser',
+                    ),
+                    'files' => array(
+                        'with_2_files' => array(
+                            'myexception'  => 'with_2_files/core/exception/myexception.php',
+                            'myconnection' => 'with_2_files/core/exception/myconnection.php',
+                        ),
+                        'with_everything' => array(
+                            'myexception'  => 'with_everything/core/exception/myexception.php',
+                            'myconnection' => 'with_everything/core/exception/myconnection.php',
+                        )
+                    ),
                     'settings' => array(
                         array('group' => 'my_checkconfirm', 'name' => 'blCheckConfirm', 'type' => 'bool', 'value' => 'true'),
                         array('group' => 'my_displayname',  'name' => 'sDisplayName',   'type' => 'str',  'value' => 'Some name'),
                     ),
-                    'templates' => '',
+                    'disabledModules' => array(
+                    ),
+                    'templates' => array(
+                        'with_2_templates' => array(
+                            'order_special.tpl'      => 'with_2_templates/views/admin/tpl/order_special.tpl',
+                            'user_connections.tpl'   => 'with_2_templates/views/tpl/user_connections.tpl',
+                        ),
+                        'with_everything' => array(
+                            'order_special.tpl'      => 'with_everything/views/admin/tpl/order_special.tpl',
+                            'user_connections.tpl'   => 'with_everything/views/tpl/user_connections.tpl',
+                        ),
+                    ),
+                    'versions' => array(
+                        'extending_1_class' => '1.0',
+                        'with_2_templates' => '1.0',
+                        'with_2_files' => '1.0',
+                        'extending_3_blocks' => '1.0',
+                        'with_events' => '1.0',
+                        'with_everything' => '1.0',
+                    ),
+                    'events' => array(
+                        'extending_1_class' => null,
+                        'with_2_templates' => null,
+                        'with_2_files' => null,
+                        'extending_3_blocks' => null,
+                        'with_events' => array(
+                            'onActivate'   => 'MyEvents::onActivate',
+                            'onDeactivate' => 'MyEvents::onDeactivate'
+                        ),
+                        'with_everything' => array(
+                            'onActivate'   => 'MyEvents::onActivate',
+                            'onDeactivate' => 'MyEvents::onDeactivate'
+                        ),
+                    ),
                 )
-            ),
+            )
         );
     }
 
     /**
      * Tests if module was activated.
      *
-     * @dataProvider providerModuleDeactivation
+     * @dataProvider providerModuleActivation
      */
     public function testModuleActivation( $aInstallModules, $sModule, $aResultToAsserts )
     {
-        $this->markTestSkipped( 'Development in progress...' );
         $oModuleEnvironment = new Environment();
         $oModuleEnvironment->prepare( $aInstallModules );
 
@@ -76,47 +137,43 @@ class Integration_Modules_ModuleActivationFirstTest extends OxidTestCase
         $this->_runAsserts( $aResultToAsserts, $sModule );
     }
 
-    private function _runAsserts( $aAsserts, $sModule )
-    {
-        /*//There are no templates removing functionality, after implementation uncomment;
-        $this->_assertTemplates( $aAsserts['templates'], $sModule ); */
-        $this->_assertBlocks( $aAsserts['blocks'] );
-        /*$this->_assertBlocks();
-        $this->_assertBlocks();*/
-    }
-
-    private function _assertTemplates( $aTemplates, $sModule )
-    {
-        $aTemplatesToCheck = $this->getConfig()->getConfigParam( 'aModuleTemplates' );
-
-        $this->assertSame( $aTemplates, $aTemplatesToCheck[$sModule] );
-    }
-
-    private function _assertBlocks()
-    {
-
-    }
-
-    private function _assertExtensions()
-    {
-        $aModules = $this->getConfig()->getConfigParam( 'aModules' );
-    }
-
-
-    private function _assertFiles()
-    {
-        $aModuleFiles = $this->getConfig()->getConfigParam( 'aModuleFiles' );
-
-    }
-
     /**
-     * @param $aSettings
+     * Runs all asserts
+     *
+     * @param $aExpectedResult
+     * @param $sModuleId
      */
-    private function _assertConfigs( $aSettings )
+    private function _runAsserts( $aExpectedResult, $sModuleId )
     {
-        $oConfig = oxConfig::getInstance();
-        foreach( $aSettings as $sKey => $sVal ) {
-            $this->assertSame( $sVal, $oConfig->getConfigParam( $sKey ) );
+        $oValidator = new EnvironmentValidator();
+        $oValidator->setConfig( $this->getConfig() );
+
+        if( isset( $aExpectedResult['blocks'] ) ){
+            $this->assertTrue( $oValidator->checkBlocks( $aExpectedResult['blocks']), 'Blocks do not match expectations' );
+        }
+
+        if( isset( $aExpectedResult['extend'] ) ){
+            $this->assertTrue( $oValidator->checkExtensions( $aExpectedResult['extend']), 'Extensions do not match expectations' );
+        }
+
+        if( isset( $aExpectedResult['files'] ) ){
+            $this->assertTrue( $oValidator->checkFiles( $aExpectedResult['files']), 'Files do not match expectations' );
+        }
+
+        if( isset( $aExpectedResult['events'] ) ){
+            $this->assertTrue( $oValidator->checkEvents( $aExpectedResult['events']), 'Events do not match expectations' );
+        }
+
+        if( isset( $aExpectedResult['settings'] ) ){
+            $this->assertTrue( $oValidator->checkConfigs( $aExpectedResult['settings'], $sModuleId), 'Configs do not match expectations' );
+        }
+
+        if( isset( $aExpectedResult['versions'] ) ){
+            $this->assertTrue( $oValidator->checkVersions( $aExpectedResult['versions']), 'Versions do not match expectations' );
+        }
+
+        if( isset( $aExpectedResult['templates'] ) ){
+            $this->assertTrue( $oValidator->checkTemplates( $aExpectedResult['templates']), 'Templates do not match expectations' );
         }
     }
 

a68a2a0518a749b2a3c672003ef3503ca9cda6a5
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-02-06 16:13:18 +0200

    0005635: new line parameters causes HTTP request splitting convert new line char to html code (cherry picked from commit 9547452)

--- source/core/oxconfig.php
+++ source/core/oxconfig.php
@@ -803,7 +803,7 @@ class oxConfig extends oxSuperCfg
             $sValue = $newValue;
         } elseif ( is_string( $sValue ) ) {
             $sValue = str_replace( array( '&',     '<',    '>',    '"',      "'",      chr(0), '\\', "\n", "\r" ),
-                                   array( '&amp;', '&lt;', '&gt;', '&quot;', '&#039;', '',     '&#092;', '%0A', '%0D' ),
+                                   array( '&amp;', '&lt;', '&gt;', '&quot;', '&#039;', '', '&#092;', '&#10;', '&#13;' ),
                                    $sValue );
         }
         return $sValue;

0e943c207cb9d3159393914c5fec2747013f76b2
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-02-14 12:35:06 +0200

    0005635: new line parameters causes HTTP request splitting (cherry picked from commit 530127f) (cherry picked from commit 794e09d)

--- source/application/controllers/details.php
+++ source/application/controllers/details.php
@@ -1015,7 +1015,7 @@ class Details extends oxUBase
      */
     public function getTag()
     {
-        return oxRegistry::getConfig()->getRequestParameter("searchtag", false);
+        return oxConfig::getParameter("searchtag");
     }
 
     /**

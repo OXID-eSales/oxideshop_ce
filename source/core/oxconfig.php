cbdc0a1e7fec8de1d0a712f6f3347f479d606de7
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-02-14 12:34:10 +0200

    0005635: new line parameters causes HTTP request splitting (cherry picked from commit 00009d2) (cherry picked from commit c2a6983)

--- source/core/oxconfig.php
+++ source/core/oxconfig.php
@@ -791,8 +791,8 @@ class oxConfig extends oxSuperCfg
             $sValue = $newValue;
         } elseif (is_string($sValue)) {
             $sValue = str_replace(
-                array('&', '<', '>', '"', "'", chr(0), '\\'),
-                array('&amp;', '&lt;', '&gt;', '&quot;', '&#039;', '', '&#092;'),
+                array('&', '<', '>', '"', "'", chr(0), '\\', "\n", "\r" ),
+                array( '&amp;', '&lt;', '&gt;', '&quot;', '&#039;', '', '&#092;', '%0A', '%0D'),
                 $sValue
             );
         }

cef2010160d1c1542a62cde1e982bcbb5394a544
l3k <linas.kukulskis@gmail.com>
2014-03-07 09:34:44 +0200

    0004622: Intangible product should not be calculated in shipping price. (cherry picked from commit e9fbf2f)

--- source/application/models/oxdelivery.php
+++ source/application/models/oxdelivery.php
@@ -216,17 +216,16 @@ class oxDelivery extends oxI18n
         $dAmount  = 0;
         $oProduct = $oBasketItem->getArticle( false );
 
+        $blExclNonMaterial = $this->getConfig()->getConfigParam( 'blExclNonMaterialFromDelivery' );
+
         // mark free shipping products
-        if ( $oProduct->oxarticles__oxfreeshipping->value ) {
+        if ( $oProduct->oxarticles__oxfreeshipping->value || ($oProduct->oxarticles__oxnonmaterial->value && $blExclNonMaterial) ) {
             if ($this->_blFreeShipping !== false) {
                 $this->_blFreeShipping = true;
             }
         } else {
 
-            $blExclNonMaterial = $this->getConfig()->getConfigParam( 'blExclNonMaterialFromDelivery' );
-            if ( !( $oProduct->oxarticles__oxnonmaterial->value && $blExclNonMaterial ) ) {
-                $this->_blFreeShipping = false;
-            }
+            $this->_blFreeShipping = false;
 
             switch ( $this->oxdelivery__oxdeltype->value ) {
                 case 'p': // price

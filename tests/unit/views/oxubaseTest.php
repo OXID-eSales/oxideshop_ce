63a880f7f77f21a45748b528aa0a55523c6c0c35
l3k <linas.kukulskis@gmail.com>
2014-03-21 17:58:57 +0200

    0004847: duplicated title / description tags in product detail page concerned by Google webmaster tools: getPageTitle

--- tests/unit/views/oxubaseTest.php
+++ tests/unit/views/oxubaseTest.php
@@ -2497,14 +2497,14 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
     {
         return array(
                 array( array( 'prefix' => 'Prefix', 'title' => 'Title', 'suffix' => 'Suffix', 'pageSuffix' => 'PageSuffix' ), 'Prefix | Title | Suffix | PageSuffix' ),
-                array( array( 'prefix' => 'Prefix', 'title' => 'Title', 'suffix' => 'Suffix' ), 'Prefix | Title | Suffix' ),
-                array( array( 'prefix' => 'Prefix', 'suffix' => 'Suffix' ), 'Prefix | Suffix' ),
-                array( array( 'title' => 'Title', 'suffix' => 'Suffix' ), 'Title | Suffix' ),
-                array( array( 'prefix' => 'Prefix', 'title' => 'Title'), 'Prefix | Title' ),
-                array( array( 'title' => 'Title' ), 'Title' ),
-                array( array(), '' ),
-                array( array( 'prefix' => 'Prefix', 'title' => null), 'Prefix' ),
-                array( array( 'prefix' => 'Prefix', 'title' => false), 'Prefix' ),
+                array( array( 'prefix' => 'Prefix', 'title' => 'Title', 'suffix' => 'Suffix', 'pageSuffix' => '' ), 'Prefix | Title | Suffix' ),
+                array( array( 'prefix' => 'Prefix', 'title' => '', 'suffix' => 'Suffix', 'pageSuffix' => '' ), 'Prefix | Suffix' ),
+                array( array( 'prefix' => '', 'title' => 'Title', 'suffix' => 'Suffix', 'pageSuffix' => '' ), 'Title | Suffix' ),
+                array( array( 'prefix' => 'Prefix', 'title' => 'Title', 'suffix' => '', 'pageSuffix' => '' ), 'Prefix | Title' ),
+                array( array( 'prefix' => '', 'title' => 'Title', 'suffix' => '', 'pageSuffix' => '' ), 'Title' ),
+                array( array( 'prefix' => '', 'title' => '', 'suffix' => '', 'pageSuffix' => ''), '' ),
+                array( array( 'prefix' => 'Prefix', 'title' => null, 'suffix' => '', 'pageSuffix' => '' ), 'Prefix' ),
+                array( array( 'prefix' => 'Prefix', 'title' => false, 'suffix' => '', 'pageSuffix' => '' ), 'Prefix' ),
         );
     }
 

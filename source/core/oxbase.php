0ed22b3b4fba4c10c58ff9403e8f8aecc86efd9d
Andrius Šilgalis <andrius.silgalis@gmail.com>
2014-04-08 15:15:22 +0300

    Some methods are EE specific.

--- source/core/oxbase.php
+++ source/core/oxbase.php
@@ -1372,12 +1372,6 @@ class oxBase extends oxSuperCfg
 
         $blRet = (bool) $oDb->execute( $sInsert );
 
-        //sets newly createde map id to object
-        $iInsertId = $oDb->Insert_ID();
-        if ($iInsertId) {
-            $this->setMapId($iInsertId);
-        }
-
 
         return $blRet;
     }

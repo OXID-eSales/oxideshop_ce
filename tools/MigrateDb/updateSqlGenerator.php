0339a0f24f5b979eb3568e1216cc994a7af1e72c
Andrius Šilgalis <andrius.silgalis@gmail.com>
2014-04-25 15:13:18 +0300

    ESDEV-1689 Correction in comments to file names.

--- tools/MigrateDb/updateSqlGenerator.php
+++ tools/MigrateDb/updateSqlGenerator.php
@@ -8,8 +8,8 @@
  * USAGE:
  * - manually set $NUMBER_OF_SUBSHOPS constant to required number of shops
  * - run this script:
- *      php 2-updateSqlGenerator.php
- * - your result file 3-migrate.sql is ready
+ *      php updateSqlGenerator.php
+ * - your result file 2-migrate.sql is ready
  *
  * @copyright (c)2014 OXID eSales AG
  */

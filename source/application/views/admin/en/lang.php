4b3982299e73ccf1b7fbd29c072de4ebe85505bf
Jürgen Busch <juergen.busch@oxid-esales.com>
2014-06-13 10:47:21 +0300

    Corrections in SHOP_LICENSE_ERROR_*

--- source/application/views/admin/en/lang.php
+++ source/application/views/admin/en/lang.php
@@ -1856,11 +1856,11 @@ $aLang = array(
 'serial_updated'                                           => 'License key has been updated',
 
 // Shop licensing problems
-'SHOP_LICENSE_ERROR_GRACE_EXPIRED'                         => 'The license key of your OXID eShop has been finally expired. Please contact the Technical Support.',
+'SHOP_LICENSE_ERROR_GRACE_EXPIRED'                         => 'The license key of your OXID eShop finally expired. Please contact the Technical Support.',
 'SHOP_LICENSE_ERROR_GRACE_WILL_EXPIRE'                     => 'The license key of your OXID eShop is expired. OXID eShop will go offline within 24 hours. Please enter a valid license key or contact the Technical Support!',
-'SHOP_LICENSE_ERROR_shop_unlicensed'                       => 'A problem with the license key of your OXID eShop has been occurred. For this reason, a hidden countdown of 7 days has been started. After this period the OXID eShop will go offline. Please enter the valid license key once again or contact the Technical Support!',
-'SHOP_LICENSE_ERROR_serial_expired'                        => 'The license key of your OXID eShop has been expired. For this reason, a hidden countdown of 7 days has been started. After this period the OXID eShop will go offline. Please enter a valid license key or contact the Technical Support!',
-'SHOP_LICENSE_ERROR_incorrect_mandate_amount'              => 'The number of the licensed sub-shops has been exceeded. For this reason, a hidden countdown of 7 days has been started. After this period the OXID eShop will go offline. Please enter a valid license key  or contact the Technical Support!',
+'SHOP_LICENSE_ERROR_shop_unlicensed'                       => 'A problem with the license key of your OXID eShop occurred. That\'s why a hidden countdown of 7 days was started. After this period OXID eShop will go offline. Please enter the valid license key once again or contact the technical support!',
+'SHOP_LICENSE_ERROR_serial_expired'                        => 'The license key of your OXID eShop expired. That\'s why a hidden countdown of 7 days was started. After this period OXID eShop will go offline. Please enter a valid license key or contact the technical support!',
+'SHOP_LICENSE_ERROR_incorrect_mandate_amount'              => 'The number of the licensed sub-shops exceeded. That\'s why a hidden countdown of 7 days was started. After this period OXID eShop will go offline. Please enter a valid license key or contact the technical support!',
 'SHOP_LICENSE_ERROR_INFORMATION'                           => 'OXID eShop license information',
 
 'SHOP_OPTIONS_GROUP_GLOBAL'                                => 'Global',

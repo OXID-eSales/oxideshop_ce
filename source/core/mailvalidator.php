928b29c3df31cb6ca1df1633b6ce380eabb90175
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-06-17 12:32:28 +0300

    email validation class add

---
+++ source/core/mailvalidator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+class MailValidator
+{
+    /**
+     * User email validation function. Returns true if email is OK otherwise - false;
+     * Syntax validation is performed only.
+     *
+     * @param string $sEmail user email
+     *
+     * @return bool
+     */
+    public function isValidEmail( $sEmail )
+    {
+        $blValid = true;
+        if ( $sEmail != 'admin' ) {
+            $sEmailTpl = "/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/i";
+            $blValid = ( getStr()->preg_match( $sEmailTpl, $sEmail ) != 0 );
+        }
+
+        return $blValid;
+    }
+}
---
+++ tests/unit/core/mailvalidatorTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+require_once realpath( "." ).'/unit/OxidTestCase.php';
+require_once realpath( "." ).'/unit/test_config.inc.php';
+
+class Unit_Core_mailValidatorTest extends OxidTestCase
+{
+    public function providerValidateEmailWithValidEmail()
+    {
+        return array(
+            array('mathias.krieck@oxid-esales.com'),
+            array('mytest@com.org'),
+        );
+    }
+
+    /**
+     * @param $sEmail email to validate.
+     * @dataProvider providerValidateEmailWithValidEmail
+     */
+    public function testValidateEmailWithValidEmail($sEmail)
+    {
+        $oMailValidator = oxNew('MailValidator');
+        $this->assertTrue( $oMailValidator->isValidEmail($sEmail), 'Mail '. $sEmail .' validation failed.'  );
+    }
+
+    public function providerValidateEmailWithNotValidEmail()
+    {
+        return array(
+            array('�mathias.krieck@oxid-esales.com'),
+            array('my/test@com.org'),
+            array('@com.org'),
+            array('mytestcom.org'),
+            array('mytest@com'),
+        );
+    }
+    /**
+     * @param $sEmail email to validate.
+     * @dataProvider providerValidateEmailWithNotValidEmail
+     */
+    public function testValidateEmailWithNotValidEmail($sEmail)
+    {
+        $oMailValidator = oxNew('MailValidator');
+        $this->assertFalse($oMailValidator->isValidEmail($sEmail));
+    }
+}

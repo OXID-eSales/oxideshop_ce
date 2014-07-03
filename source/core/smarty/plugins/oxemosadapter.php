b6ac8f181d6940d0e7e22bf06f81f26a7faead5d
shiftas <Shiftass@gmail.com>
2014-07-03 14:52:01 +0300

    #5704: Econda tracking of user registrations is faulty.
    Creating pages contents and order step names for later usage in getCode() method instead of using strings inside the method.

--- source/core/smarty/plugins/oxemosadapter.php
+++ source/core/smarty/plugins/oxemosadapter.php
@@ -67,6 +67,69 @@ class oxEmosAdapter extends oxSuperCfg
     protected $_oEmos = null;
 
     /**
+     * Emos pages content
+     *
+     * @var array
+     */
+    protected $_aPagesContent = array(
+        'start' => 'Start',
+        'basket' => 'Shop/Kaufprozess/Warenkorb',
+        'user' => 'Shop/Kaufprozess/Kundendaten',
+        'user_1' => 'Shop/Kaufprozess/Kundendaten/OhneReg',
+        'user_2' => 'Shop/Kaufprozess/Kundendaten/BereitsKunde',
+        'user_3' => 'Shop/Kaufprozess/Kundendaten/NeuesKonto',
+        'payment' => 'Shop/Kaufprozess/Zahlungsoptionen',
+        'order' => 'Shop/Kaufprozess/Bestelluebersicht',
+        'thankyou' => 'Shop/Kaufprozess/Bestaetigung',
+        'search' => 'Shop/Suche',
+        'account_wishlist' => 'Service/Wunschzettel',
+        'contact_success' => 'Service/Kontakt/Success',
+        'contact_failure' => 'Service/Kontakt/Form',
+        'help' => 'Service/Hilfe',
+        'newsletter_success' => 'Service/Newsletter/Success',
+        'newsletter_failure' => 'Service/Newsletter/Form',
+        'guestbook' => 'Service/Gaestebuch',
+        'links' => 'Service/Links',
+        'info_impressum.tpl' => 'Info/Impressum',
+        'info_agb.tpl' => 'Info/AGB',
+        'info_order_info.tpl' => 'Info/Bestellinfo',
+        'info_delivery_info.tpl' => 'Info/Versandinfo',
+        'info_security_info.tpl' => 'Info/Sicherheit',
+        'account_login' => 'Login/Uebersicht',
+        'account_logout' => 'Login/Formular/Logout',
+        'account_needlogin' => 'Login/Formular/Login',
+        'account_user' => 'Login/Kundendaten',
+        'account_order' => 'Login/Bestellungen',
+        'account_noticelist' => 'Login/Merkzettel',
+        'account_newsletter' => 'Login/Newsletter',
+        'account_whishlist' => 'Login/Wunschzettel',
+        'forgotpassword' => 'Login/PW vergessen',
+        'content_oximpressum' => 'Info/Impressum',
+        'content_oxagb' => 'Info/AGB',
+        'content_oxorderinfo' => 'Info/Bestellinfo',
+        'content_oxdeliveryinfo' => 'Info/Versandinfo',
+        'content_oxsecurityinfo' => 'Info/Sicherheit',
+        'register' => 'Service/Register',
+    );
+
+    /**
+     * Emos order step names
+     *
+     * @var array
+     */
+    protected $_aOrderSteps = array(
+        'basket' => '1_Warenkorb',
+        'order_process' => '2_Kundendaten',
+        'user' => '2_Kundendaten',
+        'user_1' => '2_Kundendaten/OhneReg',
+        'user_2' => '2_Kundendaten/BereitsKunde',
+        'user_3' => '2_Kundendaten/NeuesKonto',
+        'payment' => '3_Zahlungsoptionen',
+        'order' => '4_Bestelluebersicht',
+        'thankyou' => '5_Bestaetigung',
+    );
+
+    /**
      * oxEmosAdapter class instance.
      *
      * @var oxEmosAdapter instance

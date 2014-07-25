f7597b59287d62d75aafe2494f973fec4ebb3a03
l3k <linas.kukulskis@gmail.com>
2014-07-25 10:40:46 +0300

    0005806: Online VAT ID check doesn't work: vat in class

---
+++ source/application/models/oxcompanyvatin.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+
+/**
+ * Company VAT identification number (VATIN)
+ */
+class oxCompanyVatIn
+{
+    /**
+     * VATIN
+     *
+     * @var string
+     */
+    private $_sCompanyVatNumber = '';
+
+    /**
+     * Constructor
+     *
+     * @param string $sCompanyVatNumber - company vat identification number.
+     */
+    function __construct( $sCompanyVatNumber )
+    {
+        $this->_sCompanyVatNumber = $sCompanyVatNumber;
+    }
+
+    /**
+     * Returns country code from number.
+     *
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return (string) substr( $this->_sCompanyVatNumber, 0, 2 );
+    }
+
+    /**
+     * Cast to string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->_sCompanyVatNumber;
+    }
+}

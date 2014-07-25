359193a8f2730c7c9688999982efedda1610d0eb
l3k <linas.kukulskis@gmail.com>
2014-07-25 11:27:30 +0300

    0005806: Online VAT ID check doesn't work: country checker

---
+++ tests/unit/core/oxcompanyvatincountrycheckerTest.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+require_once realpath( "." ).'/unit/OxidTestCase.php';
+require_once realpath( "." ).'/unit/test_config.inc.php';
+
+
+class Unit_Core_oxCompanyVatInCountryCheckerTest extends OxidTestCase
+{
+    public function testGetCountry_set()
+    {
+        $oChecker = new oxCompanyVatInCountryChecker();
+        $oCountry = new oxCountry();
+
+        $oChecker->setCountry($oCountry);
+        $this->assertSame($oCountry, $oChecker->getCountry() );
+    }
+
+    public function testGetCountry_empty()
+    {
+        $oChecker = new oxCompanyVatInCountryChecker();
+        $this->assertNull( $oChecker->getCountry() );
+    }
+
+    public function testValidate_countryNotSet()
+    {
+        $oChecker = new oxCompanyVatInCountryChecker();
+        $oVatIn = new oxCompanyVatIn('DE1234');
+
+        $this->assertFalse( $oChecker->validate($oVatIn) );
+    }
+
+    /**
+     * @dataProvider validateDataProvider
+     */
+    public function testValidate( $sCountryCode, $sVatIn, $blExpectValidationResult )
+    {
+        $oVatIn = new oxCompanyVatIn($sVatIn);
+        $oCountry = new oxCountry();
+        $oCountry->oxcountry__oxvatinprefix = new oxField( $sCountryCode );
+
+        $oChecker = new oxCompanyVatInCountryChecker();
+        $oChecker->setCountry($oCountry);
+
+        $this->assertSame($blExpectValidationResult, $oChecker->validate($oVatIn) );
+    }
+
+    public function validateDataProvider()
+    {
+        return array(
+            array('LT', 'LT12345', true),
+            array('LT', '', false),
+            array('LT', '11', false),
+            array('LT', 'ab', false),
+            array('DE', 'LT12345', false),
+        );
+    }
+
+    public function testValidate_notValid_errorMessage()
+    {
+        $oVatIn = new oxCompanyVatIn('LT12345');
+        $oCountry = new oxCountry();
+        $oCountry->oxcountry__oxvatinprefix = new oxField( 'DE' );
+
+        $oChecker = new oxCompanyVatInCountryChecker();
+        $oChecker->setCountry($oCountry);
+        $oChecker->validate($oVatIn);
+
+        $this->assertSame(oxCompanyVatInCountryChecker::ERROR_DIFFERENT_COUNTRY, $oChecker->getError());
+    }
+
+    public function testValidate_valid_noErrorMessage()
+    {
+        $oVatIn = new oxCompanyVatIn('LT12345');
+        $oCountry = new oxCountry();
+        $oCountry->oxcountry__oxvatinprefix = new oxField( 'LT' );
+
+        $oChecker = new oxCompanyVatInCountryChecker();
+        $oChecker->setCountry($oCountry);
+        $oChecker->validate($oVatIn);
+
+        $this->assertSame('', $oChecker->getError());
+    }
+}

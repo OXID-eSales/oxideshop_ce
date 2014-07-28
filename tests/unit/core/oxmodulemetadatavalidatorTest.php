02bb689f4ab3493f6ac22c2dbac2c1da67ae1a97
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-07-28 13:26:40 +0300

    ESDEV-2156 Create metadata validator
    test if validation fails when module metadata file has php syntax error

--- tests/unit/core/oxmodulemetadatavalidatorTest.php
+++ tests/unit/core/oxmodulemetadatavalidatorTest.php
@@ -52,4 +52,25 @@ class Unit_Core_oxModuleMetadataValidatorTest extends OxidTestCase
         $oMetadataValidator->setModule($oModule);
         $this->assertSame(false, $oMetadataValidator->validate());
     }
+
+    public function testValidateModuleWithInvalidMetadataFile()
+    {
+        $sMetadataFileName = 'metadata.php';
+        $sMetadataContent = '<?php php syntax error';
+
+        $sPathToMetadata = $this->createFile($sMetadataFileName, $sMetadataContent);
+
+        $oModuleStub = $this->getMock('oxModule', array('getMetadataPath'));
+        $oModuleStub->expects($this->any())
+            ->method('getMetadataPath')
+            ->will($this->returnValue($sPathToMetadata));
+
+        /** @var oxModule $oModule */
+        $oModule = $oModuleStub;
+
+        $oMetadataValidator = new oxModuleMetadataValidator();
+        $oMetadataValidator->setModule($oModule);
+
+        $this->assertSame(false, $oMetadataValidator->validate());
+    }
 }

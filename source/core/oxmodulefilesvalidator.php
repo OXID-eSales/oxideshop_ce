7b57d24b133a50601e82f6bdc25c4b75cdd6ac98
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-08-11 16:35:44 +0300

    ESDEV-2156 Create metadata validator  oxModuleFilesValidator added to check if all module files exists

--- source/core/oxmodulefilesvalidator.php
+++ source/core/oxmodulefilesvalidator.php
@@ -20,39 +20,27 @@
  * @version   OXID eShop CE
  */
 
-class oxModuleFilesValidator implements oxIModuleValidator
+class oxModuleFilesValidator implements oxIModuleMetadataValidator
 {
     /**
-     * Missing module files list.
-     *
-     * @var array
+     * @var oxModule
      */
-    private $_aMissingFiles = array();
+    private $_oModule = null;
 
     /**
-     * Shop directory where modules are stored.
-     *
-     * @var string
+     * @return oxModule
      */
-    private $_sPathToModuleDirectory = null;
-
-    /**
-     * @return string
-     */
-    public function getPathToModuleDirectory()
+    public function getModule()
     {
-        if (is_null($this->_sPathToModuleDirectory)) {
-            $this->setPathToModuleDirectory(oxRegistry::getConfig()->getModulesDir());
-        }
-        return $this->_sPathToModuleDirectory;
+        return $this->_oModule;
     }
 
     /**
-     * @param string $sPathToModuleDirectory
+     * @param oxModule $oModule
      */
-    public function setPathToModuleDirectory($sPathToModuleDirectory)
+    public function setModule($oModule)
     {
-        $this->_sPathToModuleDirectory = $sPathToModuleDirectory;
+        $this->_oModule = $oModule;
     }
 
     /**
@@ -60,106 +48,10 @@ class oxModuleFilesValidator implements oxIModuleValidator
      * Return true if module files exists.
      * Return false if at least one module file does not exist.
      *
-     * @param oxModule $oModule object to validate metadata.
-     *
      * @return bool
      */
-    public function validate(oxModule $oModule)
+    public function validate()
     {
-        $this->_resetMissingFiles();
-        $blModuleValid = $this->_allModuleExtensionsExists($oModule);
-        $blModuleValid = $this->_allModuleFilesExists($oModule) && $blModuleValid;
-
-        return $blModuleValid;
-    }
-
-    /**
-     * Get missing files which result to invalid module.
-     *
-     * @return array
-     */
-    public function getMissingFiles()
-    {
-        return $this->_aMissingFiles;
-    }
-
-    /**
-     * Resets missing files array.
-     */
-    protected function _resetMissingFiles()
-    {
-        $this->_aMissingFiles = array();
-    }
-
-    /**
-     * Return true if all module files which extends shop class exists.
-     *
-     * @param oxModule $oModule object to validate metadata.
-     *
-     * @return bool
-     */
-    protected function _allModuleExtensionsExists($oModule)
-    {
-        $aModuleExtendedFiles = $oModule->getExtensions();
-        $blAllModuleExtensionsExists = $this->_allFilesExists($aModuleExtendedFiles, true, 'extensions');
-        return $blAllModuleExtensionsExists;
-    }
-
-    /**
-     * Return true if all module independent PHP files exist.
-     *
-     * @param oxModule $oModule object to validate metadata.
-     *
-     * @return mixed
-     */
-    protected function _allModuleFilesExists($oModule)
-    {
-        $aModuleExtendedFiles = $oModule->getFiles();
-        $blAllModuleFilesExists = $this->_allFilesExists($aModuleExtendedFiles);
-        return $blAllModuleFilesExists;
-
-    }
-
-    /**
-     * Return true if all requested file exists.
-     *
-     * @param array $aModuleExtendedFiles of files which must exist.
-     * @param bool $blAddExtension if add .php extension to checked files.
-     * @param string $sListName if add .php extension to checked files.
-     *
-     * @return bool
-     */
-    private function _allFilesExists($aModuleExtendedFiles, $blAddExtension = false, $sListName = 'files')
-    {
-        $blAllModuleFilesExists = true;
-        foreach ($aModuleExtendedFiles as $sModuleName => $sModulePath) {
-            $sPathToModuleDirectory = $this->getPathToModuleDirectory();
-            $sPathToModuleDirectory = $this->_addDirectorySeparatorAtTheEndIfNeeded($sPathToModuleDirectory);
-            $sExtPath = $sPathToModuleDirectory . $sModulePath;
-            if ($blAddExtension) {
-                $sExtPath .= '.php';
-            }
-            if (!file_exists($sExtPath)) {
-                $blAllModuleFilesExists = false;
-                $this->_aMissingFiles[$sListName][$sModuleName] = $sModulePath;
-            }
-        }
-        return $blAllModuleFilesExists;
-    }
-
-    /**
-     * Check if path has directory separator at the end. Add it if needed.
-     *
-     * @param $sPathToModuleDirectory
-     *
-     * @return string
-     */
-    private function _addDirectorySeparatorAtTheEndIfNeeded($sPathToModuleDirectory)
-    {
-        if (substr($sPathToModuleDirectory, -1) != DIRECTORY_SEPARATOR) {
-            $sPathToModuleDirectory .= DIRECTORY_SEPARATOR;
-            return $sPathToModuleDirectory;
-        }
-        return $sPathToModuleDirectory;
+        return false;
     }
 }

e9520dafaf95e32679c849c35b5df16e6086dc26
shiftas <Shiftass@gmail.com>
2014-08-11 12:04:51 +0300

    ESDEV-2199 Captcha formatting, user payment key, specific string manipulation improvement
    Adding encryptor class.

---
+++ tests/unit/core/oxencryptorTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+/**
+ * Class Unit_Core_oxEncryptorTest
+ */
+class Unit_Core_oxEncryptorTest extends OxidTestCase
+{
+
+    public function providerEncodingAndDecoding()
+    {
+        return array(
+            array('testString', '', 'ox_MCcrOiwrDCstNjE4Njs!'),
+            array('testString', 1, 'ox_XidFOkIrYitDNl84WDs!'),
+            array('testString', 'testKey', 'ox_Gx0HETgRKisGDB0TIgE!'),
+        );
+    }
+
+    /**
+     * @dataProvider providerEncodingAndDecoding
+     */
+    public function testEncodingAndDecoding($sString, $sKey, $sEncodedString)
+    {
+        $oEncryptor = new oxEncryptor();
+
+        $this->assertSame($sEncodedString, $oEncryptor->encrypt($sString, $sKey));
+    }
+}

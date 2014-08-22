ce71ca190caa0d5bd35e315e871f27a1df1f285c
mantasvaitkunas <manvait@gmail.com>
2014-08-22 11:38:13 +0300

    ESDEV-2262 Added oxservernode and oxservernodechecker to CE

---
+++ source/core/oxservernode.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+/**
+ * Class oxServerNode used as entity for server node information.
+ *
+ * @internal Do not make a module extension for this class.
+ * @see http://wiki.oxidforge.org/Tutorials/Core_OXID_eShop_classes:_must_not_be_extended
+ */
+class oxServerNode
+{
+    /**
+     * @var string
+     */
+    private $_sId;
+
+    /**
+     * @var string
+     */
+    private $_sIp;
+
+    /**
+     * @var int
+     */
+    private $_iTimestamp;
+
+    /**
+     * Flag which stores timestamp.
+     *
+     * @var int
+     */
+    private $_iLastFrontendUsage;
+
+    /**
+     * Flag which stores timestamp.
+     *
+     * @var int
+     */
+    private $_iLastAdminUsage;
+
+    /**
+     * @param string $sId
+     */
+    public function setId($sId)
+    {
+        $this->_sId = $sId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->_sId;
+    }
+
+    /**
+     * @param string $sIp
+     */
+    public function setIp($sIp)
+    {
+        $this->_sIp = $sIp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->_sIp;
+    }
+
+    /**
+     * @param int $iTimestamp
+     */
+    public function setTimestamp($iTimestamp)
+    {
+        $this->_iTimestamp = $iTimestamp;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimestamp()
+    {
+        return $this->_iTimestamp;
+    }
+
+    /**
+     * @param int|null $iLastAdminUsage
+     */
+    public function setLastAdminUsage($iLastAdminUsage)
+    {
+        $this->_iLastAdminUsage = $iLastAdminUsage;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getLastAdminUsage()
+    {
+        return $this->_iLastAdminUsage;
+    }
+
+    /**
+     * @param int|null $iLastFrontendUsage Admin server flag which stores timestamp.
+     */
+    public function setLastFrontendUsage($iLastFrontendUsage)
+    {
+        $this->_iLastFrontendUsage = $iLastFrontendUsage;
+    }
+
+    /**
+     * @return int|null Frontend server flag which stores timestamp.
+     */
+    public function getLastFrontendUsage()
+    {
+        return $this->_iLastFrontendUsage;
+    }
+}
---
+++ tests/unit/core/oxservernodecheckerTest.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+/**
+ * Class Unit_Core_oxServerNodeCheckerTest
+ *
+ * @covers oxServerNodeChecker
+ */
+class Unit_Core_oxServerNodeCheckerTest extends OxidTestCase
+{
+    public function tearDown()
+    {
+        parent::tearDown();
+        $oUtilsDate = new oxUtilsDate();
+        oxRegistry::set('oxUtilsDate', $oUtilsDate);
+    }
+
+    public function providerCheckWhenNodeIsValid()
+    {
+        $iNodeCreationTime = 1400000000;
+        $iWhenNodeIsStillValid = $iNodeCreationTime + 22 * 60 * 60;
+
+        return array(
+            // When node time is the same as current time.
+            array($iNodeCreationTime),
+            // When server node is still valid.
+            array($iWhenNodeIsStillValid)
+        );
+    }
+
+    /**
+     * @param $iCurrentTime
+     *
+     * @dataProvider providerCheckWhenNodeIsValid
+     */
+    public function testCheckWhenNodeIsValid($iCurrentTime)
+    {
+        $this->_prepareCurrentTime($iCurrentTime);
+        $oServerNodeChecker = new oxServerNodeChecker();
+
+        $this->assertTrue($oServerNodeChecker->check($this->_getMockedNode()), 'Server node must be valid.');
+    }
+
+    public function providerCheckIfNodeIsNotValid()
+    {
+        $iNodeCreationTime = 1400000000;
+        $iExactTimeWhenNodeIsNotValid = $iNodeCreationTime + 23 * 60 * 60;
+        $iTimeWhenServerNodeIsNotValid = $iNodeCreationTime + 28 * 60 * 60;
+        $iServerTimeIsSmallerThanNodeTime = $iNodeCreationTime - 1;
+
+        return array(
+            // Exact time when server node is not valid.
+            array($iExactTimeWhenNodeIsNotValid),
+            // Time when node is not valid.
+            array($iTimeWhenServerNodeIsNotValid),
+            // When server time is smaller then node time.
+            array($iServerTimeIsSmallerThanNodeTime),
+        );
+    }
+
+    /**
+     * @param $iCurrentTime
+     *
+     * @dataProvider providerCheckIfNodeIsNotValid
+     */
+    public function testCheckIfNodeIsNotValid($iCurrentTime)
+    {
+        $this->_prepareCurrentTime($iCurrentTime);
+        $oServerNodeChecker = new oxServerNodeChecker();
+
+        $this->assertFalse($oServerNodeChecker->check($this->_getMockedNode()), 'Server node must be not valid.');
+    }
+
+    public function testCheckWhenNodeDoesNotReturnTimestamp()
+    {
+        $oServerNodeChecker = new oxServerNodeChecker();
+        /** @var oxServerNode $oNode */
+        $oNode = $this->getMock('oxServerNode', array('getTimestamp'));
+        $oNode->expects($this->any())->method('getTimestamp')->will($this->returnValue(null));
+
+        $this->assertFalse($oServerNodeChecker->check($oNode), 'Server node must be not valid when returns timestamp null.');
+    }
+
+    /**
+     * @param int $iCurrentTime
+     */
+    private function _prepareCurrentTime($iCurrentTime)
+    {
+        $oUtilsDate = $this->getMock('oxUtilsDate', array('getTime'));
+        $oUtilsDate->expects($this->any())->method('getTime')->will($this->returnValue($iCurrentTime));
+        /** @var oxUtilsDate $oUtils */
+        oxRegistry::set('oxUtilsDate', $oUtilsDate);
+    }
+
+    /**
+     * @return oxServerNode
+     */
+    private function _getMockedNode()
+    {
+        /** @var oxServerNode $oNode */
+        $oNode = $this->getMock('oxServerNode', array('getTimestamp'));
+        $oNode->expects($this->any())->method('getTimestamp')->will($this->returnValue(1400000000));
+
+        return $oNode;
+    }
+}

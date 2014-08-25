c2a642541ed7d48bb0f3dbd9b0e99d3c868aca8e
shiftas <Shiftass@gmail.com>
2014-08-25 10:40:55 +0300

    ESDEV-2264 ID for whole client cluster.
    Adding universally unique id generator.

--- tests/unit/core/oxuniversallyuniqueidgeneratorTest.php
+++ tests/unit/core/oxuniversallyuniqueidgeneratorTest.php
@@ -27,16 +27,124 @@
 class Unit_Core_oxUniversallyUniqueIdGeneratorTest extends OxidTestCase
 {
 
-    public function testUUIDGeneration()
+    public function testUUIDUniqueness()
     {
-        $oChecked = $this->getMock('oxOpenSSLFunctionalityChecker', array('isOpenSslRandomBytesGeneratorAvailable'));
-        $oChecked->expects($this->any())->method('isOpenSslRandomBytesGeneratorAvailable')->will($this->returnValue(false));
-        $oGenerator = new oxUniversallyUniqueIdGenerator($oChecked);
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+
         $aIds = array();
-        for ($i=0; $i<10; $i++) {
+        for ($i=0; $i<100; $i++) {
             $aIds[] = $oGenerator->generate();
         }
 
-        $this->assertEquals(count($aIds), count(array_unique($aIds)));
+        $this->assertEquals(100, count(array_unique($aIds)));
+    }
+
+    /**
+     * Generated UUID should consist of 5 word character groups separated by dashes.
+     */
+    public function testUUIDStructure()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+        $sId = $oGenerator->generate();
+
+        $this->assertRegExp('/^[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}$/', $sId);
+    }
+
+    public function testUUIDV4Uniqueness()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+
+        $aIds = array();
+        for ($i=0; $i<100; $i++) {
+            $aIds[] = $oGenerator->generateV4();
+        }
+
+        $this->assertEquals(100, count(array_unique($aIds)));
+    }
+
+    /**
+     * V4 UUID can be generated based either on openSSL or mt_rand.
+     * mt_rand is used when openSSL is not available.
+     */
+    public function testUUIDV4UniquenessWhenInFallbackMode()
+    {
+        $oCheckerMock = $this->getMock('oxOpenSSLFunctionalityChecker', array('isOpenSslRandomBytesGeneratorAvailable'));
+        $oCheckerMock->expects($this->any())->method('isOpenSslRandomBytesGeneratorAvailable')->will($this->returnValue(false));
+        /** @var oxOpenSSLFunctionalityChecker $oChecker */
+        $oChecker = $oCheckerMock;
+
+        $oGenerator = new oxUniversallyUniqueIdGenerator($oChecker);
+
+        $aIds = array();
+        for ($i=0; $i<100; $i++) {
+            $aIds[] = $oGenerator->generateV4();
+        }
+
+        $this->assertEquals(100, count(array_unique($aIds)));
+    }
+
+    /**
+     * Generated v4 UUID should consist of 5 word character groups separated by dashes.
+     */
+    public function testUUIDV4Structure()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+        $sId = $oGenerator->generateV4();
+
+        $this->assertRegExp('/^[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}$/', $sId);
+    }
+
+    /**
+     * Generating UUID v5 with different salt should yield different result.
+     */
+    public function testUUIDV5UniquenessWithDifferentSalt()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+
+        $aIds = array();
+        for ($i=0; $i<100; $i++) {
+            $aIds[] = $oGenerator->generateV5('seed', 'salt'.$i);
+        }
+
+        $this->assertEquals(100, count(array_unique($aIds)));
+    }
+
+    /**
+     * Generating UUID v5 with different seed should yield different result.
+     */
+    public function testUUIDV5UniquenessWithDifferentSeed()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+
+        $aIds = array();
+        for ($i=0; $i<100; $i++) {
+            $aIds[] = $oGenerator->generateV5('seed'.$i, 'salt');
+        }
+
+        $this->assertEquals(100, count(array_unique($aIds)));
+    }
+
+    /**
+     * Generating UUID v5 with same seed and salt should yield same result.
+     */
+    public function testUUIDV5EqualityWithSameSeedAndSalt()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+
+        $sId1 = $oGenerator->generateV5('seed', 'salt');
+        $sId2 = $oGenerator->generateV5('seed', 'salt');
+
+        $this->assertEquals($sId1, $sId2);
+    }
+
+    /**
+     * Generated v5 UUID should consist of 5 word character groups separated by dashes.
+     */
+    public function testUUIDV5Structure()
+    {
+        $oGenerator = new oxUniversallyUniqueIdGenerator();
+        $sId = $oGenerator->generateV5('seed', 'salt');
+
+        $this->assertRegExp('/^[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}$/', $sId);
     }
 }

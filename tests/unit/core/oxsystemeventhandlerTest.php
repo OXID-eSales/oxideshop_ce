304d007d95e4fbc401ab0b2e062c61f0dae0a05a
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-08-26 10:24:25 +0300

    test method name changed

--- tests/unit/core/oxsystemeventhandlerTest.php
+++ tests/unit/core/oxsystemeventhandlerTest.php
@@ -30,7 +30,7 @@ require_once realpath( "." ).'/unit/test_config.inc.php';
  */
 class Unit_Core_oxSystemEventHandlerTest extends OxidTestCase
 {
-    public function testOnAdminLoginOMVN()
+    public function testOnAdminLoginOnlineModuleVersionNotifier()
     {
         $oSystemEventHandler = new oxSystemEventHandler();
 

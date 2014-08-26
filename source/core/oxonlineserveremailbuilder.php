204730d72ddd686f16858bae950923fc237f6333
mantasvaitkunas <manvait@gmail.com>
2014-08-26 14:49:53 +0300

    ESDEV-2267 Send email if normal request does not succeed
    Added email builder

---
+++ source/core/oxonlineserveremailbuilder.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+/**
+ * Class oxOnlineServerEmailBuilder is responsible for email sending when it's not possible to make call via CURL.
+ *
+ * @internal Do not make a module extension for this class.
+ * @see http://www.oxid-forge.com/do_not_extend_classes_list/
+ *
+ * @ignore This class will not be included in documentation.
+ */
+class oxOnlineServerEmailBuilder
+{
+    /**
+     * Created oxEmail object and sets values.
+     *
+     * @param string $sBody
+     *
+     * @return oxEmail
+     */
+    public function build($sBody)
+    {
+        /** @var oxEmail $oExpirationEmail */
+        $oExpirationEmail = oxNew('oxEmail');
+        $oExpirationEmail->setSubject(oxRegistry::getLang()->translateString('SUBJECT_UNABLE_TO_SEND_VIA_CURL', null, true));
+        $oExpirationEmail->setRecipient('info@oxid-esales.com');
+        $oExpirationEmail->setFrom($this->_getShopInfoAddress());
+        $oExpirationEmail->setBody($sBody);
+
+        return $oExpirationEmail;
+    }
+
+    /**
+     * Returns active shop info email address.
+     *
+     * @return string
+     */
+    private function _getShopInfoAddress()
+    {
+        $oShop = oxRegistry::getConfig()->getActiveShop();
+
+        return $oShop->oxshops__oxinfoemail->value;
+    }
+}
---
+++ tests/unit/core/oxonlineserveremailbuilderTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+require_once realpath( "." ).'/unit/OxidTestCase.php';
+
+/**
+ * Class Unit_Core_oxOnlineServerEmailBuilderTest
+ */
+class Unit_Core_oxOnlineServerEmailBuilderTest extends OxidTestCase
+{
+    public function testBuildIfParametersWereSetCorrectly()
+    {
+        $sBody = '_testXML';
+        $oExpirationEmailBuilder = new oxOnlineServerEmailBuilder();
+        $oExpirationEmail = $oExpirationEmailBuilder->build($sBody);
+        $aRecipient = $oExpirationEmail->getRecipient();
+
+        $this->assertSame($sBody, $oExpirationEmail->getBody(), 'Email content is not as it should be.');
+        $this->assertSame('info@oxid-esales.com', $aRecipient[0][0], 'Recipient email address is wrong.');
+        $this->assertSame(oxRegistry::getLang()->translateString('SUBJECT_UNABLE_TO_SEND_VIA_CURL', null, true), $oExpirationEmail->getSubject(), 'Subject is wrong.');
+    }
+}

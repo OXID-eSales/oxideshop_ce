0e6e4508bf1d4adc8912d703f9e2525e0194cbc5
l3k <linas.kukulskis@gmail.com>
2014-08-27 16:12:57 +0300

    ESDEV-2292 Clean old oxOnlineModuleVersionNotifier
    headers

--- source/core/oxonlinelicensecheckresponse.php
+++ source/core/oxonlinelicensecheckresponse.php
@@ -21,7 +21,12 @@
  */
 
 /**
- * Class oxOnlineLicenseCheckRequest
+ * Online license check response class
+ *
+ * @internal Do not make a module extension for this class.
+ * @see http://www.oxid-forge.com/do_not_extend_classes_list/
+ *
+ * @ignore This class will not be included in documentation.
  */
 class oxOnlineLicenseCheckResponse
 {

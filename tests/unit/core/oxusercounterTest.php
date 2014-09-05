59be0e43126a3138868fa93ecf059b8b76c1370a
shiftas <Shiftass@gmail.com>
2014-09-05 12:46:36 +0300

    ESDEV-2371 Add new information for license sending: subshop and admin users amount ESDEV-2386 Create oxUserCounter
    Creating basic oxUserCounter class. Currently only to see interface.

---
+++ tests/unit/core/oxusercounterTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
+ * @version   OXID eShop CE
+ */
+
+class Unit_Core_oxuserbasketTest extends OxidTestCase
+{
+    public function testCountingAdmins()
+    {
+        $oCounter = new oxUserCounter();
+        $this->assertEquals(6, $oCounter->getAdminCount());
+    }
+    public function testCountingMallAdmins()
+    {
+        $oCounter = new oxUserCounter();
+        $this->assertEquals(6, $oCounter->getMallAdminCount());
+    }
+
+    public function testCountingSubShopAdmins()
+    {
+        $oCounter = new oxUserCounter();
+        $this->assertEquals(0, $oCounter->getSubShopAdminCount());
+    }
+
+    public function testCountingUsersByGroup()
+    {
+        $oCounter = new oxUserCounter();
+        $this->assertEquals(6, $oCounter->getUserCountByGroup('malladmin'));
+    }
+}

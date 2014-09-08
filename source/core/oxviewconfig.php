06f7625d000fd1155d9fed42e8b9d58736880ef8
mantasvaitkunas <manvait@gmail.com>
2014-09-08 15:01:30 +0300

    ESDEV-2352 Retrieve session challenge token from session in oxViewConfig. (cherry picked from commit ea4a1fe)

--- source/core/oxviewconfig.php
+++ source/core/oxviewconfig.php
@@ -1424,4 +1424,45 @@ class oxViewConfig extends oxSuperCfg
         return $this->_sActiveTheme;
     }
 
+    /**
+     * Returns shop logo image file name from config option
+     *
+     * @return string
+     */
+    public function getShopLogo()
+    {
+        if (is_null($this->_sShopLogo)) {
+
+            $sLogoImage = $this->getConfig()->getConfigParam('sShopLogo');
+            if (empty($sLogoImage)) {
+                $sLogoImage = "logo.png";
+            }
+
+            $this->setShopLogo($sLogoImage);
+        }
+
+        return $this->_sShopLogo;
+    }
+
+    /**
+     * Sets shop logo
+     *
+     * @param string $sLogo shop logo image file name
+     *
+     * @return null
+     */
+    public function setShopLogo($sLogo)
+    {
+        $this->_sShopLogo = $sLogo;
+    }
+
+    /**
+     * retrieve session challenge token from session
+     *
+     * @return string
+     */
+    public function getSessionChallengeToken()
+    {
+        return $this->getSession()->getSessionChallengeToken();
+    }
 }

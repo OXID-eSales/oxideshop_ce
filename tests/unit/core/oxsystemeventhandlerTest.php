ad4ea3b01f19d982b940c40fd51ad22fb74ee994
stasiukaitis-saulius <stasiukaitis.saulius@gmail.com>
2014-09-09 10:42:38 +0300

    bootstrap includes needed files for tests

--- tests/unit/core/oxsystemeventhandlerTest.php
+++ tests/unit/core/oxsystemeventhandlerTest.php
@@ -20,9 +20,6 @@
  * @version   OXID eShop CE
  */
 
-require_once realpath( "." ).'/unit/OxidTestCase.php';
-require_once realpath( "." ).'/unit/test_config.inc.php';
-
 /**
  * @covers oxSystemEventHandler
  */

2734573141bfcd663eb1fa6ecf516d946d41e3da
Tomas Liubinas <git@liubinas.com>
2014-09-10 18:21:54 +0300

    Applying coding standards - SpaceAfterOpenBracket

--- source/setup/tpl/dbinfo.php
+++ source/setup/tpl/dbinfo.php
@@ -22,73 +22,92 @@
 require "_header.php"; ?>
 <?php
 $this->getText('STEP_3_DESC');
-$aDB = $this->getViewParam( "aDB" );
-$blMbStringOn = $this->getViewParam( "blMbStringOn" );
-$blUnicodeSupport = $this->getViewParam( "blUnicodeSupport" );
+$aDB = $this->getViewParam("aDB");
+$blMbStringOn = $this->getViewParam("blMbStringOn");
+$blUnicodeSupport = $this->getViewParam("blUnicodeSupport");
 
 $sChecked = '';
-if ( ( !isset( $aDB['iUtfMode'] ) || $aDB['iUtfMode'] == 1 ) && $blMbStringOn > 1 && $blUnicodeSupport > 1 ) {
+if ((!isset($aDB['iUtfMode']) || $aDB['iUtfMode'] == 1) && $blMbStringOn > 1 && $blUnicodeSupport > 1) {
     $sChecked = 'checked';
 }
-$sDisabled = ( $blMbStringOn > 1 && $blUnicodeSupport > 1 ) ? '' : 'disabled';
+$sDisabled = ($blMbStringOn > 1 && $blUnicodeSupport > 1) ? '' : 'disabled';
 ?><br>
-<br>
-<form action="index.php" method="post">
-<input type="hidden" name="istep" value="<?php $this->getSetupStep( 'STEP_DB_CONNECT' ); ?>">
+    <br>
+    <form action="index.php" method="post">
+        <input type="hidden" name="istep" value="<?php $this->getSetupStep('STEP_DB_CONNECT'); ?>">
 
-<table cellpadding="0" cellspacing="5" border="0">
-  <tr>
-    <td><?php $this->getText('STEP_3_DB_HOSTNAME'); ?>:</td>
-    <td>&nbsp;&nbsp;<input size="40" name="aDB[dbHost]" class="editinput" value="<?php echo( $aDB['dbHost']);?>"> </td>
-  </tr>
-  <tr>
-    <td><?php $this->getText('STEP_3_DB_DATABSE_NAME'); ?>:</td>
-    <td>&nbsp;&nbsp;<input size="40" name="aDB[dbName]" class="editinput" value="<?php echo( $aDB['dbName']);?>"><br>&nbsp;&nbsp;(<?php $this->getText('STEP_3_CREATE_DB_WHEN_NO_DB_FOUND'); ?>)</td>
-  </tr>
-  <tr>
-    <td><?php $this->getText('STEP_3_DB_USER_NAME'); ?>:</td>
-    <td>&nbsp;&nbsp;<input size="40" name="aDB[dbUser]" class="editinput" value="<?php echo( $aDB['dbUser']);?>"> </td>
-  </tr>
-  <tr>
-    <td><?php $this->getText('STEP_3_DB_PASSWORD'); ?>:</td>
-    <td>
-        &nbsp;&nbsp;<input size="40" name="aDB[dbPwd]" id="sDbPass" class="editinput" type="password" value="<?php echo( $aDB['dbPwd']);?>"><input size="40" name="aDB[dbPwd]" id="sDbPassPlain" class="editinput" type="text" disabled="disabled" style="display:none">
-        <input type="checkbox" id="sDbPassCheckbox" onClick="JavaScript:changeField();"><?php $this->getText('STEP_3_DB_PASSWORD_SHOW'); ?>
-    </td>
-  </tr>
-  <tr>
-    <td><?php $this->getText('STEP_3_DB_DEMODATA'); ?>:</td>
-    <td>
-        &nbsp;&nbsp;<input type="radio" name="aDB[dbiDemoData]" value="1" <?php if( $aDB['dbiDemoData'] == 1) echo( "checked"); ?>><?php $this->getText('BUTTON_RADIO_INSTALL_DB_DEMO'); ?><br>
-        &nbsp;&nbsp;<input type="radio" name="aDB[dbiDemoData]" value="0" <?php if( $aDB['dbiDemoData'] == 0) echo( "checked"); ?>><?php $this->getText('BUTTON_RADIO_NOT_INSTALL_DB_DEMO'); ?><br>
-    </td>
-  </tr>
-  <tr>
-    <td><?php $this->getText('STEP_3_UTFMODE'); ?>:</td>
-    <td>
-        &nbsp;&nbsp;<input type="checkbox" name="aDB[iUtfMode]" value="1" <?php echo $sChecked; echo $sDisabled; ?>>
-        <?php
-            if ( $blMbStringOn > 1 && $blUnicodeSupport > 1 ) {
-                $this->getText('STEP_3_UTFINFO');
-            } else {
-                $this->getText('STEP_3_UTFNOTSUPPORTED');
-                if ( $blMbStringOn < 2 ) {
-                    $this->getText('STEP_3_UTFNOTSUPPORTED1');
-                }
-                if ( ($blMbStringOn + $blUnicodeSupport) == 2) {
-                    echo ",";
-                }
-                if ( $blUnicodeSupport < 2 ) {
-                    $this->getText('STEP_3_UTFNOTSUPPORTED2');
-                }
-                echo ".";
-            }
-        ?>
-        <br>
-    </td>
-  </tr>
-</table>
-<input type="hidden" name="sid" value="<?php $this->getSid(); ?>">
-<input type="submit" id="step3Submit" class="edittext" value="<?php $this->getText('BUTTON_DB_INSTALL'); ?>">
-</form>
+        <table cellpadding="0" cellspacing="5" border="0">
+            <tr>
+                <td><?php $this->getText('STEP_3_DB_HOSTNAME'); ?>:</td>
+                <td>&nbsp;&nbsp;<input size="40" name="aDB[dbHost]" class="editinput"
+                                       value="<?php echo($aDB['dbHost']); ?>"></td>
+            </tr>
+            <tr>
+                <td><?php $this->getText('STEP_3_DB_DATABSE_NAME'); ?>:</td>
+                <td>&nbsp;&nbsp;<input size="40" name="aDB[dbName]" class="editinput"
+                                       value="<?php echo($aDB['dbName']); ?>"><br>&nbsp;&nbsp;(<?php $this->getText('STEP_3_CREATE_DB_WHEN_NO_DB_FOUND'); ?>
+                    )
+                </td>
+            </tr>
+            <tr>
+                <td><?php $this->getText('STEP_3_DB_USER_NAME'); ?>:</td>
+                <td>&nbsp;&nbsp;<input size="40" name="aDB[dbUser]" class="editinput"
+                                       value="<?php echo($aDB['dbUser']); ?>"></td>
+            </tr>
+            <tr>
+                <td><?php $this->getText('STEP_3_DB_PASSWORD'); ?>:</td>
+                <td>
+                    &nbsp;&nbsp;<input size="40" name="aDB[dbPwd]" id="sDbPass" class="editinput" type="password"
+                                       value="<?php echo($aDB['dbPwd']); ?>"><input size="40" name="aDB[dbPwd]"
+                                                                                    id="sDbPassPlain" class="editinput"
+                                                                                    type="text" disabled="disabled"
+                                                                                    style="display:none">
+                    <input type="checkbox" id="sDbPassCheckbox"
+                           onClick="JavaScript:changeField();"><?php $this->getText('STEP_3_DB_PASSWORD_SHOW'); ?>
+                </td>
+            </tr>
+            <tr>
+                <td><?php $this->getText('STEP_3_DB_DEMODATA'); ?>:</td>
+                <td>
+                    &nbsp;&nbsp;<input type="radio" name="aDB[dbiDemoData]"
+                                       value="1" <?php if ($aDB['dbiDemoData'] == 1) {
+                        echo("checked");
+                    } ?>><?php $this->getText('BUTTON_RADIO_INSTALL_DB_DEMO'); ?>
+                    <br>
+                    &nbsp;&nbsp;<input type="radio" name="aDB[dbiDemoData]"
+                                       value="0" <?php if ($aDB['dbiDemoData'] == 0) {
+                        echo("checked");
+                    } ?>><?php $this->getText('BUTTON_RADIO_NOT_INSTALL_DB_DEMO'); ?>
+                    <br>
+                </td>
+            </tr>
+            <tr>
+                <td><?php $this->getText('STEP_3_UTFMODE'); ?>:</td>
+                <td>
+                    &nbsp;&nbsp;<input type="checkbox" name="aDB[iUtfMode]" value="1" <?php echo $sChecked;
+                    echo $sDisabled; ?>>
+                    <?php
+                    if ($blMbStringOn > 1 && $blUnicodeSupport > 1) {
+                        $this->getText('STEP_3_UTFINFO');
+                    } else {
+                        $this->getText('STEP_3_UTFNOTSUPPORTED');
+                        if ($blMbStringOn < 2) {
+                            $this->getText('STEP_3_UTFNOTSUPPORTED1');
+                        }
+                        if (($blMbStringOn + $blUnicodeSupport) == 2) {
+                            echo ",";
+                        }
+                        if ($blUnicodeSupport < 2) {
+                            $this->getText('STEP_3_UTFNOTSUPPORTED2');
+                        }
+                        echo ".";
+                    }
+                    ?>
+                    <br>
+                </td>
+            </tr>
+        </table>
+        <input type="hidden" name="sid" value="<?php $this->getSid(); ?>">
+        <input type="submit" id="step3Submit" class="edittext" value="<?php $this->getText('BUTTON_DB_INSTALL'); ?>">
+    </form>
 <?php require "_footer.php";
--- source/setup/tpl/systemreq.php
+++ source/setup/tpl/systemreq.php
@@ -20,66 +20,78 @@
  * @version   OXID eShop CE
  */
 require "_header.php"; ?>
-<strong><?php $this->getText('STEP_0_DESC'); ?></strong><br><br>
+    <strong><?php $this->getText('STEP_0_DESC'); ?></strong><br><br>
 
-<table cellpadding="1" cellspacing="0">
-    <tr>
-        <td nowrap><?php $this->getText('SELECT_SETUP_LANG'); ?>: </td>
-        <td>
-            <form action="index.php" id="langSelectionForm" method="post">
-            <select name="setup_lang" onChange="document.getElementById('langSelectionForm').submit();" style="font-size: 11px;">
-            <?php
-            $aLanguages = $this->getViewParam( "aLanguages" );
-            foreach ($aLanguages as $sLangId => $sLangTitle) {
-                ?>
-                <option value="<?php echo $sLangId; ?>" <?php if ( $this->getViewParam( "sSetupLang" ) == $sLangId ) echo 'selected'; ?>><?php echo $sLangTitle; ?></option>
-                <?php
-            }
-            ?>
-            </select>
-            <noscript>
-            <input type="submit" name="setup_lang_submit" value="<?php $this->getText('SELECT_SETUP_LANG_SUBMIT'); ?>" style="font-size: 11px;">
-            </noscript>
-            <input type="hidden" name="sid" value="<?php $this->getSid(); ?>">
-            <input type="hidden" name="istep" value="<?php $this->getSetupStep( 'STEP_SYSTEMREQ' ); ?>">
-            </form>
-        </td>
-        <td style="padding: 0px 5px;"><?php $this->getText('SELECT_SETUP_LANG_HINT'); ?></td>
-    </tr>
-</table>
-<br>
+    <table cellpadding="1" cellspacing="0">
+        <tr>
+            <td nowrap><?php $this->getText('SELECT_SETUP_LANG'); ?>:</td>
+            <td>
+                <form action="index.php" id="langSelectionForm" method="post">
+                    <select name="setup_lang" onChange="document.getElementById('langSelectionForm').submit();"
+                            style="font-size: 11px;">
+                        <?php
+                        $aLanguages = $this->getViewParam("aLanguages");
+                        foreach ($aLanguages as $sLangId => $sLangTitle) {
+                            ?>
+                            <option
+                                value="<?php echo $sLangId; ?>" <?php if ($this->getViewParam("sSetupLang") == $sLangId) {
+                                echo 'selected';
+                            } ?>><?php echo $sLangTitle; ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    <noscript>
+                        <input type="submit" name="setup_lang_submit"
+                               value="<?php $this->getText('SELECT_SETUP_LANG_SUBMIT'); ?>" style="font-size: 11px;">
+                    </noscript>
+                    <input type="hidden" name="sid" value="<?php $this->getSid(); ?>">
+                    <input type="hidden" name="istep" value="<?php $this->getSetupStep('STEP_SYSTEMREQ'); ?>">
+                </form>
+            </td>
+            <td style="padding: 0px 5px;"><?php $this->getText('SELECT_SETUP_LANG_HINT'); ?></td>
+        </tr>
+    </table>
+    <br>
 
     <ul class="req">
-    <?php
-    $aGroupModuleInfo = $this->getViewParam( "aGroupModuleInfo" );
-    foreach ($aGroupModuleInfo as $sGroupName => $aGroupInfo) {
-        ?><li class="group"><?php echo $sGroupName; ?><ul><?php
-        foreach ($aGroupInfo as $aModuleInfo) {
-            ?><li id="<?php echo $aModuleInfo['module']; ?>" class="<?php echo $aModuleInfo['class']; ?>"><?php
-            if ( $aModuleInfo['class'] == "fail" || $aModuleInfo['class'] == "pmin" || $aModuleInfo['class'] == "null" ) {
-                ?><a href="<?php $this->getReqInfoUrl( $aModuleInfo['module'] ); ?>" target="_blank"><?php
+        <?php
+        $aGroupModuleInfo = $this->getViewParam("aGroupModuleInfo");
+        foreach ($aGroupModuleInfo as $sGroupName => $aGroupInfo) {
+            ?>
+            <li class="group"><?php echo $sGroupName; ?>
+            <ul><?php
+            foreach ($aGroupInfo as $aModuleInfo) {
+                ?>
+                <li id="<?php echo $aModuleInfo['module']; ?>" class="<?php echo $aModuleInfo['class']; ?>"><?php
+            if ($aModuleInfo['class'] == "fail" || $aModuleInfo['class'] == "pmin" || $aModuleInfo['class'] == "null") {
+                ?><a href="<?php $this->getReqInfoUrl($aModuleInfo['module']); ?>" target="_blank"><?php
             }
-            echo $aModuleInfo['modulename'];
-            if ( $aModuleInfo['class'] == "fail" || $aModuleInfo['class'] == "pmin" ) {
+                echo $aModuleInfo['modulename'];
+            if ($aModuleInfo['class'] == "fail" || $aModuleInfo['class'] == "pmin") {
                 ?></a><?php
             }
-            ?></li><?php
+                ?></li><?php
+            }
+            ?></ul></li><?php
         }
-        ?></ul></li><?php
-    }
-    ?><li class="clear"></li></ul>
-    <?php $this->getText('STEP_0_TEXT'); ?>
+        ?>
+        <li class="clear"></li>
+    </ul>
+<?php $this->getText('STEP_0_TEXT'); ?>
     <br><br>
 
-<?php if ( $this->getViewParam( "blContinue" ) === true) { ?>
-<form action="index.php" method="post">
-<input type="hidden" name="sid" value="<?php $this->getSid(); ?>">
-<input type="hidden" name="istep" value="<?php $this->getSetupStep( 'STEP_WELCOME' ); ?>">
-<input type="submit" id="step0Submit" class="edittext" value="<?php $this->getText('BUTTON_PROCEED_INSTALL'); ?>">
-</form>
+<?php if ($this->getViewParam("blContinue") === true) { ?>
+    <form action="index.php" method="post">
+        <input type="hidden" name="sid" value="<?php $this->getSid(); ?>">
+        <input type="hidden" name="istep" value="<?php $this->getSetupStep('STEP_WELCOME'); ?>">
+        <input type="submit" id="step0Submit" class="edittext"
+               value="<?php $this->getText('BUTTON_PROCEED_INSTALL'); ?>">
+    </form>
 <?php
 } else {
     ?><b><?php $this->getText('STEP_0_ERROR_TEXT'); ?></b><br>
-    <a target="_blank" href="<?php $this->getText('STEP_0_ERROR_URL'); ?>"><?php $this->getText('STEP_0_ERROR_URL'); ?></a><?php
+    <a target="_blank"
+       href="<?php $this->getText('STEP_0_ERROR_URL'); ?>"><?php $this->getText('STEP_0_ERROR_URL'); ?></a><?php
 }
 require "_footer.php";

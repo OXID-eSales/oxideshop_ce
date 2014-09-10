75c289e89a548c816ac70f0cbb0ca0d45b988366
Artūras Ševčenko <arturas.sevcenko@gmail.com>
2014-09-10 17:56:46 +0300

    Fix checkstyle warnings of "Type InvalidNoReturn"

--- source/application/controllers/admin/article_stock.php
+++ source/application/controllers/admin/article_stock.php
@@ -94,8 +94,6 @@ class Article_Stock extends oxAdminDetails
 
     /**
      * Saves article Inventori information changes.
-     *
-     * @return mixed
      */
     public function save()
     {
@@ -216,8 +214,6 @@ class Article_Stock extends oxAdminDetails
 
     /**
      * Updates all amount prices for article at once
-     *
-     * @return null
      */
     public function updateprices()
     {
@@ -233,8 +229,6 @@ class Article_Stock extends oxAdminDetails
 
     /**
      * Adds amount price to article
-     *
-     * @return null
      */
     public function deleteprice()
     {

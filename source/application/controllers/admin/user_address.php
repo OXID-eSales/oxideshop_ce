cd5e7bf6672ac10d2ac231957efa17c94157215e
Artūras Ševčenko <arturas.sevcenko@gmail.com>
2014-09-11 10:25:02 +0300

    Fix checkstyle warnings of "Type InvalidNoReturn"

--- source/application/controllers/admin/user_address.php
+++ source/application/controllers/admin/user_address.php
@@ -89,8 +89,6 @@ class User_Address extends oxAdminDetails
 
     /**
      * Saves user addressing information.
-     *
-     * @return mixed
      */
     public function save()
     {
@@ -114,8 +112,6 @@ class User_Address extends oxAdminDetails
 
     /**
      * Deletes user addressing information.
-     *
-     * @return null
      */
     public function delAddress()
     {

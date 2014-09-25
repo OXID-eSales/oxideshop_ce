e4b985b09e89fcc8e706b2b45865a51d42afd4ab
shiftas <Shiftass@gmail.com>
2014-09-25 13:44:54 +0300

    0005628: Changes in oxNews-Longdescription does not change in frontend
    Regeneration of category description value is needed even when blCheckTemplates is set to false.
    (cherry picked from commit 159a98c)
    (cherry picked from commit 8202b60)

--- source/application/models/oxcategory.php
+++ source/application/models/oxcategory.php
@@ -1098,7 +1098,9 @@ class oxCategory extends oxI18n implements oxIUrl
     public function getLongDesc()
     {
         if (isset($this->oxcategories__oxlongdesc) && $this->oxcategories__oxlongdesc instanceof oxField) {
-            return oxRegistry::get("oxUtilsView")->parseThroughSmarty($this->oxcategories__oxlongdesc->getRawValue(), $this->getId() . $this->getLanguage());
+            /** @var oxUtilsView $oUtilsView */
+            $oUtilsView = oxRegistry::get("oxUtilsView");
+            return oxRegistry::get("oxUtilsView")->parseThroughSmarty($this->oxcategories__oxlongdesc->getRawValue(), $this->getId() . $this->getLanguage(), null, true);
         }
     }
 

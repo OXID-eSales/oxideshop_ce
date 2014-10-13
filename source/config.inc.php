6b8fe76e7f948c8bd82ff96f91d1b38151e0d792
Frank Zunderer <info@zunderweb.de>
2014-10-13 10:32:41 +0300

    Add blSkipViewUsage to config.inc

--- source/config.inc.php
+++ source/config.inc.php
@@ -191,8 +191,3 @@
      * Do not disable module if class from extension path does not exist.
      */
     $this->blDoNotDisableModuleOnError = false;
-    
-    /** 
-     * Enable temporarily in case you can't access the backend due to broken views 
-     */
-    $this->blSkipViewUsage = false;

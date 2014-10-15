0bd2d03bd98459c7fdca6f5ed9fb28dbcd59c273
Tadas Rimkus <dev.tadas.rimkus@gmail.com>
2014-10-15 10:35:20 +0300

    Added method to unflag seo update on object save if it's certain field is not changed. Added article and category title check for seo flagging. (cherry picked from commit a1da8b4)

--- source/application/models/oxarticle.php
+++ source/application/models/oxarticle.php
@@ -3999,6 +3999,8 @@ class oxArticle extends oxI18n implements oxIArticle, oxIUrl
     protected function _update()
     {
 
+        $this->_setUpdateSeoOnFieldChange('oxtitle');
+
         $this->_skipSaveFields();
 
         $myConfig = $this->getConfig();

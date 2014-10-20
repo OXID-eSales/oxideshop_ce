b23d215d5e563feb5d8bbcdeae6199a64b0e0273
shiftas <Shiftass@gmail.com>
2014-10-20 12:19:27 +0300

    #5753: Salt generation doesn't ensure the salt unpredictability.
    Adding back handling of plaing password to oxUser::getPasswordHash(), as it is still used by ERP.
    (cherry picked from commit 42d9af6)

--- source/application/models/oxuser.php
+++ source/application/models/oxuser.php
@@ -2191,6 +2191,9 @@ class oxUser extends oxBase
     {
         $sHash = null;
         if ( $this->oxuser__oxpassword->value ) {
+            if ( strlen( $this->oxuser__oxpassword->value ) < 32 ) {
+                $this->setPassword( $this->oxuser__oxpassword->value );
+            }
             $sHash = $this->oxuser__oxpassword->value;
         }
         return $sHash;

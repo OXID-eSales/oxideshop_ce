cefbec01af32de0416da84e0e205118158e705af
Adrian Kirchner <mail@adrian-kirchner.com>
2015-01-04 16:28:03 +0100

    disable output of unittests memory consumption in travis ci environment only

--- tests/unit/OxidTestCase.php
+++ tests/unit/OxidTestCase.php
@@ -245,7 +245,7 @@ class OxidTestCase extends PHPUnit_Framework_TestCase
         $oDbRestore = self::_getDbRestore();
         $oDbRestore->restoreDB();
 
-        if (function_exists('memory_get_usage')) {
+        if (function_exists('memory_get_usage') && !getenv('TRAVIS_ERROR_LEVEL')) {
             echo "\n" . round(memory_get_usage(1) / 1024 / 1024) . 'M (' . round(memory_get_peak_usage(1) / 1024 / 1024) . 'M)' . "\n";
         }
     }

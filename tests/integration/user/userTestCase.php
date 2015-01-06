58e78ae4031e78fb66ef78524e15c7bdb590f2b1
Tomas Liubinas <git@liubinas.com>
2015-01-06 16:50:32 +0200

    ESDEV-2570 Refactor UserTestCase to be possible to create old/new password encoded user records

--- tests/integration/user/userTestCase.php
+++ tests/integration/user/userTestCase.php
@@ -16,12 +16,40 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
 class UserTestCase extends OxidTestCase
 {
+
+    /**
+     * Password encoded with old algorithm.
+     *
+     * @var string
+     */
+    protected $_sOldEncodedPassword = '4bb11fbb0c6bf332517a7ec397e49f1c';
+
+    /**
+     * Salt generated with old algorithm.
+     *
+     * @var string
+     */
+    protected $_sOldSalt = '3262383936333839303439393466346533653733366533346137326666393632';
+
+    /**
+     * Password encoded with new algorithm.
+     *
+     * @var string
+     */
+    protected $_sNewEncodedPassword = 'b016e37ac8ec71449b475e84a941e3c39a27fb8f0710d4b47d6116ad6a6afcaa0c17006a4c01ffc67f3db95772fe001584cb4ce7e5bacd74198c24d1851841d5';
+
+    /**
+     * Salt generated with new algorithm.
+     *
+     * @var string
+     */
+    protected $_sNewSalt = '56784f8ffc657fff84915b93e12a626e';
 
     /** @var string */
     protected $_sDefaultUserName = '_testUserName@oxid-esales.com';
@@ -42,12 +70,31 @@ class UserTestCase extends OxidTestCase
         parent::tearDown();
     }
 
-    protected function _createDefaultUser($sRight, $iShopId)
+    /**
+     * Creates user with the default credentials for given shop.
+     *
+     * MD5 encoded password style is used for legacy shops
+     *
+     * @param string $sRight            OXRIGHTS column value ('malladmin', 'user' or <shopid>)
+     * @param int    $iShopId           Shop ID
+     * @param bool   $blMd5EncodedStyle Use MD5 encoded (legacy) password encryption
+     *
+     * @return oxUser
+     */
+    protected function _createDefaultUser($sRight, $iShopId, $blMd5EncodedStyle = true)
     {
+        if ($blMd5EncodedStyle) {
+            $sPassVal = $this->_sOldEncodedPassword;
+            $sSaltVal = $this->_sOldSalt;
+        } else {
+            $sPassVal = $this->_sNewEncodedPassword;
+            $sSaltVal = $this->_sNewSalt;
+        }
+
         $oUser = new oxUser();
         $oUser->oxuser__oxusername = new oxField('_testUserName@oxid-esales.com', oxField::T_RAW);
-        $oUser->oxuser__oxpassword = new oxField('4bb11fbb0c6bf332517a7ec397e49f1c', oxField::T_RAW);
-        $oUser->oxuser__oxpasssalt = new oxField('3262383936333839303439393466346533653733366533346137326666393632', oxField::T_RAW);
+        $oUser->oxuser__oxpassword = new oxField($sPassVal, oxField::T_RAW);
+        $oUser->oxuser__oxpasssalt = new oxField($sSaltVal, oxField::T_RAW);
         $oUser->save();
 
         $oUserFromBase = new oxBase();

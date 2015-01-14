65c6f0ca688619b61e15037f325181e801a489ec
Arturas Sevcenko <arturas.sevcenko@oxid-esales.com>
2015-01-14 15:05:51 +0200

    ESDEV-2610 Change OLIS protocol version from 1.0 to 1.1
    Protocol version 1.0 for eShop version >=5.2 was not intended.
    
    (cherry picked from commit 35079d3)

--- source/core/oxonlinelicensecheckrequest.php
+++ source/core/oxonlinelicensecheckrequest.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
@@ -35,7 +35,7 @@ class oxOnlineLicenseCheckRequest extends oxOnlineRequest
      *
      * @var string
      */
-    public $pVersion = '1.0';
+    public $pVersion = '1.1';
 
     /**
      * Serial keys.

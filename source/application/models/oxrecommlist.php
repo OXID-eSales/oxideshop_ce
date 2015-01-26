993d16b09ba1ed80d966e0f2c826f9690f6052a4
stasiukaitis-saulius <abaldukas90>
2015-01-26 16:04:23 +0200

    comment fixed

--- source/application/models/oxrecommlist.php
+++ source/application/models/oxrecommlist.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
@@ -97,7 +97,7 @@ class oxRecommList extends oxBase implements oxIUrl
     /**
      * Returns count of recommendation list items
      *
-     * @return array of oxUserBasketItems
+     * @return integer
      */
     public function getArtCount()
     {

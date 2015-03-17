990d7ea95ccc16fd1d9b995865f8d0e94a307099
shiftas <Shiftass@gmail.com>
2015-03-17 11:11:58 +0200

    ESDEV-2803 Update Smarty from 2.6.26 to 2.6.28
    Smarty added a patch for 2.6 version to support php 5.5.
    (cherry picked from commit 51f635f)

--- source/core/smarty/Smarty.class.php
+++ source/core/smarty/Smarty.class.php
@@ -27,10 +27,10 @@
  * @author Monte Ohrt <monte at ohrt dot com>
  * @author Andrei Zmievski <andrei@php.net>
  * @package Smarty
- * @version 2.6.26
+ * @version 2.6.28
  */
 
-/* $Id$ */
+/* $Id: Smarty.class.php 4660 2012-09-24 20:05:15Z uwe.tews@googlemail.com $ */
 
 /**
  * DIR_SEP isn't used anymore, but third party apps might
@@ -465,7 +465,7 @@ class Smarty
      *
      * @var string
      */
-    var $_version              = '2.6.26';
+    var $_version              = '2.6.28';
 
     /**
      * current template inclusion depth
@@ -1090,7 +1090,8 @@ class Smarty
      */
     function trigger_error($error_msg, $error_type = E_USER_WARNING)
     {
-        trigger_error("Smarty error: $error_msg", $error_type);
+        $msg = htmlentities($error_msg);
+        trigger_error("Smarty error: $msg", $error_type);
     }
 
 

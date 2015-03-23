a86ba020fdd18ab633cc6995f673fdc89cd7f7ec
Tadas Rimkus <dev.tadas.rimkus@gmail.com>
2015-03-23 16:52:22 +0200

    Add reset user list when deleting user.

--- source/application/controllers/admin/user_list.php
+++ source/application/controllers/admin/user_list.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
@@ -84,6 +84,7 @@ class User_List extends oxAdminList
     public function deleteEntry()
     {
         if ($this->_allowAdminEdit($this->getEditObjectId())) {
+            $this->_oList = null;
             return parent::deleteEntry();
         }
     }

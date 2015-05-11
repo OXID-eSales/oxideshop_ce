1ddec96832d03d87c101ef6f1d03e8744eb47252
Mantas Vaitkūnas <mantas.vaitkunas@oxid-esales.com>
2015-05-11 15:26:29 +0300

    ESDEV-2883 Remove OXID_PHP_UNIT from oxLang class
    Remove OXID_PHP_UNIT conditions as they are not needed anymore.

--- source/core/oxlang.php
+++ source/core/oxlang.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
@@ -606,10 +606,6 @@ class oxLang extends oxSuperCfg
             $this->_iBaseLanguageId = (int) $iLang;
         }
 
-        if (defined('OXID_PHP_UNIT')) {
-            modSession::getInstance();
-        }
-
         oxRegistry::getSession()->setVariable('language', $iLang);
     }
 
@@ -628,10 +624,6 @@ class oxLang extends oxSuperCfg
             if (!isset($aLanguages[$this->_iTplLanguageId])) {
                 $this->_iTplLanguageId = key($aLanguages);
             }
-        }
-
-        if (defined('OXID_PHP_UNIT')) {
-            modSession::getInstance();
         }
 
         oxRegistry::getSession()->setVariable('tpllanguage', $this->_iTplLanguageId);

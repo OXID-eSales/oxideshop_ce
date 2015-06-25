2ea1a47ef121885f6be04573441e6a74d38ebe9a
shiftas <Shiftass@gmail.com>
2015-06-25 10:36:16 +0300

    Add oxPdf class to Enterprise edition

--- tests/unit/core/oxpdfTest.php
+++ tests/unit/core/oxpdfTest.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2015
+ * @copyright (C) OXID eSales AG 2003-2016
  * @version   OXID eShop CE
  */
 
@@ -25,7 +25,6 @@
  */
 class Unit_Core_oxpdfTest extends OxidTestCase
 {
-
     /**
      * oxPdf::PutLink() test case
      *

3fe91c9003dec6db278b5fbfbdf0bfd84131e730
Mantas Vaitkūnas <mantas.vaitkunas@oxid-esales.com>
2015-07-17 10:09:11 +0300

    ESDEV-3002 Create additional.inc files in edition based directories

--- tests/additional.inc.php
+++ tests/additional.inc.php
@@ -1,7 +1,15 @@
 <?php
 
 $serviceCaller = new \OxidEsales\TestingLibrary\ServiceCaller();
-$serviceCaller->setParameter('importSql', '@'. __DIR__ .'/testsql/testdata' .OXID_VERSION_SUFIX . '.sql');
+if (OXID_VERSION_PE_CE) {
+    $serviceCaller->setParameter('importSql', '@'. __DIR__ .'/Fixtures/testdata.sql');
+}
+if (OXID_VERSION_PE_PE) {
+    $serviceCaller->setParameter('importSql', realpath('../source/Edition/Professional/Tests/Fixtures/testdata.sql'));
+}
+if (OXID_VERSION_EE) {
+    $serviceCaller->setParameter('importSql', realpath('../source/Edition/Enterprise/Tests/Fixtures/testdata.sql'));
+}
 $serviceCaller->callService('ShopPreparation', 1);
 
 define('oxADMIN_LOGIN', oxDb::getDb()->getOne("select OXUSERNAME from oxuser where oxid='oxdefaultadmin'"));

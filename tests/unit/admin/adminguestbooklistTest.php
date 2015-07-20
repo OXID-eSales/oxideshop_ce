16d7aee87bb8a2aaf668f9e5fc91bcc2b1ebbdee
Anton Fedurtsya <anton.fedurtsya@oxid-esales.com>
2015-07-20 13:13:06 +0300

    ESDEV-2994 Remove edition based code from adminguestbook_list
    Edition based code was moved to AdminGuestbookList class.

--- tests/unit/admin/adminguestbooklistTest.php
+++ tests/unit/admin/adminguestbooklistTest.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2015
+ * @copyright (C) OXID eSales AG 2003-2016
  * @version   OXID eShop CE
  */
 
@@ -51,6 +51,4 @@ class Unit_Admin_AdminGuestbookListTest extends OxidTestCase
         $this->assertEquals(1, $aViewData["mylist"]->count());
 
     }
-
-
 }

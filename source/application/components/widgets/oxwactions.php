fef995640124d6fb16f207f82b132e1ff02c8834
hkreuter <heike.reuter@oxid-esales.com>
2015-07-27 12:41:05 +0200

    ESDEV-2969 PR37 Update file header and apply minor style fixes
    Change file header to newest OXID version.
    Minor style fixes.
    Relates #37

--- source/application/components/widgets/oxwactions.php
+++ source/application/components/widgets/oxwactions.php
@@ -1,25 +1,24 @@
 <?php
+
 /**
- *    This file is part of OXID eShop Community Edition.
+ * This file is part of OXID eShop Community Edition.
  *
- *    OXID eShop Community Edition is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    OXID eShop Community Edition is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @package   views
- * @copyright (C) OXID eSales AG 2003-2013
- * @version OXID eShop CE
- * @version   SVN: $Id$
+ * @copyright (C) OXID eSales AG 2003-2015
+ * @version   OXID eShop CE
  */
 
 /**
@@ -50,12 +49,10 @@ class oxwActions extends oxWidget
     public function getAction()
     {
         $sActionId = $this->getViewParameter("action");
-        if ( $sActionId && $this->_getLoadActionsParam() )
-        {
-            $oArtList = oxNew( 'oxarticlelist' );
-            $oArtList->loadActionArticles( $sActionId );
-            if ( $oArtList->count() )
-            {
+        if ($sActionId && $this->_getLoadActionsParam()) {
+            $oArtList = oxNew('oxarticlelist');
+            $oArtList->loadActionArticles($sActionId);
+            if ($oArtList->count()) {
                 return $oArtList;
             }
         }
@@ -68,7 +65,8 @@ class oxwActions extends oxWidget
      */
     protected function _getLoadActionsParam()
     {
-        $this->_blLoadActions = $this->getConfig()->getConfigParam( 'bl_perfLoadAktion' );
+        $this->_blLoadActions = $this->getConfig()->getConfigParam('bl_perfLoadAktion');
+
         return $this->_blLoadActions;
     }
 
@@ -80,13 +78,12 @@ class oxwActions extends oxWidget
     public function getActionName()
     {
         $sActionId = $this->getViewParameter("action");
-        $oAction = oxNew( "oxactions" );
-        if($oAction->load( $sActionId ))
-        {
+        $oAction   = oxNew("oxactions");
+        if ($oAction->load($sActionId)) {
             return $oAction->oxactions__oxtitle->value;
         }
     }
-    
+
     /**
      * Returns products list type
      *
@@ -96,5 +93,4 @@ class oxwActions extends oxWidget
     {
         return $this->getViewParameter("listtype");
     }
-
 }

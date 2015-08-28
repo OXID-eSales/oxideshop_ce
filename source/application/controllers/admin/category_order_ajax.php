18316b293fdb285ce54c1c2606d9741f1adb8a2b
Saulius Stasiukaitis <saulius.stasiukaitis@oxid-esales.com>
2015-08-28 17:33:12 +0300

    ESDEV-2890 Remove edition tags from isDerived

--- source/application/controllers/admin/category_order_ajax.php
+++ source/application/controllers/admin/category_order_ajax.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2015
+ * @copyright (C) OXID eSales AG 2003-2016
  * @version   OXID eShop CE
  */
 
@@ -185,6 +185,11 @@ class category_order_ajax extends ajaxListComponent
         $sId = oxRegistry::getConfig()->getRequestParameter("oxid");
         if ($oCategory->load($sId)) {
 
+            //Disable editing for derived items
+            if ($oCategory->isDerived()) {
+                return;
+            }
+
             $this->resetContentCache();
 
             $aNewOrder = oxRegistry::getSession()->getVariable("neworder_sess");
@@ -221,6 +226,10 @@ class category_order_ajax extends ajaxListComponent
         $sId = oxRegistry::getConfig()->getRequestParameter("oxid");
         if ($oCategory->load($sId)) {
 
+            //Disable editing for derived items
+            if ($oCategory->isDerived()) {
+                return;
+            }
 
             $oDb = oxDb::getDb();
 

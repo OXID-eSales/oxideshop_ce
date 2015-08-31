20006370ceb9f41e50ca9907da34720a9a3587b6
Stefan Krenz <krenz@marmalade.de>
2015-08-31 16:45:04 +0300

    6221 Compare vendor and category id's more strictly in manufacturer tree building
    Related with bug: http://bugs.oxid-esales.com/view.php?id=6221

--- source/application/models/oxmanufacturerlist.php
+++ source/application/models/oxmanufacturerlist.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
@@ -123,7 +123,7 @@ class oxManufacturerList extends oxList
         foreach ($this as $sVndId => $oManufacturer) {
 
             // storing active manufacturer object
-            if ($sVndId == $sActCat) {
+            if ($sVndId === $sActCat) {
                 $this->setClickManufacturer($oManufacturer);
             }
 

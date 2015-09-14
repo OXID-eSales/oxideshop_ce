73d8314216316aa0e26aadf92f32305cb10ef4bc
Mantas Vaitkūnas <mantas.vaitkunas@oxid-esales.com>
2015-09-14 16:41:11 +0300

    ESDEV-3024 Add class map provider

---
+++ source/Core/ClassMapProvider.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ * @version   OXID eShop CE
+ */
+
+namespace OxidEsales\Core;
+
+use OxidEsales\Enterprise\ClassMap as EnterpriseClassMap;
+use OxidEsales\Professional\ClassMap as ProfessionalClassMap;
+
+/**
+ * Class responsible for returning class map by edition.
+ */
+class ClassMapProvider
+{
+    /** @var array */
+    private $notOverridableClassMap = array();
+
+    /** @var array */
+    private $overridableClassMap = array();
+
+    /** @var EnterpriseClassMap */
+    private $enterpriseClassMap;
+
+    /** @var ProfessionalClassMap */
+    private $professionalClassMap;
+
+    /**
+     * @param EditionSelector $editionSelector
+     */
+    public function __construct($editionSelector)
+    {
+        $this->editionSelector = $editionSelector;
+    }
+
+    /**
+     * @param ProfessionalClassMap $classMap
+     */
+    public function setClassMapProfessional($classMap)
+    {
+        $this->professionalClassMap = $classMap;
+    }
+
+    /**
+     * @param EnterpriseClassMap $classMap
+     */
+    public function setClassMapEnterprise($classMap)
+    {
+        $this->enterpriseClassMap = $classMap;
+    }
+
+    /**
+     * Method returns overridable classes class map according edition.
+     *
+     * @return array
+     */
+    public function getOverridableClassMap()
+    {
+        if (empty($this->overridableClassMap)) {
+            $this->formClassMaps();
+        }
+
+        return $this->overridableClassMap;
+    }
+
+    /**
+     * Method returns not overridable classes class map according edition.
+     *
+     * @return array
+     */
+    public function getNotOverridableClassMap()
+    {
+        if (empty($this->notOverridableClassMap)) {
+            $this->formClassMaps();
+        }
+
+        return $this->notOverridableClassMap;
+    }
+
+    /**
+     * Method forms overridable and not overridable class maps and sets them.
+     */
+    protected function formClassMaps()
+    {
+        $overridableMap = array();
+        $notOverridableMap = array();
+        $editionSelector = $this->getEditionSelector();
+        if ($editionSelector->getEdition() === EditionSelector::ENTERPRISE
+            || $editionSelector->getEdition() === EditionSelector::PROFESSIONAL
+        ) {
+            $classMapProfessional = $this->getClassMapProfessional();
+            $overridableMap = $classMapProfessional->getOverridableMap();
+            $notOverridableMap = $classMapProfessional->getNotOverridableMap();
+        }
+
+        if ($editionSelector->getEdition() === EditionSelector::ENTERPRISE) {
+            $classMapEnterprise = $this->getClassMapEnterprise();
+            $overridableMap = array_merge($overridableMap, $classMapEnterprise->getOverridableMap());
+            $notOverridableMap = array_merge($notOverridableMap, $classMapEnterprise->getNotOverridableMap());
+        }
+
+        $this->overridableClassMap = $overridableMap;
+        $this->notOverridableClassMap = $notOverridableMap;
+    }
+
+    /**
+     * Method is responsible for providing class map object.
+     *
+     * @return ProfessionalClassMap
+     */
+    protected function getClassMapProfessional()
+    {
+        if (is_null($this->professionalClassMap)) {
+            $this->professionalClassMap = new ProfessionalClassMap();
+        }
+
+        return $this->professionalClassMap;
+    }
+
+    /**
+     * Method is responsible for providing class map object.
+     *
+     * @return EnterpriseClassMap
+     */
+    protected function getClassMapEnterprise()
+    {
+        if (is_null($this->enterpriseClassMap)) {
+            $this->enterpriseClassMap = new EnterpriseClassMap();
+        }
+
+        return $this->enterpriseClassMap;
+    }
+
+    /**
+     * Getter for edition selector.
+     *
+     * @return EditionSelector
+     */
+    protected function getEditionSelector()
+    {
+        return $this->editionSelector;
+    }
+}
---
+++ tests/unit/core/ClassMapProviderTest.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ * @version   OXID eShop CE
+ */
+
+use OxidEsales\Core\ClassMapProvider;
+use OxidEsales\Core\EditionSelector;
+
+class ClassMapProviderTest extends OxidTestCase
+{
+    /**
+     * @return array
+     */
+    public function providerGetsNotOverridableClassMap()
+    {
+        $classMapProfessional = array(
+            'classnotoverloadableprofessional' => '\class\which\is\under\PE\not\overloadable'
+        );
+        $classMapEnterprise = array(
+            'classnotoverloadableenterprise' => '\class\which\is\under\EE\not\overloadable'
+        );
+        $resultProfessional = $classMapProfessional;
+        $resultEnterprise = array(
+            'classnotoverloadableprofessional' => '\class\which\is\under\PE\not\overloadable',
+            'classnotoverloadableenterprise' => '\class\which\is\under\EE\not\overloadable'
+        );
+        $resultCommunity = array();
+
+        return array(
+            array($classMapProfessional, $classMapEnterprise, EditionSelector::PROFESSIONAL, $resultProfessional),
+            array($classMapProfessional, $classMapEnterprise, EditionSelector::ENTERPRISE, $resultEnterprise),
+            array(null, null, EditionSelector::COMMUNITY, $resultCommunity),
+        );
+    }
+
+    /**
+     * @param array  $mapNotOverridableProfessional
+     * @param array  $mapNotOverridableEnterprise
+     * @param string $edition
+     * @param array  $result
+     *
+     * @dataProvider providerGetsNotOverridableClassMap
+     */
+    public function testGetsNotOverridableClassMap(
+        $mapNotOverridableProfessional,
+        $mapNotOverridableEnterprise,
+        $edition,
+        $result
+    ) {
+        /** @var EditionSelector|PHPUnit_Framework_MockObject_MockObject $editionSelector */
+        $editionSelector = $this->getMockBuilder('\OxidEsales\Core\EditionSelector')->getMock();
+        $editionSelector->expects($this->atLeastOnce())->method('getEdition')->will($this->returnValue($edition));
+
+        /** @var \OxidEsales\Professional\ClassMap|PHPUnit_Framework_MockObject_MockObject $classMapProfessional */
+        $classMapProfessional = $this->getMockBuilder('\OxidEsales\Professional\ClassMap')->getMock();
+        $classMapProfessional->expects($this->any())->method('getNotOverridableMap')->will($this->returnValue($mapNotOverridableProfessional));
+        $classMapProfessional->expects($this->any())->method('getOverridableMap')->will($this->returnValue(array()));
+
+        /** @var \OxidEsales\Enterprise\ClassMap $classMapEnterprise|PHPUnit_Framework_MockObject_MockObject */
+        $classMapEnterprise = $this->getMockBuilder('\OxidEsales\Enterprise\ClassMap')->getMock();
+        $classMapEnterprise->expects($this->any())->method('getNotOverridableMap')->will($this->returnValue($mapNotOverridableEnterprise));
+        $classMapEnterprise->expects($this->any())->method('getOverridableMap')->will($this->returnValue(array()));
+
+        $classMapSelector = new ClassMapProvider($editionSelector);
+        $classMapSelector->setClassMapProfessional($classMapProfessional);
+        $classMapSelector->setClassMapEnterprise($classMapEnterprise);
+
+        $this->assertSame($result, $classMapSelector->getNotOverridableClassMap());
+    }
+
+    /**
+     * @param array  $mapOverridableProfessional
+     * @param array  $mapOverridableEnterprise
+     * @param string $edition
+     * @param array  $result
+     *
+     * @dataProvider providerGetsNotOverridableClassMap
+     */
+    public function testGetsOverridableClassMap(
+        $mapOverridableProfessional,
+        $mapOverridableEnterprise,
+        $edition,
+        $result
+    ) {
+        /** @var EditionSelector|PHPUnit_Framework_MockObject_MockObject $editionSelector */
+        $editionSelector = $this->getMockBuilder('\OxidEsales\Core\EditionSelector')->getMock();
+        $editionSelector->expects($this->atLeastOnce())->method('getEdition')->will($this->returnValue($edition));
+
+        /** @var \OxidEsales\Professional\ClassMap|PHPUnit_Framework_MockObject_MockObject $classMapProfessional */
+        $classMapProfessional = $this->getMockBuilder('\OxidEsales\Professional\ClassMap')->getMock();
+        $classMapProfessional->expects($this->any())->method('getOverridableMap')->will($this->returnValue($mapOverridableProfessional));
+        $classMapProfessional->expects($this->any())->method('getNotOverridableMap')->will($this->returnValue(array()));
+
+        /** @var \OxidEsales\Enterprise\ClassMap $classMapEnterprise|PHPUnit_Framework_MockObject_MockObject */
+        $classMapEnterprise = $this->getMockBuilder('\OxidEsales\Enterprise\ClassMap')->getMock();
+        $classMapEnterprise->expects($this->any())->method('getOverridableMap')->will($this->returnValue($mapOverridableEnterprise));
+        $classMapEnterprise->expects($this->any())->method('getNotOverridableMap')->will($this->returnValue(array()));
+
+        $classMapSelector = new ClassMapProvider($editionSelector);
+        $classMapSelector->setClassMapProfessional($classMapProfessional);
+        $classMapSelector->setClassMapEnterprise($classMapEnterprise);
+
+        $this->assertSame($result, $classMapSelector->getOverridableClassMap());
+    }
+}

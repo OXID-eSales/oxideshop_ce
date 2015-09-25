6ae122e2d8278ff27c79498af4cca31e417d53ce
Daniel Seifert <ds@shopmodule.com>
2015-09-25 13:21:39 +0300

    6233 Fix Language switch url in lists
    https://bugs.oxid-esales.com/view.php?id=6233
    
    Resolves #258

--- source/application/controllers/oxubase.php
+++ source/application/controllers/oxubase.php
@@ -16,7 +16,7 @@
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2014
+ * @copyright (C) OXID eSales AG 2003-2015
  * @version   OXID eShop CE
  */
 
@@ -1668,7 +1668,7 @@ class oxUBase extends oxView
      */
     public function getLink($iLang = null)
     {
-        return $this->_addPageNrParam($this->getBaseLink($iLang), $this->getActPage());
+        return $this->_addPageNrParam($this->getBaseLink($iLang), $this->getActPage(), $iLang);
     }
 
     /**

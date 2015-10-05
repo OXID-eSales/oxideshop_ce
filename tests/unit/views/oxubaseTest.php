5b4b7b0c972f974cb2b470ccbc832518e941213c
Anton Fedurtsya <anton.fedurtsya@oxid-esales.com>
2015-10-05 10:54:10 +0300

    6233 Test case to check if uBase::getLink transfers parameters deeper

--- tests/unit/views/oxubaseTest.php
+++ tests/unit/views/oxubaseTest.php
@@ -189,7 +189,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
 
     public function testGetActTagSeo()
     {
-            $sTag = "liebliche";
+        $sTag = "liebliche";
 
         oxTestModules::addFunction("oxutils", "seoIsActive", "{return true;}");
         oxTestModules::addFunction("oxutilsserver", "getServerVar", "{ \$aArgs = func_get_args(); if ( \$aArgs[0] === 'HTTP_HOST' ) { return '" . oxRegistry::getConfig()->getShopUrl() . "'; } elseif ( \$aArgs[0] === 'SCRIPT_NAME' ) { return ''; } else { return \$_SERVER[\$aArgs[0]]; } }");
@@ -242,7 +242,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
 
     public function testGetActManufacturer()
     {
-            $sId = 'fe07958b49de225bd1dbc7594fb9a6b0';
+        $sId = 'fe07958b49de225bd1dbc7594fb9a6b0';
         $this->setRequestParam('mnid', $sId);
 
         $oUBase = new oxUBase();
@@ -263,7 +263,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
 
     public function testGetActVendor__()
     {
-            $sId = 'v_68342e2955d7401e6.18967838';
+        $sId = 'v_68342e2955d7401e6.18967838';
         $this->setRequestParam('cnid', $sId);
 
         $oUBase = new oxUBase();
@@ -333,13 +333,13 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
         $sShopURL = $myConfig->getShopUrl();
         $sShopID = $myConfig->getShopId();
 
-            $oView = new oxubase();
-            $sId = $oView->getViewId();
-            $this->assertEquals("ox|0|0|0|0", $oView->getViewId());
+        $oView = new oxubase();
+        $sId = $oView->getViewId();
+        $this->assertEquals("ox|0|0|0|0", $oView->getViewId());
 
-            // and caching
-            oxRegistry::getLang()->setBaseLanguage(1);
-            $this->assertEquals("ox|0|0|0|0", $oView->getViewId());
+        // and caching
+        oxRegistry::getLang()->setBaseLanguage(1);
+        $this->assertEquals("ox|0|0|0|0", $oView->getViewId());
     }
 
 
@@ -363,7 +363,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
         $sShopID = $myConfig->getShopId();
 
 
-            $this->assertEquals("ox|1|1|0|0", $sId);
+        $this->assertEquals("ox|1|1|0|0", $sId);
     }
 
     /*
@@ -381,7 +381,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
         $sShopID = $myConfig->getShopId();
 
 
-            $this->assertEquals("ox|0|0|0|0|ssl", $sId);
+        $this->assertEquals("ox|0|0|0|0|ssl", $sId);
     }
 
     public function testGetMetaDescriptionForStartView()
@@ -1080,6 +1080,21 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
         $this->assertEquals('&amp;listtype=tag&amp;searchtag=testtag', $sGot);
     }
 
+    /**
+     * Bug 6233 test case.
+     */
+    public function testGetLinkTransfersThirdParameter()
+    {
+        $languageId = 2;
+        $activePage = 10;
+
+        $baseView = $this->getMock('oxubase', array('getBaseLink', 'getActPage', '_addPageNrParam'));
+        $baseView->expects($this->once())->method('getActPage')->will($this->returnValue($activePage));
+        $baseView->expects($this->once())->method('getBaseLink')->with($this->equalTo($languageId))->will($this->returnValue('link'));
+        $baseView->expects($this->once())->method('_addPageNrParam')->with($this->equalTo('link'), $this->equalTo($activePage), $this->equalTo($languageId));
+        $baseView->getLink($languageId);
+    }
+
     public function testGetLinkWithDefinedPageNumber()
     {
         $oConfig = $this->getConfig();
@@ -1106,9 +1121,9 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
 
         $oV = $this->getMock('oxubase', array('_getRequestParams', '_getSubject'));
         $oArt = new oxArticle();
-            $oArt->loadInLang(1, '1126');
-            $sExp = "Geschenke/Bar-Equipment/Bar-Set-ABSINTH.html";
-            $sExpEng = "en/Gifts/Bar-Equipment/Bar-Set-ABSINTH.html";
+        $oArt->loadInLang(1, '1126');
+        $sExp = "Geschenke/Bar-Equipment/Bar-Set-ABSINTH.html";
+        $sExpEng = "en/Gifts/Bar-Equipment/Bar-Set-ABSINTH.html";
 
         $oV->expects($this->any())->method('_getSubject')->will($this->returnValue($oArt));
 
@@ -1125,9 +1140,9 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
         $oV = $this->getMock('oxubase', array('_getRequestParams', '_getSubject'));
         $oArt = new oxArticle();
         $oArt->setLinkType(OXARTICLE_LINKTYPE_MANUFACTURER);
-            $oArt->loadInLang(1, '1964');
-            $sVndExp = "Nach-Hersteller/Bush/Original-BUSH-Beach-Radio.html";
-            $sVndExpEng = "en/By-Manufacturer/Bush/Original-BUSH-Beach-Radio.html";
+        $oArt->loadInLang(1, '1964');
+        $sVndExp = "Nach-Hersteller/Bush/Original-BUSH-Beach-Radio.html";
+        $sVndExpEng = "en/By-Manufacturer/Bush/Original-BUSH-Beach-Radio.html";
 
         $oV->expects($this->any())->method('_getSubject')->will($this->returnValue($oArt));
 
@@ -1577,7 +1592,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
 
         $oUBase->setNonPublicVar("_blTop5Action", true);
         $aList = $oUBase->getTop5ArticleList();
-            $this->assertEquals(4, $aList->count());
+        $this->assertEquals(4, $aList->count());
     }
 
     public function testGetTop5ArticleList_notDefaultCount()
@@ -1594,7 +1609,7 @@ class Unit_Views_oxUBaseTest extends OxidTestCase
 
         $oUBase->setNonPublicVar("_blBargainAction", true);
         $aList = $oUBase->getBargainArticleList();
-            $this->assertEquals(4, $aList->count());
+        $this->assertEquals(4, $aList->count());
     }
 
     public function testGetNewsRealStatus()

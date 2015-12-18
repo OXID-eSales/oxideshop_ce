73eae6782f339682ac1a59b76053367d608f8419
Anton Fedurtsya <anton.fedurtsya@oxid-esales.com>
2015-12-18 12:20:26 +0200

    ESDEV-2733 Extracting Edition code from oxadminlog_module to AdminLogSqlDecorator class

---
+++ source/Core/AdminLogSqlDecorator.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ * @version   OXID eShop CE
+ */
+
+namespace OxidEsales\Eshop\Core;
+
+use oxDb;
+
+/**
+ * Decorator for
+ */
+class AdminLogSqlDecorator
+{
+    protected $table = 'oxadminlog';
+
+    /**
+     * Injects argument to admin log insert sql.
+     *
+     * @param string $originalSql
+     * @return string
+     */
+    public function prepareSqlForLogging($originalSql)
+    {
+        $userId = $this->getUserId();
+
+        $preparedSql = "insert into {$this->table} (oxuserid, oxsql) values ('{$userId}', " . $this->quote($originalSql) . ")";
+
+        return $preparedSql;
+    }
+
+    /**
+     * Get currently logged in admin user id
+     *
+     * @return string
+     */
+    protected function getUserId()
+    {
+        $user = oxNew('oxUser');
+        $result = '';
+        if ($user->loadAdminUser()) {
+            $result = $user->getId();
+        }
+        return $result;
+    }
+
+    /**
+     * Quotes the string for saving in database field;
+     *
+     * @param $str
+     * @return string
+     */
+    protected function quote($str)
+    {
+        $db = oxDb::getDb();
+        $result = $db->quote($str);
+        return $result;
+    }
+}

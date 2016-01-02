348978ea3234ff68628b5316530c123d7bed4729
Adrian Kirchner <mail@adrian-kirchner.com>
2016-01-02 23:15:09 +0100

    Fix incomplete cleanup of oxseohistory in onDelete* methods for main entity types (oxArticle, oxCategory, oxContent, oxManufacturer, oxVendor)

--- tests/unit/models/oxseoencodervendorTest.php
+++ tests/unit/models/oxseoencodervendorTest.php
@@ -289,8 +289,12 @@ class Unit_Models_oxSeoEncoderVendorTest extends OxidTestCase
         $sQ = "insert into oxobject2seodata ( oxobjectid, oxshopid, oxlang ) values ( 'oid', '{$sShopId}', '0' )";
         $oDb->execute($sQ);
 
+        $sQ = "insert into oxseohistory ( oxobjectid, oxident, oxshopid, oxlang ) values ( 'oid', '132', '{$sShopId}', '0' )";
+        $oDb->execute($sQ);
+
         $this->assertTrue((bool) $oDb->getOne("select 1 from oxseo where oxobjectid = 'oid'"));
         $this->assertTrue((bool) $oDb->getOne("select 1 from oxobject2seodata where oxobjectid = 'oid'"));
+        $this->assertTrue((bool) $oDb->getOne("select 1 from oxseohistory where oxobjectid = 'oid'"));
 
         $oObj = new oxbase();
         $oObj->setId('oid');
@@ -300,6 +304,7 @@ class Unit_Models_oxSeoEncoderVendorTest extends OxidTestCase
 
         $this->assertFalse((bool) $oDb->getOne("select 1 from oxseo where oxobjectid = 'oid'"));
         $this->assertFalse((bool) $oDb->getOne("select 1 from oxobject2seodata where oxobjectid = 'oid'"));
+        $this->assertFalse((bool) $oDb->getOne("select 1 from oxseohistory where oxobjectid = 'oid'"));
 
     }
 

e1e7f30457cd02058ceff2deec776d9c95c21306
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-03-08 17:36:52 +0200

    Revert "ESDEV-3440 Adjust oxemail for upgraded PHPMailer"
    This reverts commit 407ff5d993958ffb0835e6247970a83a58a6710f.
    
    This is reverted due to:
    
    * Found error on CI "smtp not working" needs further investigation
    * Missing bugtrack IDs

--- source/core/oxdb.php
+++ source/core/oxdb.php
@@ -325,7 +325,6 @@ class oxDb
     protected function _sendMail($sEmail, $sSubject, $sBody)
     {
         include_once getShopBasePath() . 'core/phpmailer/class.phpmailer.php';
-        include_once getShopBasePath() . 'core/phpmailer/class.smtp.php';
         $oMailer = new phpmailer();
         $oMailer->isMail();
 

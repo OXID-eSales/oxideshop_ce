77e270cdd2a77de999ac364a4d173b333ef1ea99
shiftas <Shiftass@gmail.com>
2016-04-14 13:40:11 +0300

    Fix acceptance test
    Do not rely on secondary ordering in lists, as sorting of secondary keys acts differently through PHP versions.

--- tests/Acceptance/Admin/CreatingItemsAdminTest.php
+++ tests/Acceptance/Admin/CreatingItemsAdminTest.php
@@ -356,8 +356,9 @@ class CreatingItemsAdminTest extends AdminTestCase
         $this->check("editval[default]");
         $this->clickAndWaitFrame("oLockButton", 'list');
         $this->clickDeleteListItem(3);
-        $this->assertEquals("Deutsch", $this->getText("//tr[@id='row.1']/td[3]"));
-        $this->assertEquals("English", $this->getText("//tr[@id='row.2']/td[3]"));
+        $this->clickAndWait("link=Language");
+        $this->assertElementPresent("link=Deutsch");
+        $this->assertElementPresent("link=English");
         $this->assertElementNotPresent("//tr[@id='row.3']");
         //trying to delete lang with id = 0. it must be impossible
         $this->clickDeleteListItem(1);

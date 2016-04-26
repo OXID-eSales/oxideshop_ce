c1178981f1ff0acc3604f2adb8984687637a1f31
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2016-04-26 21:06:50 +0200

    removed session ids from urls by default
    it is not save anymore to transfer session id in urls.
    
    on the other hand there are some valid usecases e.g.
     if you want to support user with disabled cookies,
     shops with extra ssl domain
    
    but for this usecases i suggest to set
    $this->blAllowSidInUrl = true in config.inc.php
    and for default configuration the shop should pass the hardest security tests available
    
    - extract method is sidallowedinurl
    - unit tests

--- source/Core/oxsession.php
+++ source/Core/oxsession.php
@@ -26,7 +26,6 @@
  * @deprecated on b-dev This class should not be used for direct extending. Please use parent class instead.
  *
  */
-
 class oxSession extends \OxidEsales\Eshop\Core\Session
 {
 }

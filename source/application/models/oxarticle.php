abbd65f10172567cce6eb8245cf96df561bd209c
Anton Fedurtsya <anton@fedurtsya.com>
2016-05-13 17:30:26 +0300

    0006369 Improve check for nopic.jpg existance
    oxwArticleDetails@morePics returned always true if nopic.jpg is missing
    
    Related #358

--- source/application/models/oxarticle.php
+++ source/application/models/oxarticle.php
@@ -2205,7 +2205,8 @@ class oxArticle extends oxI18n implements oxIArticle, oxIUrl
             $sPicVal = $this->getPictureUrl($i);
             $sIcoVal = $this->getIconUrl($i);
             if (!$oStr->strstr($sIcoVal, 'nopic_ico.jpg') && !$oStr->strstr($sIcoVal, 'nopic.jpg') &&
-                !$oStr->strstr($sPicVal, 'nopic_ico.jpg') && !$oStr->strstr($sPicVal, 'nopic.jpg')
+                !$oStr->strstr($sPicVal, 'nopic_ico.jpg') && !$oStr->strstr($sPicVal, 'nopic.jpg') &&
+                $sPicVal !== null
             ) {
                 if ($iCntr) {
                     $blMorePic = true;

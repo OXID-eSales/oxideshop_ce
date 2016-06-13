1df7b7bdb75b64982f35c82971b3183c774f5db6
hkreuter <heike.reuter@oxid-esales.com>
2016-06-13 10:36:56 +0200

    Fix setup.sql

--- source/Setup/Sql/database.sql
+++ source/Setup/Sql/database.sql
@@ -2434,7 +2434,7 @@ CREATE TABLE `oxuser` (
 # Data for table `oxuser`
 #
 INSERT INTO `oxuser` (`OXID`, `OXACTIVE`, `OXRIGHTS`, `OXSHOPID`, `OXUSERNAME`, `OXPASSWORD`, `OXPASSSALT`, `OXCUSTNR`, `OXUSTID`, `OXCOMPANY`, `OXFNAME`, `OXLNAME`, `OXSTREET`, `OXSTREETNR`, `OXADDINFO`, `OXCITY`, `OXCOUNTRYID`, `OXSTATEID`, `OXZIP`, `OXFON`, `OXFAX`, `OXSAL`, `OXBONI`, `OXCREATE`, `OXREGISTER`, `OXPRIVFON`, `OXMOBFON`, `OXBIRTHDATE`, `OXURL`, `OXUPDATEKEY`, `OXUPDATEEXP`, `OXPOINTS`) VALUES
-(@defaultAdminId, 1, 'malladmin', 'oxbaseshop', 'admin', 'ac81eb18344087cdaf5e83f31b3e10a82dec17db499daf0aa11937731e9abd3ff4ec29808a9827946505fd1665b3ed1ed6be9d1c04c44dcbfa920c84fe2e3ba1', '61646D696E', 1, '', 'Your Company Name', 'John', 'Doe', 'Maple Street', '2425', '', 'Any City', 'a7c40f631fc920687.20179984', '', '9041', '217-8918712', '217-8918713', 'MR', 1000, '2003-01-01 00:00:00', '2003-01-01 00:00:00', '', '', '0000-00-00', '', '', 0, 0, 0);
+(@defaultAdminId, 1, 'malladmin', 'oxbaseshop', 'admin', 'ac81eb18344087cdaf5e83f31b3e10a82dec17db499daf0aa11937731e9abd3ff4ec29808a9827946505fd1665b3ed1ed6be9d1c04c44dcbfa920c84fe2e3ba1', '61646D696E', 1, '', 'Your Company Name', 'John', 'Doe', 'Maple Street', '2425', '', 'Any City', 'a7c40f631fc920687.20179984', '', '9041', '217-8918712', '217-8918713', 'MR', 1000, '2003-01-01 00:00:00', '2003-01-01 00:00:00', '', '', '0000-00-00', '', '', 0, 0);
 #
 # Table structure for table `oxuserbaskets`
 #

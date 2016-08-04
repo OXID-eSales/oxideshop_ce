6e4a7d20b496caec27dbf1397fb78eace2b3574a
Alfred Bez <alfred.bez@googlemail.com>
2016-08-04 12:09:24 +0200

    re-worded comment in code

--- source/Application/Model/UserBasket.php
+++ source/Application/Model/UserBasket.php
@@ -306,7 +306,7 @@ class UserBasket extends \oxBase
             }
 
             if (!$dAmount) {
-                // if amount = 0 the means remove it
+                // amount = 0 removes the item
                 $oUserBasketItem->delete();
                 if (isset($this->_aBasketItems[$this->_getItemKey($sProductId, $aSel, $aPersParam)])) {
                     unset($this->_aBasketItems[$this->_getItemKey($sProductId, $aSel, $aPersParam)]);

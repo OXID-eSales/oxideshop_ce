427a0f6eee0da594da1aa6f47378ac6d9e3bf5d6
Anton Fedurtsya <anton@fedurtsya.com>
2016-09-27 11:26:16 +0300

    ESDEV-3869 Exclude migrations tables from checking the timestamp field

--- tests/Integration/Timestamp/TimestampTest.php
+++ tests/Integration/Timestamp/TimestampTest.php
@@ -193,7 +193,10 @@ class TimestampTest extends \OxidTestCase
     public function testAllTablesHasOxTimestamp()
     {
         $oDb = oxDb::getDb();
-        $sQ = "SHOW FULL tables WHERE Table_Type = 'BASE TABLE'";
+        $sQ = "SELECT TABLE_NAME FROM information_schema.tables 
+          WHERE TABLE_TYPE='BASE TABLE' 
+            AND TABLE_NAME NOT LIKE 'oxmigrations%'
+            AND TABLE_SCHEMA = DATABASE()";
         $aTableNames = $oDb->getAll($sQ);
         foreach ($aTableNames as $sKey => $aTable) {
             $sTableName = $aTable[0];

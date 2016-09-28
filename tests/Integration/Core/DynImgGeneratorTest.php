c39d51ce76c2eeff25fb9be735b01a60375ca721
Robert Blank <robert.blank@oxid-esales.com>
2016-09-28 17:33:32 +0200

    ESDEV-4008 #397 Refactored DynamicImageGenerator.php
    Bug ID 0006291
    
    - a new protected method validateImageFileExtension was introduced, which validates the file extension (case-insensitive)
    - refactored _getImageType() to use extracted validateImageFileExtension()
    - refactored _generateImage() to use the file extension of the source file to choose the proper image generation method
    - refactored _generateImage() do more validation:
    	- file is not regenerated, if it already exists and requested dimensions match. There should be no inherent business logic in this low lever method.
    	  If regeneration of files is wanted. The file has to be deleted first, either manually or programatically.
    	- file is not generated, if file extension of source and target do not match
    	- it is checked, that the source file exists
    	- the file lock on the target is always released, not just in case of successful image generation
    - the correct content type header is sent, if a nopic.jpg is shown instead of the requested, but non-existing image
    - updated .htaccess to use case insensitive RewriteRules and thus to be able to process SOME_PIC.JPG
    - updated .htaccess to use allow JPEG extension as well
    - added tests
    - fixed tests

---
+++ tests/Integration/Core/DynImgGeneratorTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ * @version   OXID eShop CE
+ */
+namespace Integration\Core;
+
+use oxDynImgGenerator;
+
+/**
+ * Tests for Dynamic Image generation
+ */
+class DynImgGeneratorTest extends \OxidTestCase
+{
+    /**
+     * When a non-existent image is requested a jpeg image named nopic.jpeg is returned instead,
+     * Test for the proper HTTP status code
+     */
+    public function testRequestNonExistentImageReturnsProperHttpStatusCode()
+    {
+        $shopUrl = $this->getConfig()->getShopUrl(1);
+        $command = 'curl -I -s ' . $shopUrl . '/out/pictures/generated/product/1/87_87_75/wrongname.JPEG';
+        $response = shell_exec($command);
+
+        $this->assertNotNull($response, 'This command failed to execute: ' . $command);
+        $this->assertContains('HTTP/1.1 404 Not Found', $response, 'When an image file is not found the HTTP status code 404 is returned');
+    }
+
+    /**
+     * When a non-existent image is requested a jpeg image named nopic.jpeg is returned instead,
+     * Test for the proper content type header.
+     */
+    public function testRequestNonExistentImageReturnsProperContentHeader()
+    {
+        $shopUrl = $this->getConfig()->getShopUrl(1);
+        $command = 'curl -I -s ' . $shopUrl . '/out/pictures/generated/product/1/87_87_75/wrongname.png';
+        $response = shell_exec($command);
+
+        $this->assertNotNull($response, 'This command failed to execute: ' . $command);
+        $this->assertContains(strtolower('Content-Type: image/jpeg;'), strtolower($response), '');
+    }
+}
---
+++ tests/Integration/Http/HtaccessTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link          http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2015
+ * @version       OXID eShop CE
+ */
+namespace Integration\Http;
+
+
+class HtaccessTest extends \OxidTestCase
+{
+    public function testHtaccessRewrite301ForRedirectedFileExtensions()
+    {
+        $shopUrl = $this->getConfig()->getShopUrl(1);
+        $command = 'curl -I -s ' . $shopUrl . '/file.someExtension' ;
+        $response = shell_exec($command);
+
+        $this->assertNotNull($response, 'This command failed to execute: ' . $command);
+        $this->assertContains('301 Moved Permanently', $response, 'All files with a not defined extension are redirected with code "301 Moved Permanently" ' . $command);
+
+    }
+
+    /**
+     * @dataProvider dataProviderTestHtaccessRewriteForFileExtensions
+     *
+     * @param $fileExtension
+     * @param $message
+     */
+    public function testHtaccessRewrite301ForNotRedirectedFileExtensions($fileExtension, $message)
+    {
+        $shopUrl = $this->getConfig()->getShopUrl(1);
+        $command = 'curl -I -s ' . $shopUrl . '/file.' . $fileExtension;
+        $response = shell_exec($command);
+
+        $this->assertNotNull($response, 'This command failed to execute: ' . $command);
+        $this->assertNotContains('301 Moved Permanently', $response, $message);
+
+    }
+
+    public function dataProviderTestHtaccessRewriteForFileExtensions()
+    {
+        return [
+            ['html', 'html files are not redirected with 301'],
+            ['jpg', 'jpg files are not redirected with 301'],
+            ['jpeg', 'jpeg files are not redirected with 301'],
+            ['css', 'css files are not redirected with 301'],
+            ['pdf', 'pdf files are not redirected with 301'],
+            ['doc', 'doc files are not redirected with 301'],
+            ['gif', 'gif files are not redirected with 301'],
+            ['png', 'png files are not redirected with 301'],
+            ['js', 'js files are not redirected with 301'],
+            ['htc', 'htc files are not redirected with 301'],
+            ['svg', 'svg files are not redirected with 301'],
+            ['HTML', 'HTML FILES ARE NOT REDIRECTED WITH 301'],
+            ['JPG', 'JPG FILES ARE NOT REDIRECTED WITH 301'],
+            ['JPEG', 'JPEG FILES ARE NOT REDIRECTED WITH 301'],
+            ['CSS', 'CSS FILES ARE NOT REDIRECTED WITH 301'],
+            ['PDF', 'PDF FILES ARE NOT REDIRECTED WITH 301'],
+            ['DOC', 'DOC FILES ARE NOT REDIRECTED WITH 301'],
+            ['GIF', 'GIF FILES ARE NOT REDIRECTED WITH 301'],
+            ['PNG', 'PNG FILES ARE NOT REDIRECTED WITH 301'],
+            ['JS', 'JS FILES ARE NOT REDIRECTED WITH 301'],
+            ['HTC', 'HTC FILES ARE NOT REDIRECTED WITH 301'],
+            ['SVG', 'SVG FILES ARE NOT REDIRECTED WITH 301'],
+        ];
+    }
+}

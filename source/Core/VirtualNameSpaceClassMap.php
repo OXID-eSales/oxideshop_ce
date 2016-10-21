ca5dbe8001934640e65087cd2ebf0144ffb0a9ca
Robert Blank <robert.blank@oxid-esales.com>
2016-10-21 13:04:35 +0200

    ESDEV-3991 Add VirtualNameSpaceClassMap

---
+++ source/Core/VirtualNameSpaceClassMap.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link          http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ * @version       OXID eShop CE
+ */
+
+namespace OxidEsales\EshopCommunity;
+
+/**
+ * @inheritdoc
+ */
+class VirtualNameSpaceClassMap extends \OxidEsales\Eshop\Core\Edition\ClassMap
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function getOverridableMap()
+    {
+        return [
+            // 'OxidEsales\EshopCommunity\Application\Model\User' => 'OxidEsales\Eshop\Application\Model\User',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getNotOverridableMap()
+    {
+        return [];
+    }
+}

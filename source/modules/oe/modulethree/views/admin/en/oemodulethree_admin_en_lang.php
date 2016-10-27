271bdb01e7846fdcfb1a2ab3597c945fe120d392
Robert Blank <robert.blank@oxid-esales.com>
2016-10-27 15:22:23 +0200

    ESDEV-3884 Add module 3

---
+++ source/modules/oe/modulethree/views/admin/en/oemodulethree_admin_en_lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @category      module
+ * @package       modulethree
+ * @author        John Doe
+ * @link          www.johndoe.com
+ * @copyright (C) John Doe 20162016
+ */
+
+
+$sLangName = 'English';
+
+$aLang = array(
+    'charset'     => 'UTF-8',
+    'oemodulethree' => 'OXID eSales example module for v6.0 - 3',
+);

f14b7d65361e8df445e1ec1c11a7a37bc34b779d
Robert Blank <robert.blank@oxid-esales.com>
2016-11-03 18:03:16 +0100

    ESDEV-4107 Fix wrong classesnames

--- source/Application/Model/oxstate.php
+++ source/Application/Model/oxstate.php
@@ -27,6 +27,6 @@
  *
  * @deprecated on b-dev This class should not be used for direct extending. Please use parent class instead.
  */
-class State extends \OxidEsales\EshopCommunity\Application\Model\State
+class oxState extends \OxidEsales\EshopCommunity\Application\Model\State
 {
 }

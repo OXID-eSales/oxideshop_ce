ec703ea14e336967914aa8e33d9c2e3a77901310
sebbbbauer <i@sebastianbauer.name>
2016-11-04 08:24:17 +0100

    ESDEV-3883: correct license indentation

--- tests/Integration/Core/Database/Adapter/Doctrine/ResultSetBaseTest.php
+++ tests/Integration/Core/Database/Adapter/Doctrine/ResultSetBaseTest.php
@@ -15,9 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @link          http://www.oxid-esales.com
+ * @link      http://www.oxid-esales.com
  * @copyright (C) OXID eSales AG 2003-2016
- * @version       OXID eShop CE
+ * @version   OXID eShop CE
  */
 
 namespace OxidEsales\EshopCommunity\Tests\Integration\Core\Database\Adapter\Doctrine;

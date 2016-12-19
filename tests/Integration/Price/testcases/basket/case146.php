4ef316c6a3b247a5fdef0c5e29c44cb64e0105ae
sebbbbauer <sebastian.bauer@oxid-esales.com>
2016-12-19 15:25:15 +0100

    ESDEV-3576: change test case encoding to utf8

--- tests/Integration/Price/testcases/basket/case146.php
+++ tests/Integration/Price/testcases/basket/case146.php
@@ -4,7 +4,7 @@
  * Price view mode: Brutto;
  * Product count: 4;
  * VAT info:  count of used vat =1(17%);
- * Currency rate: 0.68€ ;
+ * Currency rate: 0.68Â€ ;
  * Discounts: -;
  * Wrapping:  -;
  * Gift cart: -;

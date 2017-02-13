8e0dbed08876879265b2b1f42139af653b1f1479
hkreuter <heike.reuter@oxid-esales.com>
2017-02-13 16:12:52 +0100

    ESDEV-4268 Add acceptance test for module metadata 2.0

---
+++ tests/Acceptance/Admin/testData/modules/test_module_controller_routing/test_module_controller_routing_other.tpl
@@ -0,0 +1,11 @@
+<h1 class="page-header">Test module for controller routing noNS other template - [{$the_module_message}]</h1>
+
+<form action="[{$oViewConf->getSelfActionLink()}]" name="MyModuleControllerAction" method="post" role="form">
+    <div>
+        [{$oViewConf->getHiddenSid()}]
+        <input type="hidden" name="cl" value="test_module_controller_routing_MyOtherModuleController">
+        <input type="hidden" name="fnc" value="displayMessage">
+        <input type="text" size="10" maxlength="200" name="mymodule_message" value="[{$the_module_message}]">
+        <button type="submit" id="MyModuleControllerActionButton" class="submitButton">[{oxmultilang ident="SUBMIT"}]</button>
+    </div>
+</form>
---
+++ tests/Acceptance/Admin/testData/modules/test_module_controller_routing_ns/MyModuleController.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2017
+ * @version   OXID eShop CE
+ */
+
+namespace OxidEsales\EshopCommunity\Tests\Acceptance\Admin\testData\modules\test_module_controller_routing_ns;
+
+use OxidEsales\Eshop\Core\Registry;
+
+class MyModuleController extends \OxidEsales\Eshop\Application\Controller\FrontendController
+{
+    /**
+     * Current view template
+     *
+     * @var string
+     */
+    protected $_sThisTemplate = 'test_module_controller_routing_ns.tpl';
+
+    /**
+     * Message from request
+     */
+     protected $message = '';
+
+    /**
+     * Rendering method.
+     *
+     * @return mixed
+     */
+    public function render()
+    {
+        $template = parent::render();
+
+        return $template;
+    }
+
+    /**
+     * Display message.
+     */
+    public function displayMessage()
+    {
+        $this->_aViewData['the_module_message'] =  $this->getMessage();
+        $this->render();
+    }
+
+    /**
+     * Template variable getter. Returns entered message
+     *
+     * @return object
+     */
+    public function getMessage()
+    {
+        $this->message = (string) Registry::getConfig()->getRequestParameter('mymodule_message');
+
+        return $this->message;
+    }
+}

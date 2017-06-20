a4962d88fe45409cd9fe74a663b5c17db2ea13c9
Ambrosius Topor <ambrosius.topor@oxid-esales.com>
2017-06-20 11:03:44 +0200

    ESDEV-4575 Stabilize Unit tests

--- tests/Unit/Application/Controller/ShopControlTest.php
+++ tests/Unit/Application/Controller/ShopControlTest.php
@@ -24,7 +24,6 @@ namespace OxidEsales\EshopCommunity\Tests\Unit\Application\Controller;
 use modDB;
 use \Exception;
 use \oxException;
-use OxidEsales\EshopCommunity\Core\Exception\RoutingException;
 use OxidEsales\EshopCommunity\Core\Exception\ConnectionException;
 use OxidEsales\EshopCommunity\Core\Exception\ExceptionToDisplay;
 use OxidEsales\EshopCommunity\Core\Output;
@@ -356,6 +355,8 @@ class ShopControlTest extends \OxidTestCase
         }
         $this->getConfig()->setConfigParam('sTheme', 'azure');
 
+        $controllerClassName = 'content';
+
         oxTestModules::addFunction('oxUtils', 'isSearchEngine', '{ return false; }');
         oxTestModules::addFunction('oxUtils', 'setHeader', '{}');
 
@@ -370,12 +371,11 @@ class ShopControlTest extends \OxidTestCase
         $aTasks = array("isAdmin", "_log", "_startMonitor", "getConfig", "_stopMonitor", '_getOutputManager', '_executeMaintenanceTasks');
 
         $oOut = $this->getMock(\OxidEsales\Eshop\Core\Output::class, array('output', 'flushOutput', 'sendHeaders'));
-        $oOut->expects($this->once())->method('output')->with($this->equalTo('content'));
+        $oOut->expects($this->once())->method('output')->with($this->equalTo($controllerClassName));
         $oOut->expects($this->once())->method('flushOutput')->will($this->returnValue(null));
         $oOut->expects($this->once())->method('sendHeaders')->will($this->returnValue(null));
 
-        $oSmarty = $this->getMock("Smarty", array('fetch'));
-        $oSmarty->expects($this->once())->method('fetch')->with($this->equalTo("page/info/content.tpl"));
+        $oSmarty = $this->getSmartyMock($this->getTemplateName($controllerClassName));
 
         $oUtilsView = $this->getMock(\OxidEsales\Eshop\Core\UtilsView::class, array('getSmarty'));
         $oUtilsView->expects($this->any())->method('getSmarty')->will($this->returnValue($oSmarty));
@@ -387,7 +387,7 @@ class ShopControlTest extends \OxidTestCase
         $oControl->expects($this->any())->method('_getOutputManager')->will($this->returnValue($oOut));
         $oControl->expects($this->atLeastOnce())->method('_executeMaintenanceTasks');
 
-        $oControl->UNITprocess("content", null);
+        $oControl->UNITprocess($controllerClassName, null);
     }
 
     public function testProcessJson()
@@ -396,6 +396,8 @@ class ShopControlTest extends \OxidTestCase
             $this->markTestSkipped('This test is for Community/Professional edition only.');
         }
         $this->getConfig()->setConfigParam('sTheme', 'azure');
+
+        $controllerClassName = 'content';
 
         oxTestModules::addFunction('oxUtils', 'isSearchEngine', '{ return false; }');
         oxTestModules::addFunction('oxUtils', 'setHeader', '{}');
@@ -415,11 +417,10 @@ class ShopControlTest extends \OxidTestCase
         $oOut = $this->getMock(\OxidEsales\Eshop\Core\Output::class, array('output', 'flushOutput', 'sendHeaders', 'setOutputFormat'));
         $oOut->expects($this->at(0))->method('setOutputFormat')->with($this->equalTo(oxOutput::OUTPUT_FORMAT_JSON));
         $oOut->expects($this->at(1))->method('sendHeaders')->will($this->returnValue(null));
-        $oOut->expects($this->at(3))->method('output')->with($this->equalTo('content'), $this->anything());
+        $oOut->expects($this->at(3))->method('output')->with($this->equalTo($controllerClassName), $this->anything());
         $oOut->expects($this->at(4))->method('flushOutput')->will($this->returnValue(null));
 
-        $oSmarty = $this->getMock("Smarty", array('fetch'));
-        $oSmarty->expects($this->once())->method('fetch')->with($this->equalTo("page/info/content.tpl"));
+        $oSmarty = $this->getSmartyMock($this->getTemplateName($controllerClassName));
 
         $oUtilsView = $this->getMock(\OxidEsales\Eshop\Core\UtilsView::class, array('getSmarty'));
         $oUtilsView->expects($this->any())->method('getSmarty')->will($this->returnValue($oSmarty));
@@ -432,7 +433,7 @@ class ShopControlTest extends \OxidTestCase
         $oControl->expects($this->any())->method('_getErrors')->will($this->returnValue(array()));
         $oControl->expects($this->atLeastOnce())->method('_executeMaintenanceTasks');
 
-        $oControl->UNITprocess("content", null);
+        $oControl->UNITprocess($controllerClassName, null);
     }
 
     public function testProcessJsonWithErrors()
@@ -441,6 +442,8 @@ class ShopControlTest extends \OxidTestCase
             $this->markTestSkipped('This test is for Community/Professional edition only.');
         }
         $this->getConfig()->setConfigParam('sTheme', 'azure');
+
+        $controllerClassName = 'content';
 
         oxTestModules::addFunction('oxUtils', 'isSearchEngine', '{ return false; }');
         oxTestModules::addFunction('oxUtils', 'setHeader', '{}');
@@ -468,11 +471,10 @@ class ShopControlTest extends \OxidTestCase
             )
         );
         $oOut->expects($this->at(2))->method('sendHeaders')->will($this->returnValue(null));
-        $oOut->expects($this->at(3))->method('output')->with($this->equalTo('content'), $this->anything());
+        $oOut->expects($this->at(3))->method('output')->with($this->equalTo($controllerClassName), $this->anything());
         $oOut->expects($this->at(4))->method('flushOutput')->will($this->returnValue(null));
 
-        $oSmarty = $this->getMock("Smarty", array('fetch'));
-        $oSmarty->expects($this->once())->method('fetch')->with($this->equalTo("page/info/content.tpl"));
+        $oSmarty = $this->getSmartyMock($this->getTemplateName($controllerClassName));
 
         $oUtilsView = $this->getMock(\OxidEsales\Eshop\Core\UtilsView::class, array('getSmarty'));
         $oUtilsView->expects($this->any())->method('getSmarty')->will($this->returnValue($oSmarty));
@@ -496,7 +498,7 @@ class ShopControlTest extends \OxidTestCase
 
         $oControl->expects($this->any())->method('_getErrors')->will($this->returnValue($aErrors));
 
-        $oControl->UNITprocess("content", null);
+        $oControl->UNITprocess($controllerClassName, null);
     }
 
     /**
@@ -668,6 +670,34 @@ class ShopControlTest extends \OxidTestCase
         $control->expects($this->once())->method('handleRoutingException')->with($this->equalTo($routingException));
 
         $control->start();
+    }
+
+    /**
+     * Check that fetch method returns expected template name.
+     * Could be useful as an integrational test to test that template from controller is set to Smarty
+     *
+     * @param $expectedTemplate
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getSmartyMock($expectedTemplate)
+    {
+        $oSmarty = $this->getMock("Smarty", array('fetch'));
+        $oSmarty->expects($this->once())->method('fetch')->with($this->equalTo($expectedTemplate));
+
+        return $oSmarty;
+    }
+
+    /**
+     * @param $controllerClassName
+     * @return mixed
+     */
+    private function getTemplateName($controllerClassName)
+    {
+        $control = oxNew($controllerClassName);
+        $control->render();
+        $expectedTemplate = $control->getTemplateName();
+        return $expectedTemplate;
     }
 
 }

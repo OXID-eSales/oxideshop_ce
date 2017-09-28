7b8268fc6dc83e4c6c2a9cfb2067fb169cf8df24
Florian Engelhardt <florian.engelhardt@bergfreunde.de>
2017-09-28 23:25:52 +0200

    fixed docblock

--- source/Core/OnlineVatIdCheck.php
+++ source/Core/OnlineVatIdCheck.php
@@ -86,8 +86,6 @@ class OnlineVatIdCheck extends \OxidEsales\Eshop\Core\CompanyVatInChecker
      * @param string $sErrStr  error message
      * @param string $sErrFile error file
      * @param int    $iErrLine error line
-     *
-     * @return void
      */
     public function catchWarning($iErrNo, $sErrStr, $sErrFile, $iErrLine)
     {

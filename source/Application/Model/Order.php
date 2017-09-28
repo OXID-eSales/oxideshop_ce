b1090e9c00902c28c9ef5828ae23045d7443b479
Florian Engelhardt <florian.engelhardt@bergfreunde.de>
2017-09-28 23:31:55 +0200

    Fixed order class constant
    Related #602

--- source/Application/Model/Order.php
+++ source/Application/Model/Order.php
@@ -67,15 +67,6 @@ class Order extends \OxidEsales\Eshop\Core\Model\BaseModel
     /**
      * Protection parameters used for some data in order are invalid
      *
-     * @deprecated since v6.0.0 (2018-01-05); Use ORDER_STATE_INVALIDDELADDRESSCHANGED instead.
-     *
-     * @var int
-     */
-    const ORDER_STATE_INVALIDDElADDRESSCHANGED = 7;
-
-    /**
-     * Protection parameters used for some data in order are invalid
-     *
      * @var int
      */
     const ORDER_STATE_INVALIDDELADDRESSCHANGED = 7;
@@ -2067,7 +2058,7 @@ class Order extends \OxidEsales\Eshop\Core\Model\BaseModel
 
         $iState = 0;
         if ($sDelAddressMD5 != $sDeliveryAddress || !$blFieldsValid) {
-            $iState = self::ORDER_STATE_INVALIDDElADDRESSCHANGED;
+            $iState = self::ORDER_STATE_INVALIDDELADDRESSCHANGED;
         }
 
         return $iState;

bcb1c65280269a34f59e6168b8b288bd580b69b7
iegupov <igor.iegupov@gmail.com>
2018-03-19 12:11:28 +0100

    OXDEV-802 Fix namespace for integration RatingTest

--- tests/Integration/Application/Model/RatingTest.php
+++ tests/Integration/Application/Model/RatingTest.php
@@ -3,7 +3,7 @@
  * Copyright © OXID eSales AG. All rights reserved.
  * See LICENSE file for license details.
  */
-namespace OxidEsales\EshopCommunity\Tests\Unit\Application\Model;
+namespace OxidEsales\EshopCommunity\Tests\Integration\Application\Model;
 
 use OxidEsales\Eshop\Application\Model\Article;
 use OxidEsales\Eshop\Application\Model\Rating;

cc2d87301308d6811591cfbd26755695e607df5d
Ambrosius Topor <ambrosius.topor@oxid-esales.com>
2018-03-20 13:17:15 +0100

    OXDEV-802 Fix namespace in UserRatingServiceInterface

--- source/Internal/Review/Service/UserRatingServiceInterface.php
+++ source/Internal/Review/Service/UserRatingServiceInterface.php
@@ -4,7 +4,7 @@
  * See LICENSE file for license details.
  */
 
-namespace OxidEsales\EshopCommunity\Internal\Service;
+namespace OxidEsales\EshopCommunity\Internal\Review\Service;
 
 use Doctrine\Common\Collections\ArrayCollection;
 

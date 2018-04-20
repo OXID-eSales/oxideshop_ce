d73710299aae3c838b2315637452eb0b7d8d7dbb
iegupov <igor.iegupov@gmail.com>
2018-04-20 16:05:27 +0200

    OXDEV-720 Replace old logging mechanism usages

--- source/Core/Utils.php
+++ source/Core/Utils.php
@@ -1400,15 +1400,10 @@ class Utils extends \OxidEsales\Eshop\Core\Base
      */
     public function writeToLog($logMessage, $logFileName)
     {
-        $logFilePath = $this->getConfig()->getLogsDir() . $logFileName;
-        $writeSucceed = false;
+        $logger = Registry::getLogger();
+        $logger->notice($logMessage);
 
-        if (($logFileResource = fopen($logFilePath, 'a')) !== false) {
-            fwrite($logFileResource, $logMessage);
-            $writeSucceed = fclose($logFileResource);
-        }
-
-        return $writeSucceed;
+        return true;
     }
 
     /**

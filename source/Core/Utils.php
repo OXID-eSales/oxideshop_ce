75a088c975ec34946e808bb1615607892008db0e
Dimitri Archipov <d.archipov@shopmacher.de>
2018-07-04 10:19:44 +0200

    fixed race tmp path

--- source/Core/Utils.php
+++ source/Core/Utils.php
@@ -1384,7 +1384,7 @@ class Utils extends \OxidEsales\Eshop\Core\Base
     {
         $sCache = "<?php\n\$aLangCache = " . var_export($aLangCache, true) . ";\n?>";
         $sFileName = $this->getCacheFilePath($sCacheName);
-        $sTmpFile = 'tmp/' . basename($sFileName) . '.' . uniqid('', true) . '.tmp';
+        $sTmpFile = '/tmp/' . basename($sFileName) . '.' . uniqid('', true) . '.tmp';
         $blRes = file_put_contents($sTmpFile, $sCache, LOCK_EX);
 
         rename($sTmpFile, $sFileName);

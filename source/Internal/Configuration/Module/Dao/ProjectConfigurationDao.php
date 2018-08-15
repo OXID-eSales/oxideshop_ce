ae65374bf987ee7ed5f6cb7125d621f9904eb27d
iegupov <igor.iegupov@gmail.com>
2018-08-15 11:11:56 +0200

    OXDEV-1284 Add dummy project configuration dao

---
+++ source/Internal/Configuration/Module/Dao/ProjectConfigurationDao.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Configuration\Module\Dao;
+
+use OxidEsales\EshopCommunity\Internal\Configuration\Module\DataObject\ProjectConfiguration;
+
+/**
+ * @internal
+ */
+class ProjectConfigurationDao implements ProjectConfigurationDaoInterface
+{
+    /**
+     * @return ProjectConfiguration
+     */
+    public function getConfiguration(): ProjectConfiguration
+    {
+        // TODO: Implement getConfiguration() method.
+    }
+
+    /**
+     * @param ProjectConfiguration $configuration
+     */
+    public function persistConfiguration(ProjectConfiguration $configuration)
+    {
+        // TODO: Implement persistConfiguration() method.
+    }
+}
---
+++ tests/Unit/Internal/Configuration/Module/Dao/ProjectConfigurationDaoTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Configuration\Module\Dao;
+
+/**
+ * @internal
+ */
+class ProjectConfigurationDaoTest
+{
+
+}

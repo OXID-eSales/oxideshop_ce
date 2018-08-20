db7e82373bd49a8c566b197fbeb35d36047dd856
iegupov <igor.iegupov@gmail.com>
2018-08-20 16:52:06 +0200

    OXDEV-1284 Improve ProjectConfigurationDaoTest

--- tests/Integration/Internal/Configuration/Module/Dao/ProjectConfigurationDaoTest.php
+++ tests/Integration/Internal/Configuration/Module/Dao/ProjectConfigurationDaoTest.php
@@ -8,11 +8,14 @@ declare(strict_types=1);
 
 namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Configuration\Module\Dao;
 
-use OxidEsales\EshopCommunity\Internal\Application\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Application\ContainerBuilder;
+use OxidEsales\EshopCommunity\Internal\Common\Storage\YamlFileStorage;
 use OxidEsales\EshopCommunity\Internal\Configuration\Module\Dao\ProjectConfigurationDaoInterface;
 use OxidEsales\EshopCommunity\Internal\Configuration\Module\DataObject\EnvironmentConfiguration;
 use OxidEsales\EshopCommunity\Internal\Configuration\Module\DataObject\ProjectConfiguration;
+use OxidEsales\TestingLibrary\VfsStreamWrapper;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * @internal
@@ -39,6 +42,43 @@ class ProjectConfigurationDaoTest extends TestCase
 
     private function getContainer()
     {
-        return ContainerFactory::getInstance()->getContainer();
+        $containerBuilder = new ContainerBuilder();
+
+        $container = $containerBuilder->getContainer();
+        $container->set(
+            'oxid_esales.configuration.module.project_configuration_yaml_file_storage',
+            new YamlFileStorage(
+                new FileLocator(),
+                $this->getTestConfigurationFilePath()
+            )
+        );
+
+        $projectConfigurationDaoDefinition = $container->getDefinition(ProjectConfigurationDaoInterface::class);
+        $projectConfigurationDaoDefinition->setPublic(true);
+
+        $container->setDefinition(
+            ProjectConfigurationDaoInterface::class,
+            $projectConfigurationDaoDefinition
+        );
+
+        $container->compile();
+
+        return $container;
+    }
+
+    /**
+     * @return string
+     */
+    private function getTestConfigurationFilePath(): string
+    {
+        $vfsStreamWrapper = new VfsStreamWrapper();
+        $relativePath = 'test/testProjectConfigurationDao.yaml';
+        $path = $vfsStreamWrapper->getRootPath() . $relativePath;
+
+        if (!is_file($path)) {
+            $vfsStreamWrapper->createFile($relativePath);
+        }
+
+        return $path;
     }
 }

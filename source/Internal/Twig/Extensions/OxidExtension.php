3155354b9cd2140c6b21ed79c8cbdd9487b21c3c
Tomasz Kowalewski <t.kowalewski@createit.pl>
2018-09-12 16:34:41 +0200

    OXDEV-1376 Prepare Twig extensions
    Created 4 base Twig extensions and loaded the in TwigEngine constructor

---
+++ source/Internal/Twig/Extensions/OxidExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OxidEsales\EshopCommunity\Internal\Twig\Extensions;
+
+use Twig\Extension\AbstractExtension;
+
+/**
+ * Class OxidExtension
+ */
+class OxidExtension extends AbstractExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+        ];
+    }
+}
---
+++ source/Internal/Twig/Extensions/SmartyExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OxidEsales\EshopCommunity\Internal\Twig\Extensions;
+
+use Twig\Extension\AbstractExtension;
+
+/**
+ * Class SmartyExtension
+ */
+class SmartyExtension extends AbstractExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+        ];
+    }
+}

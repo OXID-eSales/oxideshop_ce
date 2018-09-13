ba6d900b6822a9bf8e1fd5ab5ab58172b7d96a9c
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-09-13 13:15:33 +0200

    OXDEV-1376 added missing math functions
    Added missing mathematical functions to twig. Since all of these functions exist in php, I decided to add them in one commit.

--- source/Internal/Twig/Extensions/MathExtension.php
+++ source/Internal/Twig/Extensions/MathExtension.php
@@ -3,6 +3,7 @@
 namespace OxidEsales\EshopCommunity\Internal\Twig\Extensions;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
 /**
  * Class MathExtension
@@ -15,6 +16,14 @@ class MathExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
+            new TwigFunction('cos', 'cos'),
+            new TwigFunction('sin', 'sin'),
+            new TwigFunction('tan', 'tan'),
+            new TwigFunction('exp', 'exp'),
+            new TwigFunction('log', 'log'),
+            new TwigFunction('log10', 'log10'),
+            new TwigFunction('pi', 'pi'),
+            new TwigFunction('sqrt', 'sqrt'),
         ];
     }
 }

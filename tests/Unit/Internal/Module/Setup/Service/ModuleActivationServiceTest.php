41a5f2839138f5540bee7eeca991db466bf81679
iegupov <igor.iegupov@gmail.com>
2018-10-08 17:57:22 +0200

    OXDEV-1284 Move ModuleActivationServiceTest

--- tests/Unit/Internal/Module/Setup/Service/ModuleActivationServiceTest.php
+++ tests/Unit/Internal/Module/Setup/Service/ModuleActivationServiceTest.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * See LICENSE file for license details.
  */
 
-namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Module\Setup;
+namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Module\Setup\Service;
 
 use OxidEsales\EshopCommunity\Internal\Module\Configuration\DataObject\ModuleConfiguration;
 use OxidEsales\EshopCommunity\Internal\Module\Configuration\DataObject\ModuleSetting;

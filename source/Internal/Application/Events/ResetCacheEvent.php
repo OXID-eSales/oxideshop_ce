7b860b8560f2537d0fd879eba3006f49611c6e5c
hkreuter <heike@reuter-consultig.org>
2018-10-10 09:28:19 +0200

    OXDEV-1387 Add ResetCacheEvent

---
+++ source/Internal/Application/Events/ResetCacheEvent.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Application\Events;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class ResetCacheEvent
+ *
+ * @package OxidEsales\EshopCommunity\Internal\Application\Events
+ */
+class ResetCacheEvent extends Event
+{
+    /**
+     * Handle event.
+     *
+     * @return null
+     */
+    public function handleEvent()
+    {
+
+    }
+}

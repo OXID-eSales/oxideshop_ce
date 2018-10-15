305c233c07f81dccd972f2cc82f3411aa7c2f6bf
robertblank <robert.blank@oxid-esales.com>
2018-10-15 19:34:20 +0200

    Revert "OXDEV-1551 Deprecate methods and properties"
    This reverts commit 10aba1523f4a55fe5ae634b2b71df44ae7606fe4.
    Deprecations are not allowed in a patch release.

--- source/Core/Email.php
+++ source/Core/Email.php
@@ -272,11 +272,7 @@ class Email extends \PHPMailer
      */
     protected $_sCharSet = null;
 
-    /**
-     * @deprecated since v6.3.1 (2018-10-01); This property will be removed completely.
-     *
-     * @var \OxidEsales\Eshop\Core\Config
-     */
+    /** @var \OxidEsales\Eshop\Core\Config */
     protected $_oConfig = null;
 
     /**
@@ -327,8 +323,6 @@ class Email extends \PHPMailer
     }
 
     /**
-     * @deprecated since v6.3.1 (2018-10-01); This method will be removed completely. Use OxidEsales\Eshop\Core\Registry::getConfig() in the future.
-     *
      * oxConfig instance getter
      *
      * @return \OxidEsales\Eshop\Core\Config
@@ -343,8 +337,6 @@ class Email extends \PHPMailer
     }
 
     /**
-     * @deprecated since v6.3.1 (2018-10-01); This method will be removed completely. Use OxidEsales\Eshop\Core\Registry::getConfig()->setConfigParam() in the future.
-     *
      * oxConfig instance setter
      *
      * @param \OxidEsales\Eshop\Core\Config $config config object

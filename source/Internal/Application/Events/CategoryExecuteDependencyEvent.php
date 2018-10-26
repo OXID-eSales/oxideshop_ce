1272a5e1528b2852cded7811f3c502706062a805
hkreuter <heike@reuter-consultig.org>
2018-10-26 11:14:25 +0200

    OXDEV-1387 Add CategoryExecuteDependencyEvent

---
+++ source/Internal/Application/Events/CategoryExecuteDependencyEvent.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+namespace OxidEsales\EshopCommunity\Internal\Application\Events;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class CategoryExecuteDependencyEvent
+ *
+ * @package OxidEsales\EshopCommunity\Internal\Application\Events
+ */
+class CategoryExecuteDependencyEvent extends Event
+{
+    const NAME = 'oxidesales.category.executedependencyevent';
+
+    /**
+     * Category ids
+     *
+     * @var array
+     */
+    protected $categoryIds = [];
+
+    /**
+     * Flush articles flag.
+     *
+     * @var bool
+     */
+    protected $flushArticles = true;
+
+    /**
+     * Check if articles need to be flushed.
+     *
+     * @return bool
+     */
+    public function isFlushArticles()
+    {
+        return $this->flushArticles;
+    }
+
+    /**
+     * Setter for flush articles flag.
+     *
+     * @param bool $flushArticles Flush articles yes/no
+     */
+    public function setFlushArticles($flushArticles)
+    {
+        $this->flushArticles = $flushArticles;
+    }
+
+    /**
+     * Getter for category ids
+     *
+     * @return array
+     */
+    public function getCategoryIds()
+    {
+        return $this->categoryIds;
+    }
+
+    /**
+     * Setter for category ids.
+     *
+     * @param array $categoryIds
+     */
+    public function setCategoryIds($categoryIds)
+    {
+        $this->categoryIds = $categoryIds;
+    }
+
+    /**
+     * Handle event.
+     *
+     * @return null
+     */
+    public function handleEvent()
+    {
+    }
+}

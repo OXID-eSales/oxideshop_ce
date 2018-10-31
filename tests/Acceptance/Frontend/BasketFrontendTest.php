1bd88e89b888671b9f7421f808d40d032d11470a
hkreuter <heike@reuter-consultig.org>
2018-10-31 12:55:12 +0100

    fixup! 786fd4a24d43f6c927e2c2e3a8837ae66ba091e6

--- tests/Acceptance/Frontend/BasketFrontendTest.php
+++ tests/Acceptance/Frontend/BasketFrontendTest.php
@@ -1191,7 +1191,7 @@ class BasketFrontendTest extends FrontendTestCase
 
         //Order Step4
         $popupWindowId = microtime(true);
-        $this->openWindow($this->getSubShopAwareUrl(shopURL), $popupWindowId, true);
+        $this->openWindow(str_replace(':6081', '',shopURL), $popupWindowId, true);
         $this->open($this->getSubShopAwareUrl(shopURL . "en/my-address/"));
         $this->waitForText("%SHIPPING_ADDRESSES%");
         $this->click("userChangeShippingAddress");
@@ -1206,7 +1206,8 @@ class BasketFrontendTest extends FrontendTestCase
         $this->_continueToNextStep();
         $this->assertTextNotPresent("%ERROR_DELIVERY_ADDRESS_WAS_CHANGED_DURING_CHECKOUT%");
         //changing billing address once more
-        $this->openWindow($this->getSubShopAwareUrl(shopURL), $popupWindowId, true);
+        $this->openWindow(str_replace(':6081', '',shopURL), $popupWindowId, true);
+        #$this->openWindow(shopURL, $popupWindowId, true);
         $this->open($this->getSubShopAwareUrl(shopURL . "en/my-address/"));
         $this->waitForText("%SHIPPING_ADDRESSES%");
         $this->click("userChangeAddress");

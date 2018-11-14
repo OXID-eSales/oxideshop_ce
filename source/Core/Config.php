2479f979e0814b5bf137523e3e4c16df48d192f2
robertblank <robert.blank@oxid-esales.com>
2018-11-14 09:40:12 +0100

    OXDEV-1551 Use $this->getModuleDir() in Config

--- source/Core/Config.php
+++ source/Core/Config.php
@@ -1495,7 +1495,7 @@ class Config extends \OxidEsales\Eshop\Core\Base
 
         if (!$finalTemplatePath) {
             $templatePathCalculator = $this->getModuleTemplatePathCalculator();
-            $templatePathCalculator->setModulesPath(\OxidEsales\Eshop\Core\Registry::getConfig()->getModulesDir());
+            $templatePathCalculator->setModulesPath($this->getModulesDir());
             try {
                 $finalTemplatePath = $templatePathCalculator->calculateModuleTemplatePath($templateName);
             } catch (Exception $e) {

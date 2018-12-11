47a17b6cd3dc9642900458272474f80b0d9c51ce
robertblank <robert.blank@oxid-esales.com>
2018-12-11 18:21:59 +0100

    OXDEV-1284 Add missing TestData

---
+++ tests/Integration/Internal/Module/MetaData/TestData/TestModuleWithSurplusData/metadata.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'ID'          => 'TestModuleWithSurplusData',
+    'extraStuff' => [
+        'key1' => 'value1'
+    ],
+    'moreExtraStuff' => new \StdClass()
+];

32fabe4725444d6cbd2d7477b9a2f21e9b763e2a
Mantas Vaitkunas <manvait@gmail.com>
2019-04-11 15:11:15 +0300

    OXDEV-1287 Remove misleading comment

--- source/Core/SystemEventHandler.php
+++ source/Core/SystemEventHandler.php
@@ -137,9 +137,6 @@ class SystemEventHandler
      */
     public function onAdminLogin()
     {
-        // Checks if newer versions of modules are available.
-        // Will be used by the upcoming online one click installer.
-        // Is still under development - still changes at the remote server are necessary - therefore ignoring the results for now
         try {
             if ($this->isSendingShopDataEnabled()) {
                 $this->getOnlineModuleVersionNotifier()->versionNotify();

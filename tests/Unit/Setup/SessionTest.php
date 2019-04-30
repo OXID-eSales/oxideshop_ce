a83d0de9d3195e3a73c1efde3fbbd8996a1279c8
hkreuter <heike@reuter-consultig.org>
2019-04-30 10:08:01 +0200

    OXDEV-1821 Update Setup Session test

--- tests/Unit/Setup/SessionTest.php
+++ tests/Unit/Setup/SessionTest.php
@@ -13,7 +13,7 @@ use OxidEsales\EshopCommunity\Setup\Session;
 class SessionTest extends \OxidTestCase
 {
 
-    public function setUpBeforeTestSuite()
+    public function setUp()
     {
         session_cache_limiter("no-cache");
 
@@ -90,6 +90,10 @@ class SessionTest extends \OxidTestCase
 
         $oSession = $this->_getSessionMock();
         $oSession->setIsNewSession('test');
+
+        //we need to start a session for this test
+        session_start();
+
         $oSession->UNITgetNewSessionID();
         $this->assertSame(true, $oSession->getIsNewSession());
     }

b31a8a1fe57a78618fc9f100d032849bf6935a58
Tomasz Kowalewski <t.kowalewski@createit.pl>
2019-08-27 11:02:15 +0200

    OXDEV-2318 Parametrize delete queries in Application/Model

--- source/Application/Model/Article.php
+++ source/Application/Model/Article.php
@@ -4537,8 +4537,10 @@ class Article extends \OxidEsales\Eshop\Core\Model\MultiLanguageModel implements
             ':articleId' => $articleId
         ]);
 
-        $sDelete = 'delete from oxobject2attribute where oxobjectid = ' . $quotedArticleId . ' ';
-        $oDb->execute($sDelete);
+        $sDelete = 'delete from oxobject2attribute where oxobjectid = :articleId';
+        $oDb->execute($sDelete, [
+            ':articleId' => $articleId
+        ]);
 
         $sDelete = 'delete from oxobject2category where oxobjectid = ' . $quotedArticleId . ' ';
         $oDb->execute($sDelete);

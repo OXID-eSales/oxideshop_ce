b74123b2eec9ad88f44eb01b607abf134b822f33
Abbas Afsharfarnia <afshar5152@gmail.com>
2019-09-10 10:12:12 +0200

    OXDEV-2435 #0006966 fix article admin activation

--- source/Application/views/admin/tpl/article_list.tpl
+++ source/Application/views/admin/tpl/article_list.tpl
@@ -113,7 +113,7 @@ window.onload = function ()
         [{if $listitem->oxarticles__oxid->value == $oxid}]
             [{assign var="listclass" value=listitem4}]
         [{/if}]
-        <td valign="top" class="[{$listclass}][{if $listitem->oxarticles__oxactive->value == 1}] active[{/if}]" height="15"><div class="listitemfloating">&nbsp</a></div></td>
+        <td valign="top" class="[{$listclass}][{if $listitem->showActiveCheckInAdminPanel}] active[{/if}]" height="15"><div class="listitemfloating">&nbsp</a></div></td>
         <td valign="top" class="[{$listclass}]"><div class="listitemfloating"><a href="Javascript:top.oxid.admin.editThis('[{$listitem->oxarticles__oxid->value}]');" class="[{$listclass}]">[{$listitem->oxarticles__oxartnum->value}]</a></div></td>
         <td valign="top" class="[{$listclass}]" height="15"><div class="listitemfloating">&nbsp;<a href="Javascript:top.oxid.admin.editThis('[{$listitem->oxarticles__oxid->value}]');" class="[{$listclass}]">[{$listitem->pwrsearchval|oxtruncate:200:"..":false}]</a></div></td>
         <td valign="top" class="[{$listclass}]"><div class="listitemfloating"><a href="Javascript:top.oxid.admin.editThis('[{$listitem->oxarticles__oxid->value}]');" class="[{$listclass}]">[{$listitem->oxarticles__oxshortdesc->value|strip_tags|oxtruncate:45:"..":true}]</a></div></td>
--- tests/Acceptance/Admin/ProductInfoAdminTest.php
+++ tests/Acceptance/Admin/ProductInfoAdminTest.php
@@ -6,8 +6,15 @@
 
 namespace OxidEsales\EshopCommunity\Tests\Acceptance\Admin;
 
+use OxidEsales\Eshop\Core\DatabaseProvider;
+use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\EshopCommunity\Tests\Acceptance\AdminTestCase;
 
+/**
+ * Class ProductInfoAdminTest
+ *
+ * @package OxidEsales\EshopCommunity\Tests\Acceptance\Admin
+ */
 class ProductInfoAdminTest extends AdminTestCase
 {
     /**
@@ -26,6 +33,30 @@ class ProductInfoAdminTest extends AdminTestCase
         $this->type("editval[oxarticles__oxtitle]", "[DE 2] Test product 2 šÄßüл €");
         $this->clickAndWaitFrame("saveArticle", 'list');
         $this->assertEquals("[DE 2] Test product 2 šÄßüл €", $this->getValue("editval[oxarticles__oxtitle]"));
+    }
+
+    /**
+     * Test showing activation check if from/to date was set
+     *
+     * @group productAdmin
+     */
+    public function testActivationCheckIsShown()
+    {
+        $fromDate = strval(intval(date("Y")) - 2) . '-01-01 00:00:00';
+        $toDate = strval(intval(date("Y")) + 2) . '-01-01 00:00:00';
+        $query = "UPDATE `oxarticles` SET `oxactive` = 0, `oxactivefrom` = ?, `oxactiveto` = ? WHERE `oxartnum` = '1401'";
+        DatabaseProvider::getDb()->execute($query, [$fromDate, $toDate]);
+
+        Registry::getConfig()->saveShopConfVar("bool", "blUseTimeCheck", true);
+
+        $this->loginAdmin("Administer Products", "Products");
+        $this->type("where[oxarticles][oxartnum]", "1401");
+        $this->clickAndWaitFrame("submitit", 'list');
+        $this->openListItem("link=1401");
+
+        $this->frame('list');
+        $activationCheckElement = $this->getElementLazy($this->translate("//*[@id='row.1']/td[1]"));
+        $this->assertTrue($activationCheckElement->hasClass('active'));
     }
 
     /**
@@ -49,6 +80,4 @@ class ProductInfoAdminTest extends AdminTestCase
         $this->assertEquals("99,99 EUR", $this->getText("//tr[@id='row.1']/td[6]"));
         $this->assertEquals("100,00 EUR", $this->getText("//tr[@id='row.1']/td[7]"));
     }
-
 }
-

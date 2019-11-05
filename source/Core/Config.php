65a37d30de1a4a6a624cb05c00f3265ce2a2e6b7
Abbas Afsharfarnia <afshar5152@gmail.com>
2019-11-05 12:53:34 +0100

    OXDEV-555 add option for having other shops news in multishops

--- source/Core/Config.php
+++ source/Core/Config.php
@@ -146,6 +146,17 @@ class Config extends \OxidEsales\Eshop\Core\Base
     protected $blNativeImages = true;
 
     /**
+     * Only for multishops
+     * Unload news from all shops in multishop.
+     * If $blOtherShopNews is set to true the multishop does not load news from all shops,
+     * This is applicable for depending on mall
+     * if mall mode is available.
+     *
+     * @var bool
+     */
+    protected $blDoNotLoadAllShopNews = true;
+
+    /**
      * Names of tables which are multi-shop
      *
      * @var array

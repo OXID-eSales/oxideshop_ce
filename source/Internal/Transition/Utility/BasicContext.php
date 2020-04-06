08f567b2e5772c2a71525081566ff41f7535bb5c
iegupov <igor.iegupov@gmail.com>
2020-04-06 16:44:05 +0200

    OXDEV-3330 Use cache directory from the config file

--- source/Internal/Transition/Utility/BasicContext.php
+++ source/Internal/Transition/Utility/BasicContext.php
@@ -11,6 +11,7 @@ namespace OxidEsales\EshopCommunity\Internal\Transition\Utility;
 
 use OxidEsales\EshopCommunity\Core\Autoload\BackwardsCompatibilityClassMapProvider;
 use OxidEsales\EshopCommunity\Core\ShopIdCalculator;
+use OxidEsales\Facts\Config\ConfigFile;
 use OxidEsales\Facts\Edition\EditionSelector;
 use OxidEsales\Facts\Facts;
 use Webmozart\PathUtil\Path;
@@ -32,12 +33,11 @@ class BasicContext implements BasicContextInterface
     private $facts;
 
     /**
-     * @todo change placement of container cache file and move logic to Facts.
      * @return string
      */
     public function getContainerCacheFilePath(): string
     {
-        return Path::join($this->getSourcePath(), 'tmp', 'container_cache.php');
+        return Path::join($this->getCacheDirectory(), 'container_cache.php');
     }
 
     /**
@@ -114,17 +114,6 @@ class BasicContext implements BasicContextInterface
     }
 
     /**
-     * @return Facts
-     */
-    private function getFacts(): Facts
-    {
-        if ($this->facts === null) {
-            $this->facts = new Facts();
-        }
-        return $this->facts;
-    }
-
-    /**
      * @return array
      */
     public function getAllShopIds(): array
@@ -180,5 +169,21 @@ class BasicContext implements BasicContextInterface
     public function getConfigTableName(): string
     {
         return 'oxconfig';
+    }
+
+    private function getCacheDirectory(): string
+    {
+        return (new ConfigFile())->getVar('sCompileDir');
+    }
+
+    /**
+     * @return Facts
+     */
+    private function getFacts(): Facts
+    {
+        if ($this->facts === null) {
+            $this->facts = new Facts();
+        }
+        return $this->facts;
     }
 }

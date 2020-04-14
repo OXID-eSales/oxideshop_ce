725bcad11f856d3f88edc38c0c17eb6b8a2760ad
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2020-04-14 17:27:02 +0200

    OXDEV-3303 Restore empty migration file

--- source/migration/data/Version20180214152228.php
+++ source/migration/data/Version20180214152228.php
@@ -5,19 +5,21 @@
  * See LICENSE file for license details.
  */
 
+declare(strict_types=1);
+
 namespace OxidEsales\EshopCommunity\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /** Empty migration file to suppress warning */
 class Version20180214152228 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
     }
 }

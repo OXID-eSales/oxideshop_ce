187bb223c826643e756459491088fe76f05419a2
iegupov <igor.iegupov@gmail.com>
2020-08-19 15:31:17 +0200

    OXDEV-3696 Use active modules template namespaces

---
+++ source/Internal/Framework/Module/Configuration/Service/ActiveModulesDataProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Service;
+
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDaoInterface;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\State\ModuleStateServiceInterface;
+use OxidEsales\EshopCommunity\Internal\Transition\Utility\ContextInterface;
+
+class ActiveModulesDataProvider implements ActiveModulesDataProviderInterface
+{
+    /**
+     * @var ShopConfigurationDaoInterface
+     */
+    private $shopConfigurationDao;
+
+    /**
+     * @var ModuleStateServiceInterface
+     */
+    private $moduleStateService;
+
+    /**
+     * @var ContextInterface
+     */
+    private $context;
+
+    public function __construct(
+        ShopConfigurationDaoInterface $shopConfigurationDao,
+        ModuleStateServiceInterface $moduleStateService,
+        ContextInterface $context
+    ) {
+        $this->shopConfigurationDao = $shopConfigurationDao;
+        $this->moduleStateService = $moduleStateService;
+        $this->context = $context;
+    }
+
+    public function getModuleIds(): array
+    {
+        $moduleIds = [];
+
+        foreach ($this->getActiveModuleConfigurations() as $moduleConfiguration) {
+            $moduleIds[] = $moduleConfiguration->getId();
+        }
+
+        return $moduleIds;
+    }
+
+    private function getActiveModuleConfigurations(): array
+    {
+        $moduleConfigurations = [];
+        $shopId = $this->context->getCurrentShopId();
+
+        foreach ($this->shopConfigurationDao->get($shopId)->getModuleConfigurations() as $moduleConfiguration) {
+            if ($this->moduleStateService->isActive($moduleConfiguration->getId(), $shopId)) {
+                $moduleConfigurations[] = $moduleConfiguration;
+            }
+        }
+
+        return $moduleConfigurations;
+    }
+}
--- source/Internal/Framework/Templating/Provider/NamespaceHierarchyProvider.php
+++ source/Internal/Framework/Templating/Provider/NamespaceHierarchyProvider.php
@@ -10,30 +10,24 @@ declare(strict_types=1);
 namespace OxidEsales\EshopCommunity\Internal\Framework\Templating\Provider;
 
 use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDaoInterface;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Service\ActiveModulesDataProviderInterface;
 use OxidEsales\EshopCommunity\Internal\Transition\Utility\ContextInterface;
 
 class NamespaceHierarchyProvider implements NamespaceHierarchyProviderInterface
 {
-    /** @var ShopConfigurationDaoInterface */
-    private $shopConfigurationDao;
-    /** @var ContextInterface */
-    private $context;
+    /**
+     * @var ActiveModulesDataProviderInterface
+     */
+    private $activeModulesDataProvider;
 
-    public function __construct(
-        ShopConfigurationDaoInterface $shopConfigurationDao,
-        ContextInterface $context
-    ) {
-        $this->shopConfigurationDao = $shopConfigurationDao;
-        $this->context = $context;
+    public function __construct(ActiveModulesDataProviderInterface $activeModulesDataProvider)
+    {
+        $this->activeModulesDataProvider = $activeModulesDataProvider;
     }
 
     /** @inheritDoc */
     public function getHierarchyAscending(): array
     {
-        $moduleHierarchy = $this->shopConfigurationDao
-            ->get($this->context->getCurrentShopId())
-            ->getModuleIdsOfModuleConfigurations();
-
-        return array_reverse($moduleHierarchy);
+        return $this->activeModulesDataProvider->getModuleIds();
     }
 }

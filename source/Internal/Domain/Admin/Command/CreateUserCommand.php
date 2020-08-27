79a470070d2fbb63827268d9152d9a5119e225e2
vilmal <vilma.liorensaityte@gmail.com>
2020-08-27 16:35:59 +0200

    OXDEV-3906 - Add create admin user command
    (cherry picked from commit 627e66a96b94b09ec4a151ce1cf40a18269778ae)

---
+++ source/Internal/Domain/Admin/Command/CreateUserCommand.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\EshopCommunity\Internal\Domain\Admin\Command;
+
+use OxidEsales\EshopCommunity\Internal\Domain\Admin\DataObject\Admin;
+use OxidEsales\EshopCommunity\Internal\Domain\Admin\Exception\InvalidEmailException;
+use OxidEsales\EshopCommunity\Internal\Domain\Admin\Service\AdminUserServiceInterface;
+use OxidEsales\EshopCommunity\Internal\Framework\Console\Command\NamedArgumentsTrait;
+use OxidEsales\EshopCommunity\Internal\Transition\Utility\BasicContextInterface;
+use OxidEsales\EshopCommunity\Internal\Utility\Email\EmailValidatorServiceInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    use NamedArgumentsTrait;
+
+    private const ADMIN_EMAIL = 'admin-email';
+    private const ADMIN_PASSWORD = 'admin-password';
+
+    /**
+     * @var EmailValidatorServiceInterface
+     */
+    private $emailValidatorService;
+
+    /**
+     * @var AdminUserServiceInterface
+     */
+    private $adminService;
+
+    /**
+     * @var BasicContextInterface
+     */
+    private $basicContext;
+
+    /**
+     * AdminUserSetupCommand constructor.
+     * @param EmailValidatorServiceInterface $emailValidatorService
+     * @param AdminUserServiceInterface      $adminService
+     * @param BasicContextInterface          $basicContext
+     */
+    public function __construct(
+        EmailValidatorServiceInterface $emailValidatorService,
+        AdminUserServiceInterface $adminService,
+        BasicContextInterface $basicContext
+    ) {
+        parent::__construct();
+
+        $this->emailValidatorService = $emailValidatorService;
+        $this->adminService = $adminService;
+        $this->basicContext = $basicContext;
+    }
+
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this
+            ->addOption(self::ADMIN_EMAIL, null, InputOption::VALUE_REQUIRED)
+            ->addOption(self::ADMIN_PASSWORD, null, InputOption::VALUE_REQUIRED);
+        $this->setDescription('Creates admin user');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->checkRequiredCommandOptions($this->getDefinition()->getOptions(), $input);
+
+        $this->validateAdminEmail($input->getOption(self::ADMIN_EMAIL));
+
+        $output->writeln('<info>Creating administrator account...</info>');
+        $this->createAdmin($input);
+
+        $output->writeln('<info>Administrator account has been created.</info>');
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param string $email
+     * @throws InvalidEmailException
+     */
+    private function validateAdminEmail(string $email): void
+    {
+        if (!$this->emailValidatorService->isEmailValid($email)) {
+            throw new InvalidEmailException($email);
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     */
+    private function createAdmin(InputInterface $input): void
+    {
+        $this->adminService->createAdmin(
+            $input->getOption(self::ADMIN_EMAIL),
+            $input->getOption(self::ADMIN_PASSWORD),
+            Admin::MALL_ADMIN,
+            $this->basicContext->getDefaultShopId()
+        );
+    }
+}
---
+++ tests/Integration/Internal/Domain/Admin/Command/CreateUserCommandTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Domain\Admin\Command;
+
+use OxidEsales\EshopCommunity\Application\Model\User;
+use OxidEsales\EshopCommunity\Internal\Domain\Admin\Exception\InvalidEmailException;
+use OxidEsales\EshopCommunity\Tests\Integration\Internal\ContainerTrait;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+final class CreateUserCommandTest extends TestCase
+{
+    use ContainerTrait;
+
+    private const ADMIN_EMAIL = 'someone@test.com';
+
+    public function tearDown(): void
+    {
+        $user = new User();
+        $user->delete($user->getIdByUserName(self::ADMIN_EMAIL));
+
+        parent::tearDown();
+    }
+
+    public function testExecuteWithMissingArgs(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $commandTester = new CommandTester($this->getCommand());
+        $commandTester->execute([]);
+    }
+
+    public function testExecuteWithInvalidAdminEmail(): void
+    {
+        $this->expectException(InvalidEmailException::class);
+        $commandTester = new CommandTester($this->getCommand());
+        $commandTester->execute([
+            '--admin-email' => 'admin',
+            '--admin-password' => 'admin',
+        ]);
+    }
+
+    public function testExecuteWithCompleteArgs(): void
+    {
+        $commandTester = new CommandTester($this->getCommand());
+        $exitCode = $commandTester->execute([
+            '--admin-email' => self::ADMIN_EMAIL,
+            '--admin-password' => 'some-admin-pass'
+        ]);
+
+        $this->assertSame(Command::SUCCESS, $exitCode);
+        $this->assertUserExists();
+    }
+
+    private function getCommand(): Command
+    {
+        return $this->get('console.command_loader')->get('oe:admin:create-user');
+    }
+
+    private function assertUserExists()
+    {
+        $this->assertNotFalse((new User())->getIdByUserName(self::ADMIN_EMAIL));
+    }
+}

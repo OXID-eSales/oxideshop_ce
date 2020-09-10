214148bb9469c4aeab14f29e6965e4b8ca28b531
iegupov <igor.iegupov@gmail.com>
2020-09-10 10:49:57 +0200

    OXDEV-3696 Fix tests

--- tests/Unit/Internal/Framework/Templating/Resolver/LegacyTemplateNameResolverTest.php
+++ tests/Unit/Internal/Framework/Templating/Resolver/LegacyTemplateNameResolverTest.php
@@ -21,9 +21,7 @@ class LegacyTemplateNameResolverTest extends \PHPUnit\Framework\TestCase
     public function testResolveSmartyTemplate($templateName, $response): void
     {
         $resolver = new LegacyTemplateNameResolver(
-            new TemplateNameResolver(
-                $this->getTemplateEngineMock('tpl')
-            )
+            new TemplateNameResolver('tpl')
         );
 
         $this->assertSame($response, $resolver->resolve($templateName));
@@ -61,9 +59,7 @@ class LegacyTemplateNameResolverTest extends \PHPUnit\Framework\TestCase
     public function testResolveTwigTemplate($response, $templateName): void
     {
         $resolver = new LegacyTemplateNameResolver(
-            new TemplateNameResolver(
-                $this->getTemplateEngineMock('html.twig')
-            )
+            new TemplateNameResolver('html.twig')
         );
 
         $this->assertSame($response, $resolver->resolve($templateName));

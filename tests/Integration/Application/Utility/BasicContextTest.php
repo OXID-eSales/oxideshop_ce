60bbe9d13ce4ea580c75b44524fb7bb86904cbd2
iegupov <igor.iegupov@gmail.com>
2020-10-01 15:00:13 +0200

    OXDEV-3873 Remove test for non existent functionality

--- tests/Integration/Application/Utility/BasicContextTest.php
+++ tests/Integration/Application/Utility/BasicContextTest.php
@@ -12,9 +12,7 @@ namespace OxidEsales\EshopCommunity\Tests\Integration\Application\Utility;
 use OxidEsales\EshopCommunity\Internal\Container\BootstrapContainerFactory;
 use OxidEsales\EshopCommunity\Internal\Transition\Utility\BasicContextInterface;
 use OxidEsales\EshopCommunity\Tests\Integration\Internal\ContainerTrait;
-use OxidEsales\Facts\Config\ConfigFile;
 use PHPUnit\Framework\TestCase;
-use Webmozart\PathUtil\Path;
 
 final class BasicContextTest extends TestCase
 {
@@ -40,17 +38,5 @@ final class BasicContextTest extends TestCase
     public function testGetDefaultShopId()
     {
         $this->assertSame(1, $this->basicContext->getDefaultShopId());
-    }
-
-    public function testGetModulePathCacheFilePath()
-    {
-        $getModulePathCacheFilePath = $this->basicContext->getModulePathCacheFilePath(1);
-
-        $expected = Path::join(
-            (new ConfigFile())->getVar('sCompileDir'),
-            'modules/1/module_path_cache.txt'
-        );
-
-        self::assertSame($expected, $getModulePathCacheFilePath);
     }
 }

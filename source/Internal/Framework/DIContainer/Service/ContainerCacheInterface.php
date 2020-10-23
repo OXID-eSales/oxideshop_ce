d13705983862602bd8b8780d4ed4f3ca330ea898
iegupov <igor.iegupov@gmail.com>
2020-10-23 10:56:58 +0200

    OXDEV-4053 Update ContainerCacheInterface

--- source/Internal/Framework/DIContainer/Service/ContainerCacheInterface.php
+++ source/Internal/Framework/DIContainer/Service/ContainerCacheInterface.php
@@ -9,13 +9,14 @@ declare(strict_types=1);
 
 namespace OxidEsales\EshopCommunity\Internal\Framework\DIContainer\Service;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 interface ContainerCacheInterface
 {
     public function put(ContainerBuilder $container): void;
 
-    public function get(): \ProjectServiceContainer;
+    public function get(): ContainerInterface;
 
     public function exists(): bool;
 

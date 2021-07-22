b9cc9279c370fca45da3040bda41ef9358a29a9d
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2021-07-22 10:26:38 +0200

    OXDEV-4787 Modify group_by clause

--- tests/Integration/Internal/Domain/Newsletter/DataMapper/NewsletterRecipientsDataMapperTest.php
+++ tests/Integration/Internal/Domain/Newsletter/DataMapper/NewsletterRecipientsDataMapperTest.php
@@ -9,50 +9,23 @@ declare(strict_types=1);
 
 namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Domain\Newsletter\DataMapper;
 
-use OxidEsales\EshopCommunity\Internal\Domain\Newsletter\Bridge\NewsletterRecipientsDaoBridgeInterface;
 use OxidEsales\EshopCommunity\Internal\Domain\Newsletter\DataMapper\NewsletterRecipientsDataMapper;
-use OxidEsales\EshopCommunity\Internal\Domain\Newsletter\DataMapper\NewsletterRecipientsDataMapperInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Database\QueryBuilderFactoryInterface;
-use OxidEsales\EshopCommunity\Internal\Transition\Adapter\ShopAdapterInterface;
-use OxidEsales\EshopCommunity\Tests\Integration\Internal\ContainerTrait;
-use OxidEsales\TestingLibrary\Services\Library\DatabaseRestorer\DatabaseRestorer;
+use OxidEsales\EshopCommunity\Internal\Domain\Newsletter\DataObject\NewsletterRecipient;
 use PHPUnit\Framework\TestCase;
 
 class NewsletterRecipientsDataMapperTest extends TestCase
 {
-    use ContainerTrait;
-
-    /**
-     * @var object|QueryBuilderFactoryInterface|null
-     */
-    private $queryBuilderFactory;
-
-    /**
-     * @var DatabaseRestorer
-     */
-    private $databaseRestorer;
-
-    protected function setup(): void
-    {
-        $this->databaseRestorer = new DatabaseRestorer();
-        $this->databaseRestorer->dumpDB(__CLASS__);
-        $this->queryBuilderFactory = $this->get(QueryBuilderFactoryInterface::class);
-        $this->prepareTestData();
-
-        parent::setUp();
-    }
-
-    protected function tearDown(): void
-    {
-        $this->databaseRestorer->restoreDB(__CLASS__);
-
-        parent::tearDown();
-    }
-
     public function testMapRecipientListDataToArray(): void
     {
-        $recipientsList = $this->get(NewsletterRecipientsDaoBridgeInterface::class);
-        $data = $recipientsList->getNewsletterRecipients(1);
+        $recipient = new NewsletterRecipient();
+        $recipient
+            ->setEmail('someMail')
+            ->setFistName('Soca')
+            ->setLastName('Warrior')
+            ->setSalutation('no')
+            ->setCountry('Trinidad und Tobago')
+            ->setOtpInState('1')
+            ->setUserGroups('someString');
 
         $mappedArray = [
             [
@@ -65,105 +38,19 @@ class NewsletterRecipientsDataMapperTest extends TestCase
                 NewsletterRecipientsDataMapper::ASSIGNED_USER_GROUPS,
             ],
             [
-                "MR",
-                "John",
-                "Doe",
-                "test_user@test.com",
+                "no",
+                "Soca",
+                "Warrior",
+                "someMail",
                 "subscribed",
-                "Deutschland",
-                "test_group1"
+                "Trinidad und Tobago",
+                "someString"
             ]
         ];
 
         $this->assertEquals(
             $mappedArray,
-            $this->get(NewsletterRecipientsDataMapperInterface::class)->mapRecipientListDataToArray($data)
+            (new NewsletterRecipientsDataMapper())->mapRecipientListDataToArray([$recipient])
         );
-
-    }
-
-    private function prepareTestData(): void
-    {
-        $shopAdapter = $this->get(ShopAdapterInterface::class);
-
-        $testUserId = 'test_user';
-
-        $queryBuilder = $this->queryBuilderFactory->create();
-        $queryBuilder
-            ->insert('oxuser')
-            ->values([
-                'oxid'          => ':id',
-                'OXUSERNAME'      => ':username',
-                'OXRIGHTS'      => ':userRights',
-                'OXCOUNTRYID'      => ':countryId',
-            ])
-            ->setParameters([
-                'id'    => $testUserId,
-                'username' => "test_user@test.com",
-                'userRights' => "malladmin",
-                'countryId' => "a7c40f631fc920687.20179984",
-            ]);
-
-        $queryBuilder->execute();
-
-        $queryBuilder
-            ->insert('oxgroups')
-            ->values([
-                'oxid'          => ':id',
-                'OXTITLE'      => ':title',
-                'OXTITLE_1'      => ':title'
-            ])
-            ->setParameters([
-                'id'    => 'test_group1',
-                'title' => "test_group1"
-            ]);
-
-        $queryBuilder->execute();
-
-        $queryBuilder
-            ->insert('oxobject2group')
-            ->values([
-                'oxid'          => ':id',
-                'OXSHOPID'      => '1',
-                'OXOBJECTID'      => ':userId',
-                'OXGROUPSID'      => ':groupId',
-            ])
-            ->setParameters([
-                'id'      => $shopAdapter->generateUniqueId(),
-                'userId'  => $testUserId,
-                'groupId' => 'test_group1'
-            ]);
-
-        $queryBuilder->execute();
-
-        $queryBuilder
-            ->delete('oxnewssubscribed');
-
-        $queryBuilder->execute();
-
-        $queryBuilder
-            ->insert('oxnewssubscribed')
-            ->values([
-                'oxid'          => ':id',
-                'OXUSERID'      => ':userId',
-                'OXSAL'      => ':sal',
-                'OXFNAME'      => ':fistname',
-                'OXLNAME'      => ':lastname',
-                'OXEMAIL'      => ':email',
-                'OXDBOPTIN'      => ':otpInState',
-                'OXSHOPID'      => ':shopId',
-            ])
-            ->setParameters([
-                'id'    => $shopAdapter->generateUniqueId(),
-                'userId' => $testUserId,
-                'sal' => "MR",
-                'fistname' => "John",
-                'lastname' => "Doe",
-                'email' => "test_user@test.com",
-                'otpInState' => "1",
-                'shopId' => "1"
-            ]);
-
-        $queryBuilder->execute();
     }
 }

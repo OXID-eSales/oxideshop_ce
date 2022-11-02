b10eaef6a9e25fcd852b1604e27aae924f56824a
Deve L. Oper <heike.reuter@oxid-esales.com>
2022-11-02 16:36:31 +0100

    Update test bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,8 +12,8 @@ use \Webmozart\PathUtil\Path;
 # Yes, adding a directory separator is stupid, but that's how the code expects it
 define('OX_BASE_PATH', Path::join(INSTALLATION_ROOT_PATH, 'source') . DIRECTORY_SEPARATOR);
 //define('OX_LOG_FILE', Path::join(OX_BASE_PATH, 'log', 'testrun.log'));
-require Path::join(OX_BASE_PATH, 'oxfunctions.php');
-require Path::join(OX_BASE_PATH, 'overridablefunctions.php');
+require_once Path::join(OX_BASE_PATH, 'oxfunctions.php');
+require_once Path::join(OX_BASE_PATH, 'overridablefunctions.php');
 
 // Configure Registry
 $configFile = new ConfigFile(Path::join(OX_BASE_PATH, 'config.inc.php'));

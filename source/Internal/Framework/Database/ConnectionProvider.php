5f521ee58cf8b3ecb3b4faafff40638d46c88256
Tomasz Kowalewski <t.kowalewski@createit.pl>
2023-05-11 12:10:30 +0200

    OXDEV-6960 Fix conflicting connections issue between container instances

--- source/Internal/Framework/Database/ConnectionProvider.php
+++ source/Internal/Framework/Database/ConnectionProvider.php
@@ -10,23 +10,12 @@ declare(strict_types=1);
 namespace OxidEsales\EshopCommunity\Internal\Framework\Database;
 
 use Doctrine\DBAL\Connection;
-use OxidEsales\Eshop\Core\Database\Adapter\Doctrine\Database;
 use OxidEsales\Eshop\Core\DatabaseProvider;
 
 class ConnectionProvider implements ConnectionProviderInterface
 {
-    private $connection;
-
     public function get(): Connection
     {
-        if ($this->connection === null) {
-            $database = DatabaseProvider::getDb();
-            $r = new \ReflectionMethod(Database::class, 'getConnection');
-            $r->setAccessible(true);
-
-            $this->connection = $r->invoke($database);
-        }
-
-        return $this->connection;
+        return DatabaseProvider::getDb()->getPublicConnection();
     }
 }

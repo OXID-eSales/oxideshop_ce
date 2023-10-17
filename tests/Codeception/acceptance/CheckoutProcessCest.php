2cb15265ad246a23436e24fb6446f40d84a51daf
Jelle Dehn <jelle.dehn@oxid-esales.com>
2023-10-17 17:22:07 +0200

    OXDEV-7508 Move test to EE

--- tests/Codeception/acceptance/CheckoutProcessCest.php
+++ tests/Codeception/acceptance/CheckoutProcessCest.php
@@ -13,7 +13,6 @@ use Codeception\Util\Fixtures;
 use OxidEsales\Codeception\Module\Translation\Translator;
 use OxidEsales\Codeception\Page\Account\UserAccount;
 use OxidEsales\Codeception\Page\Checkout\OrderCheckout;
-use OxidEsales\Codeception\Page\Details\ProductDetails;
 use OxidEsales\Codeception\Step\Basket;
 use OxidEsales\Codeception\Step\UserRegistrationInCheckout;
 use OxidEsales\Facts\Facts;
@@ -815,75 +814,6 @@ final class CheckoutProcessCest
         $orderCheckout->submitOrderSuccessfully();
     }
 
-    /**
-     * @group exclude_from_EE
-     */
-    public function checkFrontendDiscounts(AcceptanceTester $I): void
-    {
-        $I->wantToTest('ordering discounted articles');
-
-        $this->addDiscountedProductsToDatabase($I);
-
-        $homePage = $I->openShop();
-        $userData = $this->getExistingUserData();
-        $homePage->loginUser($userData['userLoginName'], $userData['userPassword']);
-
-        $basket = new Basket($I);
-        $basket->addProductToBasket("1000", 2);
-        $basket->addProductToBasket("1002-1", 1);
-
-        $basket = $homePage->openMiniBasket()->openBasketDisplay();
-
-        $basketItem1 = [
-            'id' => '1000',
-            'title' => 'Test product 0 [EN] šÄßüл',
-            'amount' => 2,
-            'totalPrice' => '90,00 €'
-        ];
-
-        $basketItem2 = [
-            'id' => '1002-1',
-            'title' => 'Test product 2 [EN] šÄßüл, var1 [EN] šÄßüл',
-            'amount' => 1,
-            'totalPrice' => '49,50 €'
-        ];
-
-        $unitPriceItem1 = '45,00 €';
-        $unitPriceItem2 = '49,50 €';
-        $totalPrice = '139,50 €';
-
-        $basket->seeBasketContains([$basketItem1, $basketItem2], $totalPrice);
-        $basket->seeItemUnitPrice($unitPriceItem1, '1');
-        $basket->seeItemUnitPrice($unitPriceItem2, '2');
-
-        $priceInformation = ['net' => '127,31 €', 'gross' => '139,50 €', 'shipping' => '0,00 €', 'total' => '139,50 €'];
-        $this->validateSummaryPrice($I, $priceInformation);
-
-        $basket->updateProductAmount(3, 2);
-        $basketItem2['totalPrice'] = '148,50 €';
-        $basketItem2['amount'] = 3;
-        $totalPrice = '238,50 €';
-        $basket->seeBasketContains([$basketItem1, $basketItem2], $totalPrice);
-
-        // disable discounts
-        $I->updateInDatabase('oxdiscount', ['OXACTIVE' => 0], ['OXID' => 'testcatdiscount']);
-        $I->updateInDatabase('oxdiscount', ['OXACTIVE' => 0], ['OXID' => 'testpercentdiscount']);
-        $basketItem1['totalPrice'] = '100,00 €';
-        $basketItem2['totalPrice'] = '55,00 €';
-        $basketItem2['amount'] = 1;
-        $basket->updateProductAmount($basketItem2['amount'], 2);
-
-        $unitPriceItem1 = '50,00 €';
-        $unitPriceItem2 = '55,00 €';
-        $totalPrice = '155,00 €';
-        $basket->seeItemUnitPrice($unitPriceItem1, '1');
-        $basket->seeItemUnitPrice($unitPriceItem2, '2');
-        $basket->seeBasketContains([$basketItem1, $basketItem2], $totalPrice);
-
-        $priceInformation = ['net' => '141,46 €', 'gross' => '155,00 €', 'shipping' => '0,00 €', 'total' => '155,00 €'];
-        $this->validateSummaryPrice($I, $priceInformation);
-    }
-
     private function getExistingUserData(): array
     {
         return Fixtures::get('existingUser');
@@ -929,62 +859,4 @@ final class CheckoutProcessCest
         ];
     }
 
-    private function validateSummaryPrice(AcceptanceTester $I, array $priceInformation)
-    {
-        $orderCheckout = new OrderCheckout($I);
-
-        $I->see(
-            $priceInformation['net'],
-            sprintf($orderCheckout->basketSummaryNet, Translator::translate('TOTAL_NET'))
-        );
-        $I->see(
-            $priceInformation['gross'],
-            sprintf($orderCheckout->basketSummaryGross, Translator::translate('TOTAL_GROSS'))
-        );
-        $I->see(
-            $priceInformation['shipping'],
-            sprintf($orderCheckout->basketShippingGross, Translator::translate('SHIPPING_COST'))
-        );
-        $I->see(
-            $priceInformation['total'],
-            sprintf($orderCheckout->basketTotalPrice, Translator::translate('GRAND_TOTAL'))
-        );
-    }
-
-    private function addDiscountedProductsToDatabase(AcceptanceTester $I)
-    {
-        $oxdiscountid = 'testcatdiscount';
-        $oxactive = 1;
-
-        $I->updateInDatabase('oxdiscount', ['OXACTIVE' => $oxactive], ['OXID' => $oxdiscountid]);
-        $I->haveInDatabase(
-            'oxobject2discount',
-            [
-                'OXID' => '1000todiscount',
-                'OXDISCOUNTID' => 'testcatdiscount',
-                'OXOBJECTID' => '1000',
-                'OXTYPE' => 'oxarticles'
-            ]
-        );
-
-        $I->haveInDatabase(
-            'oxdiscount',
-            [
-                'OXID' => 'testpercentdiscount',
-                'OXSHOPID' => '1',
-                'OXACTIVE' => 1,
-                'OXADDSUMTYPE' => '%',
-                'OXADDSUM' => '10'
-            ]
-        );
-        $I->haveInDatabase(
-            'oxobject2discount',
-            [
-                'OXID' => '1002-1todiscount',
-                'OXDISCOUNTID' => 'testpercentdiscount',
-                'OXOBJECTID' => '1002-1',
-                'OXTYPE' => 'oxarticles'
-            ]
-        );
-    }
 }

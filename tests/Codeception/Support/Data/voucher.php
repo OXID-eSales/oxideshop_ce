f675863e4b2f5983f14053a935ae33df175def04
Jan Speller <github@speller.biz>
2024-01-30 11:47:53 +0100

    Add Test

--- tests/Codeception/Support/Data/voucher.php
+++ tests/Codeception/Support/Data/voucher.php
@@ -34,5 +34,38 @@ return [
                     'OXID' => 'testcoucher011'
                 ]
             ]
+        ],
+    'testvoucherarticle' =>
+        [
+            'oxvoucherseries' => [
+                'OXID' => 'testvoucherarticle',
+                'OXSHOPID' => 1,
+                'OXSERIENR' => 'Article Coupon šÄßüл',
+                'OXSERIEDESCRIPTION' => 'Article Coupon šÄßüл',
+                'OXDISCOUNT' => 10.00,
+                'OXDISCOUNTTYPE' => 'percent',
+                'OXBEGINDATE' => '2008-01-01 00:00:00',
+                'OXENDDATE' => date('Y-m-d H:i:s', time() + (7 * 24 * 60 * 60)),
+                'OXALLOWSAMESERIES' => 0,
+                'OXALLOWOTHERSERIES' => 0,
+                'OXALLOWUSEANOTHER' => 0,
+                'OXMINIMUMVALUE' => 45.00,
+                'OXCALCULATEONCE' => 1
+            ],
+            'oxvouchers' => [
+                [
+                    'OXDATEUSED' => '0000-00-00',
+                    'OXRESERVED' => 0,
+                    'OXVOUCHERNR' => '123456',
+                    'OXVOUCHERSERIEID' => 'testvoucherarticle',
+                    'OXID' => 'testvoucherarticle001'
+                ]
+            ],
+            'oxobject2discount' => [
+                'OXID' => 'testvouchertoarticle001',
+                'OXDISCOUNTID' => 'testvoucherarticle',
+                'OXOBJECTID' => '10015',
+                'OXTYPE' => 'oxarticles',
+            ]
         ]
 ];

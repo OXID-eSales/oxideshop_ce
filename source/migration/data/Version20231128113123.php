7e4b5e32f71034c97c0e0621cb19bc27d7c487ee
ashraf <ashraf.boudawara@oxid-esales.com>
2024-04-11 18:01:02 +0200

    OXDEV-5103 Fix migrations default value

--- source/migration/data/Version20231128113123.php
+++ source/migration/data/Version20231128113123.php
@@ -26,13 +26,13 @@ final class Version20231128113123 extends AbstractMigration
                 . '"Message, which is shown if the article is in low stock (multilanguage)"'
         );
         $this->addSql(
-            'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKTEXT_1` TEXT NOT NULL default'
+            'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKTEXT_1` TEXT NOT NULL default ""'
         );
         $this->addSql(
-            'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKTEXT_2` TEXT NOT NULL default'
+            'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKTEXT_2` TEXT NOT NULL default ""'
         );
         $this->addSql(
-            'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKTEXT_3` TEXT NOT NULL default'
+            'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKTEXT_3` TEXT NOT NULL default ""'
         );
         $this->addSql(
             'ALTER TABLE `oxarticles` ADD column `OXLOWSTOCKACTIVE` TINYINT(1)'

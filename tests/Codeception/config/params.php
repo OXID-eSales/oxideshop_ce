8bb483b913b30049322374fc7b9516ec55784960
Jörn Ott <joern.ott@ott-consult.de>
2024-04-18 12:38:01 +0200

    Adapt codeception parameters for different root paths

--- tests/Codeception/config/params.php
+++ tests/Codeception/config/params.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 use OxidEsales\Facts\Config\ConfigFile;
 use OxidEsales\Facts\Facts;
 use OxidEsales\Codeception\Module\Database\DatabaseDefaultsFileGenerator;
+use Symfony\Component\Filesystem\Path;
 
 if ($shopRootPath = getenv('SHOP_ROOT_PATH')){
     require_once(Path::join($shopRootPath, 'source', 'bootstrap.php'));

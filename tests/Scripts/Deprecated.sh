2bce9c62bc8143b83b42166bf0365c0dd3d908cc
Jörn Ott <joern.ott@ott-consult.de>
2024-04-24 11:55:23 +0200

    fix runtests script

--- tests/Scripts/Deprecated.sh
+++ tests/Scripts/Deprecated.sh
@@ -4,7 +4,7 @@ cp /var/www/vendor/oxid-esales/testing-library/test_config.yml.dist test_config.
 # sed -e "s|shop_tests_path:.*|shop_tests_path: 'vendor/oxid-esales/tests'|" \
 #     -e "s|shop_path:.*|shop_path: 'vendor/oxid-esales/oxideshop-ce/source'|" \
 #     -i test_config.yml
-php vendor/bin/runtests \
+vendor/bin/runtests \
     --coverage-clover=tests/Reports/deprecated_tests_coverage.xml \
     AllTestsUnit 2>&1 \
 | tee "tests/Output/deprecated_tests.txt"

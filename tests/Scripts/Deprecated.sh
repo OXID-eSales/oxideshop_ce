86c6d90c6f9ca46e61334b2af1e18a5aa7ff7324
Jörn Ott <joern.ott@ott-consult.de>
2024-04-25 12:59:57 +0200

    Try deprecated tests without coverage

--- tests/Scripts/Deprecated.sh
+++ tests/Scripts/Deprecated.sh
@@ -4,8 +4,7 @@ cp vendor/oxid-esales/testing-library/test_config.yml.dist test_config.yml
 # sed -e "s|shop_tests_path:.*|shop_tests_path: 'vendor/oxid-esales/tests'|" \
 #     -e "s|shop_path:.*|shop_path: 'vendor/oxid-esales/oxideshop-ce/source'|" \
 #     -i test_config.yml
-XDEBUG_MODE=coverage vendor/bin/runtests \
-    --coverage-clover=tests/Reports/deprecated_tests_coverage.xml \
+vendor/bin/runtests \
     AllTestsUnit 2>&1 \
 | tee "tests/Output/deprecated_tests.txt"
 

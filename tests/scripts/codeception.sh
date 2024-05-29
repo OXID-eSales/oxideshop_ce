64cd6b105726716583e8d44c819ccb96943789e8
Joern Ott <joern.ott@ott-consult.de>
2024-05-29 12:12:50 +0200

    B 7.0.x leverage composer scripts (#966) (#967)
    * Switch to light workflows using scripts
    * Use install script
    * Determine the branch based on the changelog, if the branch name is problematic

---
+++ tests/scripts/codeception.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+vendor/bin/codecept build \
+    -c tests/codeception.yml
+vendor/bin/codecept run acceptanceSetup \
+    -c tests/codeception.yml \
+    --ext DotReporter 2>&1 \
+| tee tests/Output/codeception_ShopSetup.txt

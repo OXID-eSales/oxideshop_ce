900d05481ce77275e654709612f9d610a8496e00
Joern Ott <joern.ott@ott-consult.de>
2024-06-12 12:47:09 +0200

    OXDEV-8415 Add log parsing to test scripts (#973)

--- tests/scripts/codeception.sh
+++ tests/scripts/codeception.sh
@@ -2,7 +2,47 @@
 set -e
 vendor/bin/codecept build \
     -c tests/codeception.yml
-vendor/bin/codecept run acceptanceSetup \
+RESULT=$?
+echo "Codecept build exited with error code ${RESULT}"
+vendor/bin/codecept run acceptance \
     -c tests/codeception.yml \
     --ext DotReporter 2>&1 \
-| tee tests/Output/codeception_ShopSetup.txt
+| tee tests/Output/codeception_Acceptance.txt
+RESULT=$?
+echo "Codecept run exited with error code ${RESULT}"
+if [ ! -s "tests/Output/codeception_Acceptance.txt" ]; then
+    echo -e "\033[0;31mLog file is empty! Seems like no tests have been run!\033[0m"
+    RESULT=1
+fi
+cat >failure_pattern.tmp <<EOF
+fail
+\\.\\=\\=
+Warning
+Notice
+Deprecated
+Fatal
+Error
+DID NOT FINISH
+Test file ".+" not found
+Cannot open file
+No tests executed
+Could not read
+Warnings: [1-9][0-9]*
+Errors: [1-9][0-9]*
+Failed: [1-9][0-9]*
+Deprecations: [1-9][0-9]*
+Risky: [1-9][0-9]*
+EOF
+sed -e 's|(.*)\r|$1|' -i failure_pattern.tmp
+while read -r LINE ; do
+    if [ -n "${LINE}" ]; then
+        if grep -q -E "${LINE}" "tests/Output/codeception_Acceptance.txt"; then
+            echo -e "\033[0;31m runtest failed matching pattern ${LINE}\033[0m"
+            grep -E "${LINE}" "tests/Output/codeception_Acceptance.txt"
+            RESULT=1
+        else
+            echo -e "\033[0;32m codeception passed matching pattern ${LINE}"
+        fi
+    fi
+done <failure_pattern.tmp
+exit ${RESULT}

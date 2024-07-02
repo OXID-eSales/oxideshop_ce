0ad586b523d79e28f3f5207880c5346a304ad145
Jörn Ott <joern.ott@ott-consult.de>
2024-07-02 10:05:02 +0200

    Fix copying output

--- tests/scripts/shop-setup.sh
+++ tests/scripts/shop-setup.sh
@@ -54,9 +54,7 @@ echo "codecept build exited with error code ${RESULT}"
 RESULT=$?
 echo "codecept run exited with error code ${RESULT}"
 [[ ! -d tests/Output ]] && mkdir tests/Output
-if [ -n "$(find tests/Codeception/_output -type f)" ]; then
-    cp tests/Codeception/_output/* tests/Output
-fi
+find tests/Codeception/_output -type f -exec cp \{\} tests/Output/ \;
 if [ ! -s "tests/Output/codeception_${SUITE}.txt" ]; then
     echo -e "\033[0;31mLog file is empty! Seems like no tests have been run!\033[0m"
     RESULT=1

49bb0140fa671b8b9ed438a77933097c1008fc87
godefroy-le-hardi <igor.iegupov@gmail.com>
2026-02-05 14:59:30 +0100

    OXDEV-9491 Fix after merge

--- source/api.php
+++ source/api.php
@@ -7,13 +7,9 @@
 
 declare(strict_types=1);
 
-use OxidEsales\Eshop\Core\ConfigFile;
-use OxidEsales\EshopCommunity\Core\Autoload\BackwardsCompatibilityAutoload;
-use OxidEsales\EshopCommunity\Core\Registry;
 use OxidEsales\EshopCommunity\Internal\Framework\Api\Api;
 use OxidEsales\EshopCommunity\Internal\Framework\Api\ExceptionHandler;
 use OxidEsales\EshopCommunity\Internal\Framework\Env\DotenvLoader;
-use OxidEsales\Facts\Facts;
 use Symfony\Component\ErrorHandler\Debug;
 
 define('INSTALLATION_ROOT_PATH', dirname(__DIR__));
@@ -22,19 +18,10 @@ define('VENDOR_PATH', INSTALLATION_ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor' . 
 
 require_once VENDOR_PATH . 'autoload.php';
 
-/*
- * Register the backwards compatibility autoloader.
- * This autoloader will load classes for reasons of backwards compatibility like 'oxArticle'.
- */
-require_once (new Facts())->getCommunityEditionSourcePath() . '/Core/Autoload/BackwardsCompatibilityAutoload.php';
-spl_autoload_register([BackwardsCompatibilityAutoload::class, 'autoload']);
-
 require_once INSTALLATION_ROOT_PATH . '/source/oxfunctions.php';
 require_once INSTALLATION_ROOT_PATH . '/source/overridablefunctions.php';
 
 (new DotenvLoader(INSTALLATION_ROOT_PATH))->loadEnvironmentVariables();
-
-Registry::set(ConfigFile::class, new ConfigFile(OX_BASE_PATH . 'config.inc.php'));
 
 set_exception_handler([new ExceptionHandler(), 'handle']);
 

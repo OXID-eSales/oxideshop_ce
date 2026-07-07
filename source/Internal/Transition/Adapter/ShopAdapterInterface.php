400f0d2867e72fa3f51742724f107754a9dbb461
Moritz Demmer <moritz.demmer@oxid-esales.com>
2026-07-07 12:03:45 +0200

    OXDEV-8895 Activate themes and read the active theme from YAML state

--- source/Internal/Transition/Adapter/ShopAdapterInterface.php
+++ source/Internal/Transition/Adapter/ShopAdapterInterface.php
@@ -32,15 +32,5 @@ interface ShopAdapterInterface
 
     public function validateShopId(int $shopId): bool;
 
-    public function getActiveThemesList(): array;
-
-    public function getCustomTheme(): string;
-
-    public function getActiveThemeId(): string;
-
-    public function themeExists(string $themeId): bool;
-
-    public function activateTheme(string $themeId): void;
-
     public function generateDatabaseViewName(string $tableName, int $languageId, int $shopId): string;
 }

d65a189dcbdb4bc0a5f846c0239abbf84362d8ed
Moritz Demmer <moritz.demmer@oxid-esales.com>
2026-07-29 15:22:22 +0200

    OXDEV-8895 Add throws annotations to theme interfaces

--- source/Internal/Framework/Theme/Configuration/Dao/ThemeConfigurationDaoInterface.php
+++ source/Internal/Framework/Theme/Configuration/Dao/ThemeConfigurationDaoInterface.php
@@ -8,9 +8,11 @@
 namespace OxidEsales\EshopCommunity\Internal\Framework\Theme\Configuration\Dao;
 
 use OxidEsales\EshopCommunity\Internal\Framework\Theme\Configuration\DataObject\ThemeConfiguration;
+use OxidEsales\EshopCommunity\Internal\Framework\Theme\Configuration\Exception\ThemeConfigurationNotFoundException;
 
 interface ThemeConfigurationDaoInterface
 {
+    /** @throws ThemeConfigurationNotFoundException */
     public function get(string $themeId, int $shopId): ThemeConfiguration;
 
     public function save(ThemeConfiguration $configuration, int $shopId): void;

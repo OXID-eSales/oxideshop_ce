1950fbfa48d8b9c1f9ef4b40ad3d5a538b5dc7e1
Ashraf <ashraf.boudawara@oxid-esales.com>
2026-08-19 15:22:41 +0200

    OXDEV-10556 Fix wait for basket expiration logic

--- tests/Codeception/Acceptance/PrivateSalesBasketCest.php
+++ tests/Codeception/Acceptance/PrivateSalesBasketCest.php
@@ -113,11 +113,10 @@ final class PrivateSalesBasketCest
         $I->openShop()->searchFor('1000');
         $I->see(Translator::translate('NO_ITEMS_FOUND'));
 
-        $I
-            ->amGoingTo('wait for the basket to expire')
-            ->wait($basketExpirationTimeout + 0.1);
-        $I
-            ->openShop()
+        $I->amGoingTo('wait for the basket to expire')
+            ->wait($basketExpirationTimeout + 1);
+
+        $I->openShop()
             ->checkBasketEmpty()
             ->searchFor('1000');
 
